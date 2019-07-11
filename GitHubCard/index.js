4aebb247561a06bdfc4b2d5764a839c257304dee
masonkarsevar <masonkarsevar@gmail.com>
2019-07-11 08:11:47 -0700

    little experiment with the axios call function. axios is receiving data.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,7 +3,13 @@
            https://api.github.com/users/<your name>
 */
 
-
+axios.get('https://api.github.com/users/karsevar/followers')
+	.then(response => {
+		console.log(response);
+	})
+	.catch(err => {
+		console.log('Can\'t find request');
+	})
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -26,7 +32,7 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan','dustinmyers','justsml','luishrd','bigknell'];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
