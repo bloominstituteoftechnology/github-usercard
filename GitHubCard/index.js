144a5054b4705dba5052f4d9398d0e74452f3a14
masonkarsevar <masonkarsevar@gmail.com>
2019-07-11 08:04:50 -0700

    axios script added to html file

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,8 @@
            https://api.github.com/users/<your name>
 */
 
+
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
