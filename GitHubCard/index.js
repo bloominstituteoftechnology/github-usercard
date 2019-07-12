8f8c4e4fef83ce025db22d26ffabb22e76abdea1
Rosa Perez <rosaperez@Rosas-MBP.attlocal.net>
2019-07-11 17:11:29 -0700

    adding data to js

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,15 +2,18 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-// // select the main dom node to attach our dynamic content
+
+// select the main dom node to attach our dynamic content
+// const entry = document.querySelector('.entry')
+
+// const myData = 'paintedlbird7'
 
 axios.get(`https://api.github.com/users/paintedlbird7`)
 .then(response => {
-  console.log(response.data.login);
+  console.log(response.data);
   let cards = document.querySelectorAll('.cards');
-  let card = myCard(response.data.login);
+  let card = myCard(response.data);
   console.log(card)
-
   cards.appendChild(card)
 })
 
@@ -32,41 +35,6 @@ axios.get(`https://api.github.com/users/paintedlbird7`)
            create a new component and add it to the DOM as a child of .cards
 */
 
-
-/*
-creates and returns DOM node
-*/
-function myCard(
-  users) {
-  // create the elements
-  const card = document.createElement('div')
-  const img = document.createElement('img')
-  const name = document.createElement('h3')
-  
-  // set the styles
-  card.classList.add('user-card')
-  img.classList.add('user-image')
-  
-  // set the content
-  name.textContent = `Users: ${users}`
-  
-  // put together
-  card.appendChild(name)
-  return card
-}
-
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
-
-const followersArray = [];
-
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
@@ -86,6 +54,61 @@ const followersArray = [];
 </div>
 
 */
+/*
+creates and returns DOM node
+*/
+function myCard(data) {
+  // create the elements
+  const card = document.createElement('div')
+  const name = document.createElement('h3')
+  const login = document.createElement('h3')
+  const username = document.createElement('p')
+  const img = document.createElement('img')
+
+  // set the styles
+  card.classList.add('card')
+  name.classList.add('name')
+  login.classList.add('login')
+  username.classList.add('username')
+  img.classList.add('img')
+
+  // set the content
+  img.src = data.avatar_url;
+  gitHubUrl.href = data.url;
+  // title.textContent = paintedlbird7
+  card.textContent = data.card;
+  name.textContent = data.name;
+  login.textContent = data.login;
+  username.textContent = data.login;
+  location.textContent = data.location;
+  msWriteProfilerMark.textContent = `Profile: ${gitHubUrl}`;
+  bio.textContent = myData.bio;
+  // put together
+  
+  card.appendChild(card)
+  card.appendChild(name)
+  card.appendChild(login)
+  card.appendChild(username)
+  card.appendChild(img)
+
+
+
+
+  return card
+}
+
+/* Step 5: Now that you have your own card getting added to the DOM, either 
+          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
+          , manually find some other users' github handles, or use the list found 
+          at the bottom of the page. Get at least 5 different Github usernames and add them as
+          Individual strings to the friendsArray below.
+          
+          Using that array, iterate over it, requesting data for each user, creating a new card for each
+          user, and adding that card to the DOM.
+*/
+
+const followersArray = [];
+
 
 /* List of LS Instructors Github username's: 
   tetondan
