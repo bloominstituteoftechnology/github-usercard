e69aa03485e9cf6c53b7c9d1121ea45df1f3c298
Bangstry <tylerwesleyquinn@gmail.com>
2019-08-08 19:38:35 -0400

    mvp

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,33 @@
            https://api.github.com/users/<your name>
 */
 
+axios.get("https://api.github.com/users/Bangstry")
+.then(response => {
+  console.log(response);
+  cCard.appendChild(superComp(response.data));
+})
+.catch(err => {
+  console.log(err);
+})
+
+const followersArray = [];
+
+axios.get("https://api.github.com/users/Bangstry/followers")
+.then(response =>{
+  console.log(respone);
+  response.data.forEach( element =>{
+    followersArray.push(element);
+  })
+  followersArray.forEach(element =>{
+    cCard.appendChild(superComp(element));
+  })
+})
+.catch(err => {
+  console.log(err);
+})
+
+console.log(followersArray)
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -24,7 +51,14 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [{
+
+}];
+
+followersArray.forEach(item =>{
+  let followerCards = superComp(item);
+  cCard.appendChild(followersCards);
+})
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +79,48 @@ const followersArray = [];
 </div>
 
 */
+
+const cCard = document.querySelector(`.cards`)
+
+function superComp(obj){
+  const cCard = document.createElement("div"),
+  img = document.createElement("img"),
+  cCardInfo = document.createElement("div"),
+  nameH3 = document.createElement("h3");
+  userP = document.createElement("p"),
+  userLoc = document.createElement("p"),
+  profile = document.createElement("p"),
+  profileURL = document.createElement("a"),
+  followers = document.createElement("p"),
+  following = document.createElement("p"),
+  bio = document.createElement("p");
+
+  cCard.classList.add("card");
+  nameH3.classList.add("name");
+  userP.classList.add ("username");
+
+  cCard.appendChild(img);
+  cCard.appendChild(cCardInfo);
+  cCardInfo.appendChild(nameH3);
+  cCardInfo.appendChild(userP);
+  cCardInfo.appendChild(userLoc);
+  cCardInfo.appendChild(profile);
+  cCardInfo.appendChild(followers);
+  cCardInfo.appendChild(following);
+  cCardInfo.appendChild(bio);
+
+  img.src = `${obj.data.avatar_url}`;
+  nameH3.textContent = `${obj.login}`;
+  userP.textContent = `${obj.login}`;
+  userLoc.textContent = `${obj.location}`;
+  profileURL.textContent = `${obj.html_url}`;
+  followers.textContent = `${obj.followers}`;
+  following.textContent = ` ${obj.following}`;
+  bio.textContent = `${obj.bio}`;
+
+  return cCard;
+
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
