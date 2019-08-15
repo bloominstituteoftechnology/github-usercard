e6892f83c9bcc55010a2e420688c856546e62add
ndurbin <nick.durbin@gmail.com>
2019-08-14 22:57:24 -0400

    I have the foreach method on the followers array with the list of users given.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,15 +1,3 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
-
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
-
-   Skip to Step 3.
-*/
-
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
@@ -23,17 +11,26 @@
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
+let data;
 
-const followersArray = [];
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
 
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+followersArray.forEach((follower) => {
 
-*/
+  axios.get(`https://api.github.com/users/${follower}`)
+    .then((response) => {
+      data = response.data;
+      cards.appendChild(githubCard(data));
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    
+})    
 
 const cards = document.querySelector('.cards');
 
-function githubCard (card) {
+function githubCard(item) {
   // creating the variables
   const card = document.createElement('div');
   const image = document.createElement('img');
@@ -66,20 +63,15 @@ function githubCard (card) {
   cardInfo.appendChild(bio);
 
   // adding textContent to the elemements
-  image.textContent = card.image;
-  name.textContent = card.name;
-  userName.textContent = card.username;
-  location.textContent = card.location;
-  profile.textContent = card.profile;
-  address.textContent = card.address;
-  followers.textContent = card.followers;
-  following.tetContent = card.following;
-  bio.tetContent = card.bio;
-}
+  image.textContent = `${item.image}`;
+  name.textContent = `${item.name}`;
+  userName.textContent = `${item.username}`;
+  location.textContent = `${item.location}`;
+  profile.textContent = `${item.profile}`;
+  address.textContent = `${item.address}`;
+  followers.textContent = `${item.followers}`;
+  following.tetContent = `${item.following}`;
+  bio.tetContent = `${item.bio}`;
 
-axios.get(https://api.github.com/users/nickdurbin)
-axios.get(https://api.github.com/users/tetondan)
-axios.get(https://api.github.com/users/dustinmyers)
-axios.get(https://api.github.com/users/justml)
-axios.get(https://api.github.com/users/luishrd)
-axios.get(https://api.github.com/users/bigKnell)
+  return card;
+}
