47dff8dc8f8495f8e5c23403bd84a20e98b7c2bd
dang lu <danglu@dangs-mbp.lan>
2019-08-15 17:21:40 -0700

    MVP reached

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -24,7 +24,9 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [ 'tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -53,3 +55,89 @@ const followersArray = [];
   luishrd
   bigknell
 */
+ 
+
+const url = "https://api.github.com/users/dangnlu18";
+console.log(axios.get(url));
+axios.get(url)
+.then((response)=>{ return response.data})
+.catch((err)=>{
+  console.log('There was an error');console.log(err);
+});
+
+
+function createCard(obj){
+  //create card div
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  //image
+  const image = document.createElement('img');
+  image.setAttribute('src', obj.avatar_url ) ;
+  card.appendChild(image);
+
+  //card info div
+  const cardInfo = document.createElement('div');
+  cardInfo.classList.add('card-info');
+  card.appendChild(cardInfo);
+
+  //h3 title
+  const title = document.createElement('h3');
+  title.classList.add('name');
+  title.textContent = obj.name;
+  cardInfo.appendChild(title);
+
+  //username
+  const pUser = document.createElement('p');
+  pUser.classList.add('username');
+  pUser.textContent = obj.login;
+  cardInfo.appendChild(pUser);
+
+  //location
+  const pLoc = document.createElement('p');
+  pLoc.textContent = `Location: ${obj.location}`;
+  cardInfo.appendChild(pLoc);
+
+  //profile
+  const pProfile = document.createElement('p');
+  pProfile.textContent = `Profile: ${obj.html_url}`;
+  cardInfo.appendChild(pProfile);
+
+  //followers
+  const pFollowers = document.createElement('p');
+  pFollowers.textContent = `Followers: ${obj.followers}`;
+  cardInfo.appendChild(pFollowers);
+
+  //following
+  const pFollowing = document.createElement('p');
+  pFollowing.textContent = `Following: ${obj.following}`;
+  cardInfo.appendChild(pFollowing);
+
+  //bio
+  const bio = document.createElement('p');
+  bio.textContent = `Bio: ${obj.bio}`;
+  cardInfo.appendChild(pFollowers);
+
+  return card;
+}
+
+
+axios.get(url)
+.then((response)=>{ return response.data})
+.then((obj)=>{return createCard(obj)})
+.then((card)=> document.querySelector('.cards').appendChild(card))
+.catch((err)=>{
+  console.log('There was an error');console.log(err);
+});
+
+
+followersArray.forEach((follower)=>{
+  const url2 = `https://api.github.com/users/${follower}`;
+  axios.get(url2)
+  .then((response)=>{ return response.data})
+  .then((obj)=>{return createCard(obj)})
+  .then((card)=> document.querySelector('.cards').appendChild(card))
+  .catch((err)=>{
+    console.log('There was an error');console.log(err);
+  })
+});
