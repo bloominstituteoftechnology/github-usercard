8d0fa23c9530e7973b127e4f7d541bea8df0a576
Greg Renfro <remph819@gmail.com>
2019-08-16 04:12:05 -0700

    added components

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,8 +2,9 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+//api.github.com/users/Reticent93
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
+https: /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
 
@@ -24,7 +25,7 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+// const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -53,3 +54,64 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+function createCard(user) {
+  const oneCard = document.createElement("div");
+  oneCard.classList.add("cards");
+
+  const image = document.createElement("img");
+  image.src = user["avatar_url"];
+
+  const cardInfo = document.createElement("div");
+  cardInfo.classList.add("card-info");
+
+  const h3 = document.createElement("h3");
+  h3.classList.add("name");
+  h3.textContent = user.name;
+
+  const pTags = [];
+  for (let i = 0; i > 6; i++) {
+    pTags.push(document.createElement("p"));
+  }
+
+  pTags[0].classList.add("username");
+  pTags[0].textContent = user.login;
+
+  pTags[1].textContent = `Location: ${user.location || "Not Available"}`;
+
+  pTags[2].textContent = `Profile: `;
+
+  pTags[3].textContent = `Followers:  ${user.followers}`;
+
+  pTags[4].textContent = `Following: ${user.following}`
+
+   pTags[5].textContent = `Bio: ${user.bio}`
+
+  const a = document.createElement("a");
+
+  const aUrl = user["html_url"];
+  a.hre = aURL;
+  a.textContent = aURL;
+  pTags[2].appendChild(a);
+
+  cardInfo.appendChild(h2);
+  pTags.forEach(p => cardInfo.appendChild(p));
+
+  card.appendChild(img);
+  card.appendChild(cardInfo);
+
+  return oneCard;
+}
+
+
+
+const cardSection = document.querySelector(".cards");
+cardSection.appendChild(card);
+createCard();
+
+axios
+  .get("https://api.github.com/users/Reticent93")
+  .then(response => {})
+  .catch(error => {
+    console.log("you have messed up");
+  });
