0bbdb826e845d7c9ed86bd3d68be2bffab4c403a
Manju KC <kcmanjuthapa@gmail.com>
2019-09-05 14:18:47 -0400

    added axios GET request for personal info

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,11 +2,11 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
 axios.get('https://api.github.com/users/Manjukcthapa')
-  .then(data => {
-    console.log('response', data)
-  })
+.then(data =>{
+  console.log('response', data)
+})
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
