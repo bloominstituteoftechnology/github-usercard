f08d3b6f7927124c61c447bcfcbb0b489c872edf
vishalicious213 <vishalicious213@gmail.com>
2019-09-06 01:54:16 -0400

    figured out what was keeping the card from showing up - all the HTML elements were strings instead of variables!

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -23,6 +23,24 @@ axios.get("https://api.github.com/users/vishalicious213")
            create a new component and add it to the DOM as a child of .cards
 */
 
+// const gitOnPage = document.querySelector(".cards");
+
+axios.get("https://api.github.com/users/vishalicious213")
+  .then(response => {
+    console.log("Got data from GitHub");
+    console.log(response);
+    console.log('hello');
+    const gitProfile = gitCard(response.data);
+    console.log(gitProfile);
+    gitOnPage.appendChild(gitProfile)
+  })
+  .catch(err => {
+    console.log("Got error from GitHub ", err)
+  })
+
+
+
+
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
           , manually find some other users' github handles, or use the list found 
@@ -35,49 +53,6 @@ axios.get("https://api.github.com/users/vishalicious213")
 
 const followersArray = [];
 
-function gitCard(arr) {
-  // define new elements
-  const card = document.createElement("div");
-  const imgSrc = document.createElement("img");
-  const cardInfo = document.createElement("div");
-  const name = document.createElement("h3");
-  const userName = document.createElement("p");
-  const location = document.createElement("p");
-  const profile = document.createElement("p");
-  const gitPage = document.createElement("a");
-  const followers = document.createElement("p");
-  const following = document.createElement("p");
-  const bio = document.createElement("p");
-
-  // setup structure of elements (append as a whole unit later)
-  card.appendChild("imgSrc");
-  card.appendChild("cardInfo");
-  cardInfo.appendChild("name");
-  cardInfo.appendChild("userName");
-  cardInfo.appendChild("location");
-  cardInfo.appendChild("profile");
-  cardInfo.appendChild("gitPage"); // should this be nested in profile?
-  cardInfo.appendChild("followers");
-  cardInfo.appendChild("following");
-  cardInfo.appendChild("bio");
-
-  // set class names (from HTML)
-  card.classList.add("card");
-  cardInfo.classList.add("card-info");
-  name.classList.add("name");
-  userName.classList.add("username");
-
-  // set text content (get from function parameter names)
-  imgSrc.textContent = arr.avatar_url;
-  name.textContent = arr.name;
-  userName.textContent = arr.login;
-  location.textContent = arr.location;
-  profile.textContent = "Profile: ";
-  gitPage.textContent = arr.html_url;
-  followers.textContent = arr.followers_url;
-  following.textContent = arr.following_url;
-  bio.textContent = arr.bio;
-}
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -98,6 +73,57 @@ function gitCard(arr) {
 </div>
 
 */
+
+const gitOnPage = document.querySelector(".cards");
+
+function gitCard(object) {
+  // define new elements
+  const card = document.createElement("div");
+  const imgSrc = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const name = document.createElement("h3");
+  const userName = document.createElement("p");
+  const location = document.createElement("p");
+  const profile = document.createElement("p");
+  const gitPage = document.createElement("a");
+  const followers = document.createElement("p");
+  const following = document.createElement("p");
+  const bio = document.createElement("p");
+
+  // setup structure of elements (append as a whole unit later)
+  card.appendChild(imgSrc);           // I had these variables in quotes,
+  card.appendChild(cardInfo);         // which means they were strings
+  cardInfo.appendChild(name);         // Its the little things that can
+  cardInfo.appendChild(userName);     // hurt us...
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  // cardInfo.appendChild("gitPage"); // should this be nested in profile?
+  profile.appendChild(gitPage);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+  // set class names (from HTML)
+  card.classList.add("card");
+  cardInfo.classList.add("card-info");
+  name.classList.add("name");
+  userName.classList.add("username");
+
+  // set text content (get from function parameter names)
+  // imgSrc.textContent = object.data.avatar_url;
+  imgSrc.src = object.avatar_url; // its not text!
+  name.textContent = object.name;
+  userName.textContent = object.login;
+  location.textContent = object.location;
+  profile.textContent = "Profile: ";
+  gitPage.textContent = object.html_url;
+  followers.textContent = object.followers_url;
+  following.textContent = object.following_url;
+  bio.textContent = object.bio;
+
+  return card;
+}
+
 
 /* List of LS Instructors Github username's: 
   tetondan
