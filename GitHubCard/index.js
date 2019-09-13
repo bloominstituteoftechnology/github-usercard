7367b7c768b153d5b9671b910df96c434106bf0c
Westley Feller <feller.westley@gmail.com>
2019-09-12 18:43:54 -0700

    Card component created
    MVP Complete

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,55 +1,75 @@
-/* Step 1: using axios, send a GET request to the following URL
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
+const createGithubCard = (user) => {
+  const card = document.createElement('div');
+  card.className = 'card';
 
-/* Step 2: Inspect and study the data coming back, this is YOUR
-   github info! You will need to understand the structure of this
-   data in order to use it to build your component function
+  const cardImage = document.createElement('img');
+  cardImage.src = user.data.avatar_url;
+  card.appendChild(cardImage);
 
-   Skip to Step 3.
-*/
+  const cardInfo = document.createElement('div');
+  cardInfo.className = 'card-info';
+  card.appendChild(cardInfo);
 
-/* Step 4: Pass the data received from Github into your function,
-           create a new component and add it to the DOM as a child of .cards
-*/
+  const name = document.createElement('h3');
+  name.className = 'name';
+  name.textContent = user.data.name;
+  cardInfo.appendChild(name);
 
-/* Step 5: Now that you have your own card getting added to the DOM, either
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers
-          , manually find some other users' github handles, or use the list found
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
+  const userName = document.createElement('p');
+  userName.className = 'username';
+  userName.textContent = user.data.username;
+  cardInfo.appendChild(userName);
 
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
+  const location = document.createElement('p');
+  location.textContent = `Location: ${user.data.location}`;
+  cardInfo.appendChild(location);
 
-const followersArray = [];
+  const profile = document.createElement('p');
+  profile.textContent = 'Profile: ';
+  cardInfo.appendChild(profile);
 
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+  const profileLink = document.createElement('a');
+  profileLink.href = user.data.html_url;
+  profileLink.textContent = user.data.html_url;
+  profile.appendChild(profileLink);
 
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
+  const followers = document.createElement('p');
+  followers.textContent = `Followers: ${user.data.followers}`;
+  cardInfo.appendChild(followers);
 
-*/
+  const following = document.createElement('p');
+  followers.textContent = `Following: ${user.data.following}`;
+  cardInfo.appendChild(following);
 
-/* List of LS Instructors Github username's:
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+  const bio = document.createElement('p');
+  bio.textContent = `Bio: ${user.data.bio}`;
+  cardInfo.appendChild(bio);
+
+  document.querySelector('.cards').appendChild(card);
+};
+
+axios.get('https://api.github.com/users/fwesss')
+  .then((result) => {
+    createGithubCard(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const followersArray = [
+  'tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell',
+];
+
+followersArray.forEach((user) => {
+  axios.get(`https://api.github.com/users/${user}`)
+    .then(((result) => {
+      createGithubCard(result);
+    }))
+    .catch((error) => {
+      console.log(error);
+    });
+});
