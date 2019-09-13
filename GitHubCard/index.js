31a2c4c67f6c1f46e7b8889fd59f7285e66ada71
anatulea <anatulea14@.gmali.com>
2019-09-12 23:16:56 -0500

    inicial commit

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,14 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/anatulea')
+.then((results) => {
+  console.log(results);  
+  
+})
+.catch((err) => {
+  console.log(err);
+})
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
