a9979a926085f28c922f62d6c74889fcecbec411
Brennan Neilson <bvneilson@gmail.com>
2019-09-12 20:51:01 -0600

    Stretch goal - programmatic followers list

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,65 +1,26 @@
-/* Step 1: using axios, send a GET request to the following URL
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
-  axios.get('https://api.github.com/users/bvneilson').then(function (res) {
-    console.log(res.data);
-    cards.appendChild(cardFactory(res.data));
-  }).catch(function (err) {
-    console.log(err);
-  });
-
-
-/* Step 2: Inspect and study the data coming back, this is YOUR
-   github info! You will need to understand the structure of this
-   data in order to use it to build your component function
-
-   Skip to Step 3.
-*/
-
-/* Step 4: Pass the data received from Github into your function,
-           create a new component and add it to the DOM as a child of .cards
-*/
 const cards = document.querySelector('.cards');
-/* Step 5: Now that you have your own card getting added to the DOM, either
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers
-          , manually find some other users' github handles, or use the list found
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
 
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
+function displayUserAndFollowers (username) {
+  axios.get(`https://api.github.com/users/${username}`).then(function (res) {
+    const userObj = res.data;
+    cards.appendChild(cardFactory(userObj));
 
-const followersArray = ['BrockNeilson','jasonalmaturner','Toniddarden','leachcoding','hellofaweek'];
-followersArray.forEach((follower) => {
-  axios.get(`https://api.github.com/users/${follower}`).then(function (res) {
-    console.log(res.data);
-    cards.appendChild(cardFactory(res.data));
+    axios.get(`https://api.github.com/users/${username}/followers`).then(function (res) {
+      console.log(res.data);
+      res.data.forEach((follower) => {
+        axios.get(`https://api.github.com/users/${follower.login}`).then(function (res) {
+          cards.appendChild(cardFactory(res.data));
+        });
+      });
+
+    });
+
   }).catch(function (err) {
     console.log(err);
   });
-})
+}
+displayUserAndFollowers('bvneilson');
 
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
-
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
-
-*/
 function cardFactory (data) {
   const card = document.createElement('div');
   const userImg = document.createElement('img');
@@ -103,11 +64,3 @@ function cardFactory (data) {
 
   return card;
 }
-
-/* List of LS Instructors Github username's:
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
