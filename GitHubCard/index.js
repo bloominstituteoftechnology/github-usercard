355c13d0b5c4839c19483c73f83c61464d6c75e1
Schrese <53787633+Schrese@users.noreply.github.com>
2019-10-03 17:11:14 -0400

    Just beginning

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,11 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios 
+  .get('https://api.github.com/users/Schrese')
+  .then(response => {
+    console.log(response);
+  })
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -25,6 +30,42 @@
 */
 
 const followersArray = [];
+
+function cardCreator(something) {
+  const 
+  card = document.createElement('div'),
+  profilePic = docmuent.createElement('img'), 
+  cardInfo = document.createElement('div'), 
+  realName = document.createElement('h3'),
+  userName = document.createElement('p'),
+  userLocation = document.createElement('p'), 
+  userProfile = document.createElement('p'), 
+  userFollowers = document.createElement('p'), 
+  userFollowing = document.createElement('p'), 
+  userBio = document.createElement('p');
+
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  realName.classList.add('name');
+  userName.classList.add('username');
+
+  card.appendChild(profilePic);
+  card.appendChild(cardInfo);
+  card.appendChild(realName);
+  card.appendChild(userName);
+  card.appendChild(userLocation);
+  card.appendChild(userProfile);
+  card.appendChild(userFollowers);
+  card.appendChild(userFollowing);
+  card.appendChild(userBio);
+
+  realName.textContent = data.name;
+
+  return card;
+
+
+
+} 
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
