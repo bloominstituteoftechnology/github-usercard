d51908c1ea9cfea395da54d3a0a00760e76eb6c1
Brendan Schatz <brendanschatz97@gmail.com>
2019-10-03 17:43:03 -0400

    finished mvp and got it to update automatically so first stretch goal.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -15,7 +15,7 @@
 */
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
+          follow this link in your browser https://api.github.com/users/bschatzj/followers 
           , manually find some other users' github handles, or use the list found 
           at the bottom of the page. Get at least 5 different Github usernames and add them as
           Individual strings to the friendsArray below.
@@ -24,7 +24,7 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+ followersArray = ["https://api.github.com/users/ddelfaus", "https://api.github.com/users/SteveMM-III", "https://api.github.com/users/Sara-DLC","https://api.github.com/users/lflores0214", "https://api.github.com/users/primelos"];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +45,111 @@ const followersArray = [];
 </div>
 
 */
+let followArray = []
+
+// axios.get("https://api.github.com/users/bschatzj/followers")
+//   .then(response => {
+//     response.data.forEach(element => {
+//     followArray.push(element.url);
+//     return followArray;
+//   })
+// })
+//   .catch(error => {
+//     console.log("The data was not returned", error);
+//   });
+
+
+
+
+axios.get("https://api.github.com/users/bschatzj/followers")
+  .then(response => {
+    response.data.forEach(element => {
+    axios.get(element.url)
+    .then(response => {
+      console.log(response);
+      const cardNew = newCard(response);
+      cards.appendChild(cardNew);
+    })
+  })
+})
+  .catch(error => {
+    console.log("The data was not returned", error);
+  });
+
+
+axios.get("https://api.github.com/users/bschatzj")
+  .then(response => {
+    const cardNew = newCard(response);
+    cards.appendChild(cardNew);
+  })
+  .catch(error => {
+    console.log("The data was not returned", error);
+  });
+
+
+
+
+// console.log(followArray)
+
+// followArray.forEach(object => {
+// axios.get(object)
+// .then(response => {
+//   console.log(response);
+//   const cardNew = newCard(response);
+//   cards.appendChild(cardNew);
+// })
+// .catch(error => {
+//   console.log("The data was not returned", error);
+// });
+// });
+
+
+const cards = document.querySelector('.cards');
+
+function newCard(object) {
+  const card = document.createElement('div');
+  const picture = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const userName = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const link = document.createElement('a');
+  const followers= document.createElement('p');
+  const following= document.createElement('p');
+  const bio = document.createElement('p');
+
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  userName.classList.add('username');
+
+
+  card.appendChild(picture);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(userName);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  profile.appendChild(link);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+  picture.src = object.data.avatar_url;
+  name.textContent = `${object.data.name}`;
+  userName.textContent = `Username: ${object.data.login}`;
+  location.textContent = `Location: ${object.data.location}`;
+  link.textContent = `Link: ${object.data.html_url}`;
+  followers.textContent = `Followers: ${object.data.followers}`;
+  following.textContent = `Following: ${object.data.following}`;
+  bio.textContent = `Bio: ${object.data.bio} `;
+
+
+  return card;
+}
+
+
 
 /* List of LS Instructors Github username's: 
   tetondan
