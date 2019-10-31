8f59a808b5a065ce85fbb84d0c21000737d00d76
sergeikabuldzhanov <thesiegebot@gmail.com>
2019-10-31 18:49:41 +0500

    Finished MVP and auto follower stretch

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,17 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/luishrd')
+.then(response=>{
+  console.log(response);
+  const cards = document.querySelector('.cards');
+  cards.append(createCard(response.data));
+  getFollowers(response);
+})
+.catch(error=>{
+  console.log(error);
+  
+});
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -24,8 +35,20 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
-
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+/* 
+followersArray.forEach(follower=>{
+  axios.get(`https://api.github.com/users/${follower}`)
+    .then(response=>{
+      console.log(response);
+      const cards = document.querySelector('.cards');
+      cards.append(createCard(response.data));
+    })
+    .catch(error=>{
+      console.log(error);
+    });
+})
+ */
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
@@ -45,7 +68,56 @@ const followersArray = [];
 </div>
 
 */
+function createCard(obj){
+  function checkIfNull(str){
+    if(str) return str;
+    return '';
+  }
+  //Create HTML
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.innerHTML = `<img src=${checkIfNull(obj.avatar_url)} />
+  <div class="card-info">
+    <h3 class="name">${checkIfNull(obj.name)}</h3>
+    <p class="username">${obj.login}</p>
+    <p>Location: ${checkIfNull(obj.location)}</p>
+    <p>Profile:  
+      <a href=${obj.html_url}>${obj.html_url}</a>
+    </p>
+    <p>Followers: ${obj.followers}</p>
+    <p>Following: ${obj.following}</p>
+    <p>Bio: ${checkIfNull(obj.bio)}</p>
+  </div>`;
+  //Create button
+  const button = document.createElement('span');
+  button.addEventListener('click', event=>{
+    
+  })
+  return card;
+}
 
+function getFollowers(obj){
+  axios.get(obj.data.followers_url)
+    .then(response=>{
+      console.log(response);
+      const cards = document.querySelector('.cards');
+      response.data.forEach(follower=>{
+        axios.get(follower.url)
+          .then(response=>{
+            cards.append(createCard(response.data));
+          })
+          .catch(error=>{
+            console.error(error);
+            
+          });
+      })
+      return response;
+    })
+    .catch(error=>{
+      console.log(error);
+      
+    })
+}
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
