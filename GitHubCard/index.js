74ad6b646d1bf272ad754d6fd42656606c2655c1
Sarah Elias <sarah@Sarahs-MacBook-Pro.local>
2019-10-31 19:51:13 -0700

    finished work

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,9 +2,17 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-// axios.get('https://api.github.com/users/sadamexx').then(response => {
-//   console.log(response);
-// })
+axios.get('https://api.github.com/users/sadamexx').then(response => {
+  // console.log(response);
+  parentDiv.appendChild(createCards(response))
+  .catch(error => {
+    console.log(error);
+  })
+});
+
+
+const parentDiv = document.querySelector('.cards');
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -26,7 +34,19 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['javish88', 'MarFan', 'RonnieDipple', 'ShandaWoods', 'itshui3'];
+
+followersArray.forEach(function (element) {
+  axios.get(`https://api.github.com/users/${element}`)
+    .then(response => {
+      parentDiv.append(createCards(response))
+    })
+    .catch(error => {
+      console.log(error);
+    })
+})
+
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -48,13 +68,14 @@ const followersArray = [];
 
 */
 
-function createCards (object){
+function createCards(object) {
   const container = document.createElement('div');
   container.classList.add('card');
-
+  console.log(object);
   const image = document.createElement('img');
-  container.appendChild(image);
   image.src = object.data.avatar_url;
+  container.appendChild(image);
+
 
   const info = document.createElement('div');
   info.classList.add('card-info');
@@ -72,11 +93,11 @@ function createCards (object){
 
   const userLoc = document.createElement('p');
   info.appendChild(userLoc);
-  userLoc.textContent = 'Location:' + objec.data.location;
+  userLoc.textContent = 'Location:' + object.data.location;
 
   const userProf = document.createElement('p');
   info.appendChild(userProf);
-  userProf.textContent = "Profile:"
+  userProf.textContent = 'Profile:';
 
   const link = document.createElement('a');
   userProf.appendChild(link);
@@ -95,10 +116,12 @@ function createCards (object){
   userBio.textContent = object.data.bio;
 
 
-return container;
+  return container;
 }
 
-/* List of LS Instructors Github username's: 
+
+
+/* List of LS Instructors Github username's:
   tetondan
   dustinmyers
   justsml
