6ea7b1601b80e06f39517e0c17c2925a7771fe00
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2019-11-07 22:11:26 -0800

    added GitHub contribution graph and made the code DYR

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,4 +1,3 @@
-
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
@@ -18,56 +17,69 @@
 </div>
 
 */
-function cardMaker(profileInfo){
+function cardMaker(profileInfo) {
 
   // create all elements
-    const newCard = document.createElement('div');
-    const cardPic =document.createElement('img')
-    const cardContent = document.createElement('div');
-    const cardName =document.createElement('h3')
-    const cardUserName =document.createElement('p');
-    const cardLoc =document.createElement('p');
-    const cardLink =document.createElement('p');
-    const cardFollowers =document.createElement('p');
-    const cardFollowing =document.createElement('p');
-    const cardBio =document.createElement('p');
-  
-    // create structure
-    newCard.appendChild(cardPic);
-    newCard.appendChild(cardContent);
-    cardContent.appendChild(cardName);
-    cardContent.appendChild(cardUserName);
-    cardContent.appendChild(cardLoc);
-    cardContent.appendChild(cardLink);
-    cardContent.appendChild(cardFollowers);
-    cardContent.appendChild(cardFollowing);
-    cardContent.appendChild(cardBio);
-  
-     // set the content
-     cardPic.setAttribute ('src', profileInfo.avatar_url)
-     cardName.textContent = profileInfo.name
-     cardUserName.textContent = profileInfo.login
-     cardLoc.textContent = `Location: ${profileInfo.location}`
-     cardPic.setAttribute('alt', `${profileInfo.name}'s photo`)
-     cardLink.setAttribute('href', profileInfo.html_url)
-     cardLink.textContent = `Profile: ${profileInfo.html_url}`
-     cardFollowers.textContent = `Followers: ${profileInfo.followers}`
-     cardFollowing.textContent = `Following: ${profileInfo.followers}`
-     cardBio.textContent =  `Bio: ${profileInfo.bio}`
-  
-     // apply styles
-     newCard.classList.add('card');
-     cardContent.classList.add('card-info');
-     cardName.classList.add('name');
-     cardUserName.classList.add('username');
-    
- /* Step 4: Pass the data received from Github into your function, 
+  const newCard = document.createElement('div');
+  const cardPic = document.createElement('img')
+  const cardContent = document.createElement('div');
+  const cardName = document.createElement('h3')
+  const cardUserName = document.createElement('p');
+  const cardLoc = document.createElement('p');
+  const cardLink = document.createElement('p');
+  const cardLinkBtn = document.createElement('a');
+  const cardFollowers = document.createElement('p');
+  const cardFollowing = document.createElement('p');
+  const cardBio = document.createElement('p');
+  const userCalendar = document.createElement('div')
+
+  // create structure
+  newCard.appendChild(cardPic);
+  newCard.appendChild(cardContent);
+  cardContent.appendChild(cardName);
+  cardContent.appendChild(cardUserName);
+  cardContent.appendChild(cardLoc);
+  cardContent.appendChild(cardLink);
+  cardContent.appendChild(cardLinkBtn);
+  cardContent.appendChild(cardFollowers);
+  cardContent.appendChild(cardFollowing);
+  cardContent.appendChild(cardBio);
+  newCard.appendChild(userCalendar);
+
+  // set the content
+  cardPic.setAttribute('src', profileInfo.avatar_url)
+  cardName.textContent = profileInfo.name
+  cardUserName.textContent = profileInfo.login
+  cardLoc.textContent = `Location: ${profileInfo.location}`
+  cardPic.setAttribute('alt', `${profileInfo.name}'s photo`)
+  cardLink.setAttribute('href', profileInfo.html_url)
+  cardLink.textContent = `Profile: ${profileInfo.html_url}`
+  cardLinkBtn.setAttribute('href', profileInfo.html_url)
+  cardFollowers.textContent = `Followers: ${profileInfo.followers}`
+  cardFollowing.textContent = `Following: ${profileInfo.followers}`
+  cardBio.textContent = `Bio: ${profileInfo.bio}`
+
+  // apply styles
+  newCard.classList.add('card');
+  cardContent.classList.add('card-info');
+  cardName.classList.add('name');
+  cardUserName.classList.add('username');
+  userCalendar.classList.add(`${profileInfo.login}`)
+  cardLinkBtn.classList.add('fa','fa-github');
+  cardLinkBtn.style.fontSize ='30px';
+  cardLinkBtn.style.textDecoration ='none';
+  cardLinkBtn.style.color ='black';
+
+  /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
-     //return newCard;
-     const cards = document.querySelector('.cards')
-     cards.appendChild(newCard)
-  }
+  //return newCard;
+  const cards = document.querySelector('.cards');
+  cards.appendChild(newCard);
+
+  new GitHubCalendar(`.${profileInfo.login}`, profileInfo.login);
+
+}
 
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
@@ -81,64 +93,84 @@ function cardMaker(profileInfo){
    Skip to Step 3.
 */
 
-axios.get(`https://api.github.com/users/AnaTulea`)
-.then(res => {
-  console.log(res.data)
-  cardMaker(res.data)
-  })
-  .catch(err => {
-    console.log(err)
-  })
 
-  axios.get(`https://api.github.com/users/JesseBentley`)
-.then(res => {
-  console.log(res.data)
-  cardMaker(res.data)
-  })
-  .catch(err => {
-    console.log(err)
-  })
-
-
-  axios.get(`https://api.github.com/users/dcastaneda82`)
-  .then(res => {
-    console.log(res.data)
-    cardMaker(res.data)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-
-
-  axios.get(`https://api.github.com/users/Rodney-Race`)
-  .then(res => {
-    console.log(res.data)
-    cardMaker(res.data)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-
-
-    axios.get(`https://api.github.com/users/Alphaseinor`)
+function cardMakerAPI(fullname) {
+  axios.get(`https://api.github.com/users/${fullname}`)
     .then(res => {
-      console.log(res.data)
-      cardMaker(res.data)
-      })
-      .catch(err => {
-        console.log(err)
-      })
+      console.log(res.data);
+      cardMaker(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+const gitUsers = ["AnaTulea", "JesseBentley", "Rodney-Race", "dcastaneda82", "Alphaseinor", "MileyWright"];
 
-      axios.get(`https://api.github.com/users/MileyWright`)
-      .then(res => {
-        console.log(res.data)
-        cardMaker(res.data)
-        })
-        .catch(err => {
-          console.log(err)
-        })
+gitUsers.forEach(user => {
+  console.log("user", user);
+  cardMakerAPI(user);
+});
 
-        /* Step 5: Now that you have your own card getting added to the DOM, either 
+
+
+// axios.get(`https://api.github.com/users/AnaTulea`)
+// .then(res => {
+//   console.log("hhhhhhh", res)
+//   cardMaker(res.data)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
+
+//   axios.get(`https://api.github.com/users/JesseBentley`)
+// .then(res => {
+//   console.log(res.data)
+//   cardMaker(res.data)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
+
+
+//   axios.get(`https://api.github.com/users/dcastaneda82`)
+//   .then(res => {
+//     console.log(res.data)
+//     cardMaker(res.data)
+//     })
+//     .catch(err => {
+//       console.log(err)
+//     })
+
+
+//   axios.get(`https://api.github.com/users/Rodney-Race`)
+//   .then(res => {
+//     console.log(res.data)
+//     cardMaker(res.data)
+//     })
+//     .catch(err => {
+//       console.log(err)
+//     })
+
+
+//     axios.get(`https://api.github.com/users/Alphaseinor`)
+//     .then(res => {
+//       console.log(res.data)
+//       cardMaker(res.data)
+//       })
+//       .catch(err => {
+//         console.log(err)
+//       })
+
+//       axios.get(`https://api.github.com/users/MileyWright`)
+//       .then(res => {
+//         console.log(res.data)
+//         cardMaker(res.data)
+//         })
+//         .catch(err => {
+//           console.log(err)
+//         })
+
+/* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
           , manually find some other users' github handles, or use the list found 
           at the bottom of the page. Get at least 5 different Github usernames and add them as
@@ -148,7 +180,7 @@ axios.get(`https://api.github.com/users/AnaTulea`)
           user, and adding that card to the DOM.
 */
 
-const followersArray = ['https://api.github.com/users/JesseBentley','https://api.github.com/users/cesarhj19','https://api.github.com/users/Rodney-Race','https://api.github.com/users/Alphaseinor','https://api.github.com/users/MileyWright'];
+const followersArray = ['https://api.github.com/users/JesseBentley', 'https://api.github.com/users/cesarhj19', 'https://api.github.com/users/Rodney-Race', 'https://api.github.com/users/Alphaseinor', 'https://api.github.com/users/MileyWright'];
 
 /* List of LS Instructors Github username's: 
   tetondan
