86e20151d5cb88d9ce7563962a3eeff7bd019108
christina kaliora <develop@pirron.one>
2019-12-05 22:39:32 +0100

    first commit

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,15 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/christinakal')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -45,6 +53,31 @@ const followersArray = [];
 </div>
 
 */
+function makeComponent(obj){
+
+  // Create HTML elements
+  const card = document.createElement('div'),
+        userImg = document.createElement('img'),
+        info = document.createElement('div'),
+        name = document.createElement('h3'),
+        username = document.createElement('p'),
+        location = document.createElement('p'),
+        links = document.createElement('a'),
+        followers = document.createElement('p'),
+        following = document.createElement('p'),
+        bio = document.createElement('p');
+
+
+  //Assign classes
+  card.classList.add('card');
+  info.classList.add('card-info');
+  name.classList.add('name');
+  username.classList.add('username');
+
+  
+};
+
+makeComponent();
 
 /* List of LS Instructors Github username's: 
   tetondan
