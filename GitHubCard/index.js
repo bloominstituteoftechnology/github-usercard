a430dbfda692aadae54cf8b18d2a9cc8febd67ed
Sierra Curtis <sierracurtis91@gmail.com>
2019-12-05 14:27:27 -0500

    added axios.get to js file

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,13 @@
 /* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
+           (replacing the placeholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/scurtis6')
+  .then(response =>{
+    console.log(response);
+    const card = document.querySelector('.cards');
+    card.append(createCard(response.data))
+  })
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
