5a334cb1c274f872b1e2bcf991085578d59430c2
Sierra Curtis <sierracurtis91@gmail.com>
2019-12-05 16:25:59 -0500

    working on getting the link to show

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,11 +2,13 @@
            (replacing the placeholder with your Github name):
            https://api.github.com/users/<your name>
 */
+const cards = document.querySelector('.cards');
+
 axios.get('https://api.github.com/users/scurtis6')
   .then(response =>{
     console.log(response);
-    const card = document.querySelector('.cards');
-    card.append(createCard(response.data))
+    const cards = document.querySelector('.cards');
+    cards.append(createCard(response.data))
   })
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -29,13 +31,12 @@ axios.get('https://api.github.com/users/scurtis6')
           user, and adding that card to the DOM.
 */
 
-const followersArray = [tetondan, dustinmyers, justsml, luishrd, bigknell];
-followersArray.forEach(followers =>{
-  axios.get('https://api.github.com/users/${followers}')
-  .then(info =>{
-    console.log(info);
-    const card = document.querySelector('.cards');
-    card.append(createCard(info.data));
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+followersArray.forEach(item =>{
+  axios.get(`https://api.github.com/users/${item}`)
+  .then(response =>{
+    console.log(response);
+    cards.append(createCard(response.data));
   })
 })
 
@@ -50,7 +51,7 @@ followersArray.forEach(followers =>{
     <p class="username">{users user name}</p>
     <p>Location: {users location}</p>
     <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
+      <a href={address to users github page}</a>
     </p>
     <p>Followers: {users followers count}</p>
     <p>Following: {users following count}</p>
@@ -59,6 +60,53 @@ followersArray.forEach(followers =>{
 </div>
 
 */
+function createCard(object){
+  const card = document.createElement('div'),
+        img = document.createElement('img'),
+        cardInfo = document.createElement('div'),
+        name = document.createElement('h3'),
+        userName = document.createElement('p'),
+        location = document.createElement('p'),
+        profile = document.createElement('p'),
+        userLink = document.createElement('a'),
+        followers = document.createElement('p'),
+        following = document.createElement('p'),
+        bio = document.createElement('p');
+
+        card.append(img);
+        card.append(cardInfo);
+        cardInfo.append(name);
+        cardInfo.append(userName);
+        cardInfo.append(location);
+        cardInfo.append(profile);
+        cardInfo.append(followers);
+        cardInfo.append(following);
+        cardInfo.append(bio);
+        profile.append(userLink);
+
+        card.classList.add('card');
+        cardInfo.classList.add('card-info');
+        name.classList.add('name');
+        userName.classList.add('username');
+
+        img.src = object.avatar_url;
+        name.textContent = object.name;
+        userName.textContent = object.login;
+        location.textContent = `Location: ${object.location}`;
+        profile.textContent = 'Profile:';
+        userLink.href = object.html_url;
+        followers.textContent = `Followers: ${object.followers}`;
+        following.textContent = `Following: ${object.following}`;
+        bio.textContent = `Bio: ${object.bio}`
+
+
+        return card;
+}
+
+// const card = document.querySelector('.card');
+// card.forEach(object =>{
+
+// })
 
 /* List of LS Instructors Github username's: 
   tetondan
