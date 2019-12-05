d2642f268919b6b3e1f83fef74ab5c0b8b9d3673
Sierra Curtis <sierracurtis91@gmail.com>
2019-12-05 14:36:15 -0500

    added followers in the array

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -29,7 +29,8 @@ axios.get('https://api.github.com/users/scurtis6')
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [tetondan, dustinmyers, justsml, luishrd, bigknell];
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
