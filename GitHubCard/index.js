380d3445c139e11648ce4487e2b6bf06c3dd786e
Andrea Harris <Andrea@zacs-imac.lan>
2019-12-05 17:40:16 -0600

    fixing

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,10 +2,12 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+
+
+  const cards= document.querySelector(".cards");
 axios.get("https://api.github.com/users/aharris1012")
 .then(response =>{
-  console.log(response.data.message);
-  response.data.message.forEach
+  cards.append(GitCards(response.data))
 })
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -28,7 +30,17 @@ axios.get("https://api.github.com/users/aharris1012")
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
+
+
+const followersArray = ['IsabellaGuo','anamonteiro430','CAM603','PHONGdotTech','lisabpink'];
+ 
+followersArray.forEach(user =>{
+  axios.get('https://api.github.com/users/${user}')
+  .then(response =>{
+    cards.appendChild(GitCards(response.data))
+  });
+})
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -58,6 +70,7 @@ function GitCards(Users){
   const UsersName= document.createElement("p")
   const Loc= document.createElement("p")
   const Prof= document.createElement("p")
+  const Href= document.createElement("a")
   const FollowerS= document.createElement("p")
   const FollowinG=document.createElement("p")
   const BiO= document.createElement("p")
@@ -68,9 +81,39 @@ function GitCards(Users){
   CardInfo.classList.add("card-info")
   Name.classList.add("name")
   UsersName.classList.add("username")
-  
+  Href.href=""
+
+
+
+  Card.appendChild(Img);
+  Card.appendChild(CardInfo);
+  CardInfo.appendChild(Name);
+  CardInfo.appendChild(UsersName);
+  CardInfo.appendChild(Loc);
+  CardInfo.appendChild(Prof);
+  Prof.appendChild(Href);
+  CardInfo.appendChild(FollowerS);
+  CardInfo.appendChild(FollowinG);
+  CardInfo.appendChild(BiO);
+
+  Img.src=Users.avatar_url;
+  Name.textContent= Users.name;
+  UsersName.textContent= Users.login;
+  Loc.textContent= `This person lives in: ${Users.location}`;
+  Prof.textContent= "Look at this Profile:"
+  Href.href= Users.html_url;
+  FollowerS.textContent= `Followers: ${Users.followers}`;
+  FollowinG.textContent=`Following: ${Users.following}`;
+  BiO=textContent= `Bio:${Users.bio}`;
+
+return Card
+
+
+
 
 }
+
+
 
 /* List of LS Instructors Github username's: 
   tetondan
