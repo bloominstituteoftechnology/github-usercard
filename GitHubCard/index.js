d32fe25569a19adc071ac8de2a713e47c05757d3
Drew Tora <drewtora@live.com>
2019-12-15 20:53:06 -0500

    MVP v1
    Try Codesandbox

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,79 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+const followersArray = [
+  "MrTora2",
+  "Celaira",
+  "miragekamran",
+  "worksofhart"
+];
+
+let addDOM = document.querySelector(".cards");
+
+followersArray.forEach(element => {
+  axios.get(`https://api.github.com/users/${element}`)
+    .then((response) => {
+      console.log("Success");
+      console.log(response);
+      addDOM.appendChild(createCard(response.data));
+    })
+    .catch((error) => {
+      console.log("Network request was unsuccessful.");
+      console.log(error);
+    })
+
+})
+
+function createCard(userItem){
+  let div = document.createElement("div");
+  div.classList.add("card");
+
+  let img = document.createElement("img");
+  div.appendChild(img);
+  img.src = userItem.avatar_url;
+
+  let div2 = document.createElement("div");
+  div.appendChild(div2);
+  div2.classList.add("card-info");
+
+  let h3 = document.createElement("h3");
+  div2.appendChild(h3);
+  h3.classList.add("name");
+  h3.innerHTML = userItem.name;
+
+  let p1 = document.createElement("p");
+  div2.appendChild(p1);
+  p1.classList.add("username");
+  p1.innerHTML = userItem.login;
+
+  let p2 = document.createElement("p");
+  div2.appendChild(p2);
+  p2.innerHTML = "Location: " + userItem.location;
+
+  let p3 = document.createElement("p");
+  div2.appendChild(p3);
+  p3.innerHTML = "Profile:";
+
+  let a = document.createElement("a");
+  p3.appendChild(a);
+  a.href = userItem.html_url;
+  a.innerHTML = userItem.html_url;
+
+  let p4 = document.createElement("p");
+  div2.appendChild(p4);
+  p4.innerHTML = "Followers: " + userItem.followers;
+
+  let p5 = document.createElement("p");
+  div2.appendChild(p5);
+  p5.innerHTML = "Following: " + userItem.following;
+
+  let p6 = document.createElement("p");
+  div2.appendChild(p6);
+  p6.innerHTML = "Bio: " + userItem.bio;
+
+  return div;
+
+}
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -24,7 +97,6 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
