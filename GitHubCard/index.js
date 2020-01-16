7bf99650815eb7307e4501088a5740a554c2c9ee
Elijah Paminsan <e.paminsan@gmail.com>
2020-01-16 12:01:03 -0800

    update

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,55 +1,73 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
+axios.get('https://api.github.com/users/elijahdaniel').then(data => {
+  const myInfo = data.data
+  const cards = document.querySelector('.cards')
+  const cardInfo = myCard(myInfo)
+  cards.appendChild(cardInfo)
+})
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+const cards = document.querySelector('.cards')
 
-   Skip to Step 3.
-*/
+const myCard = info => {
+  const card = document.createElement('div')
+  const img = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const name = document.createElement('h3')
+  const userName = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p')
+  const profileAnchor = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
 
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
+  img.classList.add('img')
+  card.classList.add('card')
+  name.classList.add('name')
+  userName.classList.add('username')
+  bio.classList.add('p')
 
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
+  card.append(img)
+  card.append(cardInfo)
+  cardInfo.append(name)
+  cardInfo.append(userName)
+  cardInfo.append(location)
+  cardInfo.append(profile)
+  profile.append(profileAnchor)
+  cardInfo.append(followers)
+  cardInfo.append(following)
+  cardInfo.append(bio)
 
-const followersArray = [];
+  const anchor = info.html_url
 
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+  img.src = info.avatar_url
+  location.textContent = info.location
+  name.textContent = info.name
+  userName.textContent = info.login
+  profileAnchor.innerHTML = anchor.link(info.html_url)
+  followers.textContent = `Followers: ${info.followers}`
+  following.textContent = `Following: ${info.following}`
+  bio.textContent = info.bio
 
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
+  return card
+}
 
-*/
+const followersArray = [
+  'fireblastdaniel',
+  'msinnema33',
+  'rofstudios',
+  'shawnpatel98',
+  'kejdacelaj',
+  'guytonoriji'
+]
 
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+followersArray.forEach(user => {
+  axios
+    .get(`https://api.github.com/users/${user}`)
+    .then(data => {
+      const myInfo = data.data
+      const cards = document.querySelector('.cards')
+      const cardInfo = myCard(myInfo)
+      cards.appendChild(cardInfo)
+    })
+    .catch(err => err)
+})
