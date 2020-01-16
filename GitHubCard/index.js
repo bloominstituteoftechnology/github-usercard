dc9ff953a1013482fd7016165257983c4b1ab4e8
Mandi Haase <haase1020@gmail.com>
2020-01-16 15:39:10 -0500

    add elements to index.js

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,10 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+axios.get("https://api.github.com/users/haase1020")
+.then(response =>{
+  console.log(response);
+})
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -45,7 +48,21 @@ const followersArray = [];
 </div>
 
 */
+function studentCard(ghcard){
+  const newCard= document.createElement('div'),
+        newImg = document.createElement('img'),
+        cardInfo = document.createElement('div'),
+        name = document.createElement('h3'),
+        userName = document.createElement('p'),
+        location = document.createElement('p'),
+        profile = document.createElement('p'),
+        address = document.createElement('a'),
+        followers = document.createElement('p'),
+        following = document.createElement('p'),
+        bio = document.createElement('p'),
+        
 
+}
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
