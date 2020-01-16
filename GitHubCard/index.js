f13a69492eee3d5a60f1fc033b2a705c25f6d1e2
Seth Cox <Sethcox16@yahoo.com>
2020-01-16 18:55:37 -0500

    Index.js Set the followersArray The axios API is now passing the information into the function

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -24,7 +24,7 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -54,10 +54,23 @@ const followersArray = [];
   bigknell
 */
 
-function createChild (childTitle) {
+const followersArray = [
+  'SethC16',
+  'tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell',
+];
+
+
+// Function Setup
+function createChild (user) {
+
+  // Setup the createElements
   let newCard = document.createElement('div'),
-      newChild = document.createElement('div'),
       newImg = document.createElement('img'),
+      newChild = document.createElement('div'),
       newName = document.createElement('h3'),
       newUser = document.createElement('p'),
       newLoc = document.createElement('p'),
@@ -67,35 +80,75 @@ function createChild (childTitle) {
       newFlg = document.createElement('p'),
       newBio = document.createElement('p');
   
+  // Appended the childs    
   newCard.append(newImg);    
   newCard.append(newChild);
   newChild.append(newName);
   newChild.append(newUser);
   newChild.append(newLoc);
   newChild.append(newPro);
-  newPro.append(newA);
+  
   newChild.append(newFol);
   newChild.append(newFlg);
   newChild.append(newBio);
   
-  
-  newCard.classList.add('.card');
-  newChild.classList.add('.card-info');
-  newName.classList.add('.name');
-  newUser.classList.add('.username')
+  // Setup the classes
+  newCard.classList.add('card');
+  newChild.classList.add('card-info');
+  newName.classList.add('name');
+  newUser.classList.add('username')
+
+  // Setup the textContent
+  newImg.src = user.data.avatar_url;
+  newName.textContent = user.data.name;
+  newUser.textContent = user.data.login;
+  newLoc.textContent = user.data.location;
+  newPro.textContent = `Profile: `;
+  newA.textContent = user.data.html_url;
+  newA.setAttribute('href', user.data.html_url);
+  newFol.textContent = `Followers: ${user.data.followers}`;
+  newFlg.textContent = `Following: ${user.data.following}`;
+  newBio.textContent = `Bio: ${user.data.bio}`;
+
+  newPro.append(newA);
 
   return newCard;
 }
 
-const newCard = document.querySelector('card')
+// Pulling the parent Element
+const newCard = document.querySelector('.cards')
 
-axios.get('https://api.github.com/users/SethC16')
+// Follower array and forEach with API 
+followersArray.forEach(user => {
+  axios.get('https://api.github.com/users/' + user)
   .then( response => {
-     response.data.forEach( item => {
-       let cardChild = createChild(item);
-       newCard.append(cardChild);
-     })
+      newCard.append(createChild(response))
   })
   .catch( err => {
-    console.log('Nothing to display.');
+    console.log('Nothing to display.', err);
   })
+
+})
+
+// const followers = [];
+
+// axios.get('https://github.com/SethC16/followers')
+//   .then( response => {
+//     response.data.forEach( user => {
+//       followers.push(user.login);
+//     })
+//     followers.forEach( user => {
+//       axios.get('https://api.github.com/users/' + user)
+//       .then(response => {
+//         newCard(createChild(response.data));
+//       })
+//       .catch( err => {
+//         console.log('Nothing to display.', err);
+//       })
+//     })
+//     .catch( err => {
+//       console.log('Nothing to display.', err);
+//     })
+//   })
+  
+ 
