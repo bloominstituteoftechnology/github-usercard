d5dad02015f1ca5e48f5b2eb7123bbb5922dcf34
Zach Stack <stackz.personal@gmail.com>
2020-01-16 14:28:34 -0800

    build functions

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,7 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+// const container = document.querySelector('.container');
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -24,7 +25,77 @@
           user, and adding that card to the DOM.
 */
 
+axios.get("https://api.github.com/users/ZaStack/followers")
+    .then(response => {
+        followersArray.push(response.data);
+        console.log(response);
+    })
+  .catch(error => {
+    console.log("The data borked!!!", error);
+  });
+
+
 const followersArray = [];
+console.log(followersArray)
+
+
+function gitCard(followersArray, index) {
+  const card = document.createElement("div"),
+    img = document.createElement("img"),
+    cardInfo = document.createElement("div"),
+    h3 = document.createElement("h3"),
+    userName = document.createElement("p"),
+    location = document.createElement("p"),
+    profile = document.createElement("p"),
+    profileUrl = document.createElement("a"),
+    followers = document.createElement("p"),
+    following = document.createElement("p"),
+    bio = document.createElement("p");
+
+    card.classList.add("card");
+    cardInfo.classList.add("card-info");
+    h3.classList.add("name");
+    userName.classList.add("username");
+
+    const i = index;
+
+    card.append(img);
+    card.append(cardInfo);
+    cardInfo.append(h3);
+    cardInfo.append(userName);
+    cardInfo.append(location);
+    cardInfo.append(profile);
+    profile.append(profileUrl);
+    cardInfo.append(followers);
+    cardInfo.append(following);
+    cardInfo.append(bio);
+
+    img.src = followersArray[i].avatar_url;
+    h3.textContent = followersArray[i].name;
+    userName.textContent = followersArray[i].login;
+    location.textContent = followersArray[i].location;
+    profileUrl.src = followersArray[i].html_url;
+    profileUrl.textContent = followersArray[i].html_url;
+    followers.textContent = followersArray[i].followers;
+    following.textContent = followersArray[i].following;
+    bio.textContent = followersArray[i].bio;
+
+
+
+  return card;
+
+}
+
+
+const cardContainer = document.querySelector(".cards");
+
+followersArray.forEach(data => {
+  // const newCard = gitCard(data);
+  cardContainer.append(gitCard(data, index));
+  console.log(newCard);
+
+})
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
