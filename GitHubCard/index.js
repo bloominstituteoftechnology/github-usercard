411be91a2ddbdc266ddfef9c79191e6b51a28ac6
Amos Rose <amosroseacct@gmail.com>
2020-01-16 15:17:37 -0700

    assignment complete with stretch challenge

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -45,6 +45,88 @@ const followersArray = [];
 </div>
 
 */
+function cardCreator(obj) {
+  // create elements
+  const card = document.createElement("div"),
+    cardImg = document.createElement("img"),
+    cardInfo = document.createElement("div"),
+    cardName = document.createElement("h3"),
+    cardUserName = document.createElement("p"),
+    cardLocation = document.createElement("p"),
+    cardProfile = document.createElement("p"),
+    cardProfileLink = document.createElement("a"),
+    cardFollowers = document.createElement("p"),
+    cardFollowing = document.createElement("p"),
+    cardBio = document.createElement("p");
+
+  // nest elements
+  card.append(cardImg);
+  card.append(cardInfo);
+  cardInfo.append(cardName);
+  cardInfo.append(cardUserName);
+  cardInfo.append(cardLocation);
+  cardInfo.append(cardProfile);
+  cardInfo.append(cardFollowers);
+  cardInfo.append(cardFollowing);
+  cardInfo.append(cardBio);
+  cardProfile.append(cardProfileLink);
+
+  // add classes to elements
+  card.classList.add("card");
+  cardInfo.classList.add("card-info");
+  cardName.classList.add("name");
+  cardUserName.classList.add("username");
+
+  // pass userd data to card
+  cardImg.src = obj.avatar_url;
+  cardName.textContent = obj.name;
+  cardUserName.textContent = obj.login;
+  cardLocation.textContent = `Location: ${obj.location}`;
+  cardProfile.textContent = "Profile:";
+  cardProfileLink.src = obj.html_url;
+  cardProfileLink.textContent = obj.html_url;
+  cardFollowers.textContent = `Followers: ${obj.followers}`;
+  cardFollowing.textContent = `Following: ${obj.following}`;
+  cardBio.textContent = `Bio: ${obj.bio}`;
+
+  // return DOM element
+  return card;
+}
+
+// select .card element
+const cards = document.querySelector(".cards");
+
+// create new card with my data
+axios
+  .get("https://api.github.com/users/AmMiRo")
+  .then(response => {
+    // console.log(response);
+    cards.append(cardCreator(response.data));
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+//
+axios
+  .get("https://api.github.com/users/AmMiRo/followers")
+  .then(response => {
+    // console.log(response);
+    response.data.forEach(follower => {
+      axios
+        .get(follower.url)
+        .then(followerResponse => {
+          // console.log(followerResponse);
+          cards.append(cardCreator(followerResponse.data));
+        })
+        .catch(followerError => {
+          console.log(followerError);
+        });
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
 /* List of LS Instructors Github username's: 
   tetondan
