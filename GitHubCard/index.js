f15a5ef414e8abedab8f86b39c4b9b82c2e904fd
Jordan Devaney <jordandevaney28@gmail.com>
2020-01-23 23:25:57 -0500

    make github cards

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,55 +1,59 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
+//jshint esversion:6
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+const parent = document.querySelector('.cards');
 
-   Skip to Step 3.
-*/
+//Axios Calls
+axios.get('https://api.github.com/users/devaneyj3').then(res => {
+  let myCard = makeCard(res.data);
+  parent.append(myCard);
+}).catch(err => console.error(err));
 
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
+axios.get('https://api.github.com/users/devaneyj3/followers')
+  .then(res => {
+    res.data.forEach(friend => {
+      parent.appendChild(makeCard(friend));
+    });
+  }).catch(err => { console.error(err); });
 
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
+function makeCard(data) {
+  //create element
+  const card = document.createElement('div');
+  const image = document.createElement('img');
+  const card_info = document.createElement('div');
+  const name = document.createElement('h3');
+  const username = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
 
-const followersArray = [];
+  //append elements
+  card.appendChild(image);
+  card.appendChild(card_info);
+  card_info.appendChild(name);
+  card_info.appendChild(username);
+  card_info.appendChild(location);
+  card_info.appendChild(profile);
+  card_info.appendChild(followers);
+  card_info.appendChild(following);
+  card_info.appendChild(bio);
 
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+  //add classes
+  card.classList.add('card');
+  card_info.classList.add('card-info');
+  name.classList.add('name');
+  username.classList.add('username');
 
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
+  //add textContent, src, innerHTML
+  image.src = data.avatar_url;
+  name.textContent = data.name;
+  username.textContent = data.login;
+  location.textContent = `Location: ${data.location}`;
+  profile.innerHTML = `Profile: <a href="${data.html_url}"> ${data.html_url}</a>`;
+  followers.textContent = `Followers: ${data.followers}`;
+  following.textContent = `Following: ${data.following}`;
+  bio.textContent = `Bio: ${data.bio}`;
 
-*/
-
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+  return card;
+}
