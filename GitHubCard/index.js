0ec02f72e77950658d9aded71efa0d211c38121a
Ash W <amsheppard2016@gmail.com>
2020-01-26 17:51:42 -0500

    done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,11 +1,16 @@
-/* Step 1: using axios, send a GET request to the following URL 
+/* Step 1: using axios, send a GET request to the newFollowing URL 
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+
 axios
     .get("https://api.github.com/users/amsheppard2016")
+
     .then(response => {
-        console.log(response.data);
+        console.log(response);
+        const entryPoint = document.querySelector(".cards");
+        console.log(entryPoint);
+        entryPoint.appendChild(cardCreator(response));
     })
     .catch(error => {
         console.log("data not returned", error);
@@ -23,7 +28,7 @@ axios
 */
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
+          follow this link in your browser https://api.github.com/users/<Your github name>/newFollowers 
           , manually find some other users' github handles, or use the list found 
           at the bottom of the page. Get at least 5 different Github usernames and add them as
           Individual strings to the friendsArray below.
@@ -34,73 +39,97 @@ axios
 
 const followersArray = [
     "HamidAzizy",
-    "SebastianGarces",
     "shanreed",
     "LoganMReber",
-    "dustinmyers"
+    "dustinmyers",
+    "justsml"
 ];
 
+followersArray.forEach(user => {
+    axios
+        .get(`https://api.github.com/users/${user}`)
+
+        .then(response => {
+            console.log(response);
+            const entryPoint = document.querySelector(".cards");
+            console.log(entryPoint);
+            entryPoint.appendChild(cardCreator(response));
+        })
+        .catch(error => {
+            console.log("data not returned", error);
+        });
+});
 /* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+          Using DOM methods and properties, create a component that will return the newFollowing DOM element:
 
 <div class="card">
-  <img src={image url of user} />
+  <img src={image newUrl of user} />
   <div class="card-info">
     <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
+    <p class="newUsername">{users user name}</p>
+    <p>Location: {users newLocation}</p>
     <p>Profile:  
       <a href={address to users github page}>{address to users github page}</a>
     </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
+    <p>Followers: {users newFollowers count}</p>
+    <p>Following: {users newFollowing count}</p>
+    <p>Bio: {users newBio}</p>
   </div>
 </div>
 
 */
-function githubCard(data) {
-    const newImage = document.createElement("img"),
-        newCardInfo = document.createElement("div"),
-        newName = document.createElement("h3"),
-        newUsername = document.createElement("p"),
-        newLocation = document.createElement("p"),
-        newProfile = document.createElement("p"),
-        newAddress = document.createElement("a"),
-        newFollowers = document.createElement("p"),
-        newFollowing = document.createElement("p"),
-        newBio = document.createElement("p");
+const cardCreator = response => {
+    let newCard = document.createElement("div");
+    newCard.classList.add("card");
 
-    newCardInfo.classList.add("card-info");
-    newName.classList.add("name");
-    newUsername.classList.add("username");
-
-    newCardInfo.appendChild(newName);
-    newCardInfo.appendChild(newUsername);
-    newCardInfo.appendChild(newLocation);
-    newCardInfo.appendChild(newProfile);
-    newProfile.appendChild(newAddress);
-    newCardInfo.appendChild(newFollowers);
-    newCardInfo.appendChild(newFollowing);
-    newCardInfo.appendChild(newBio);
-
+    let newImage = document.createElement("img");
     newImage.setAttribute("src", response.data.avatar_url);
+    newCard.appendChild(newImage);
+
+    let newInfoCard = document.createElement("div");
+    newInfoCard.classList.add("card-info");
+    newCard.appendChild(newInfoCard);
+
+    let newName = document.createElement("h3");
     newName.textContent = response.data.name;
+    newName.classList.add("name");
+    newInfoCard.appendChild(newName);
+
+    let newUsername = document.createElement("p");
     newUsername.textContent = response.data.login;
-    newLocation.textContent = `Location: ${response.data.location}`;
-    newProfile.textContent = "Profile:";
-    //newAddress.textContent=`${response.data.html_url}`;
-    newAddress.setAttribute("href", `${response.data.html_url}`);
+    newUsername.classList.add("username");
+    newInfoCard.appendChild(newUsername);
+
+    let newLocation = document.createElement("p");
+    newLocation.textContent = `Location:  ${response.data.location}`;
+    newInfoCard.appendChild(newLocation);
+
+    let newProfile = document.createElement("p");
+    newProfile.textContent = "Profile:  ";
+    newInfoCard.appendChild(newProfile);
+
+    let newUrl = document.createElement("a");
+    newUrl.textContent = `${response.data.html_url}`;
+    newUrl.setAttribute("href", response.data.html_url);
+    newUrl.setAttribute("target", "_blank");
+    newProfile.appendChild(newUrl);
+
+    let newFollowers = document.createElement("p");
     newFollowers.textContent = `Followers: ${response.data.followers}`;
+    newInfoCard.appendChild(newFollowers);
+
+    let newFollowing = document.createElement("p");
     newFollowing.textContent = `Following: ${response.data.following}`;
+    newInfoCard.appendChild(newFollowing);
+
+    let newBio = document.createElement("p");
     newBio.textContent = `Bio: ${response.data.bio}`;
+    newInfoCard.appendChild(newBio);
 
-    return newImage;
-}
+    return newCard;
+};
 
-const entryPoint = document.querySelector(".cards");
-console.log(entryPoint);
-/* List of LS Instructors Github username's: 
+/* List of LS Instructors Github newUsername's: 
   tetondan
   dustinmyers
   justsml
