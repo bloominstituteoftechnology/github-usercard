21a715414a30ac0df43744e00f29e5ed2836853c
Sara Reidy <sara.reidy.21@gmail.com>
2020-02-11 11:13:03 -0500

    Finished dynamically adding followers

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,17 +3,78 @@
            https://api.github.com/users/<your name>
 */
 let cardParent = document.querySelector('.cards')
+//MVP
+//const followersArray = ['SandersForPresident', 'techforwarren', 'tetondan', 'dustinmyers', 'justsml' ];
 
 
+// axios.get('https://api.github.com/users/reidysj')
+//   .then(response =>{
+//     let card = cardMaker(response.data);
+//     cardParent.prepend(card);
+//   })
+//   .catch( error => {
+//     console.log('Error: ', error);
+//   });
 
-axios.get('https://api.github.com/users/reidysj')
+//   followersArray.forEach(follower => {
+//     axios.get(`https://api.github.com/users/${follower}`)
+//     .then(response =>{
+//       let card = cardMaker(response.data);
+//       cardParent.append(card);
+//     })
+//     .catch(error => {
+//       console.log('Error: ', error);
+//     });
+//   })
+
+//Stretch
+axios.get('https://api.github.com/users/tetondan')
   .then(response =>{
     let card = cardMaker(response.data);
     cardParent.prepend(card);
+    return axios.get(`${response.data.followers_url}`);
   })
-  .catch( error => {
-    console.log('Error: ', error);
-  });
+  .then(getFollowers => {
+    getFollowers.data.forEach(follower => {
+      let followerCard = cardMaker(follower);
+      cardParent.append(followerCard);
+    })
+  })
+  .catch(error => {
+    console.log('Error code: ', error);
+  })
+
+  function cardMaker(obj){
+    let cardDiv = document.createElement('div');
+    cardDiv.classList.add('card');
+    let userPic = document.createElement('img');
+    userPic.src = obj.avatar_url;
+    let infoDiv = document.createElement('div');
+    infoDiv.classList.add('card-info');
+    let name = document.createElement('h3');
+    name.classList.add('name');
+    name.textContent = obj.name;
+    let userName = document.createElement('p');
+    userName.classList.add('username');
+    userName.textContent = obj.login;
+    let location = document.createElement('p');
+    location.textContent = `Location: ${obj.location}`;
+    let profile = document.createElement('p');
+    profile.textContent = `Profile: `;
+    let linkToProfile = document.createElement('a');
+    linkToProfile.href = obj.html_url;
+    linkToProfile.textContent = obj.html_url;
+    profile.append(linkToProfile);
+    let followers = document.createElement('p');
+    followers.textContent = `Followers: ${obj.followers}`;
+    let following = document.createElement('p');
+    following.textContent = `Following: ${obj.following}`;
+    let bio = document.createElement('p');
+    bio.textContent = obj.bio;
+    infoDiv.append(name, userName, location, profile, followers, following, bio);
+    cardDiv.append(userPic, infoDiv);
+    return cardDiv
+  }
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -35,17 +96,8 @@ axios.get('https://api.github.com/users/reidysj')
           user, and adding that card to the DOM.
 */
 
-const followersArray = ['SandersForPresident', 'techforwarren', 'tetondan', 'dustinmyers', 'justsml' ];
-followersArray.forEach(follower => {
-  axios.get(`https://api.github.com/users/${follower}`)
-  .then(response =>{
-    let card = cardMaker(response.data);
-    cardParent.append(card);
-  })
-  .catch(error => {
-    console.log('Error: ', error);
-  });
-})
+
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -66,37 +118,7 @@ followersArray.forEach(follower => {
 </div>
 
 */
-function cardMaker(obj){
-  let cardDiv = document.createElement('div');
-  cardDiv.classList.add('card');
-  let userPic = document.createElement('img');
-  userPic.src = obj.avatar_url;
-  let infoDiv = document.createElement('div');
-  infoDiv.classList.add('card-info');
-  let name = document.createElement('h3');
-  name.classList.add('name');
-  name.textContent = obj.name;
-  let userName = document.createElement('p');
-  userName.classList.add('username');
-  userName.textContent = obj.login;
-  let location = document.createElement('p');
-  location.textContent = `Location: ${obj.location}`;
-  let profile = document.createElement('p');
-  profile.textContent = `Profile: `;
-  let linkToProfile = document.createElement('a');
-  linkToProfile.href = obj.url;
-  linkToProfile.textContent = obj.url;
-  profile.append(linkToProfile);
-  let followers = document.createElement('p');
-  followers.textContent = `Followers: ${obj.followers}`;
-  let following = document.createElement('p');
-  following.textContent = `Following: ${obj.following}`;
-  let bio = document.createElement('p');
-  bio.textContent = obj.bio;
-  infoDiv.append(name, userName, location, profile, followers, following, bio);
-  cardDiv.append(userPic, infoDiv);
-  return cardDiv
-}
+
 
 /* List of LS Instructors Github username's: 
   tetondan
