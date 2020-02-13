fc17e52f3bf2a372869e390f41dafa2b24a3e060
Laien Xie <angela@Laiens-MacBook-Air.local>
2020-02-13 15:20:16 -0800

    done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,15 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios
+  .get("https://api.github.com/users/angela-laien")
+  .then(response => {
+  console.log(response);
+  cardPoint.append(userCard(response.data));
+})
+  .catch(error => {
+  console.log("the data was not returned", error);
+});
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -23,8 +32,32 @@
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
+axios
+  .get("https://api.github.com/users/angela-laien/followers")
+  .then(response => {
+  //console.log(response);  
+  const followersArray = response.data;
+  followersArray.forEach(follower => {
+    //console.log(follower.login);
+  axios
+    .get(`https://api.github.com/users/${follower.login}`)
+    .then(response => {
+      cardPoint.append(userCard(response.data))
+    })
+  })
+})
+  .catch(error => {
+  console.log("the data was not returned", error);
+});
 
-const followersArray = [];
+/*const followersArray = [
+  "Pergamene",
+  "ReBarrington",
+  "fbzr",
+  "adriannasaruk",
+  "Michael-B1764",
+  "afialydia"
+]; */
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -53,3 +86,47 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+function userCard(obj) {
+  const card = document.createElement('div');
+  const image = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const username = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const address = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+  
+  card.appendChild(image);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  profile.appendChild(address);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  username.classList.add('username');
+  
+  image.src = obj.avatar_url;
+  name.textContent = obj.name;
+  username.textContent = obj.login;
+  location.textContent = `Location: ${obj.location}`;
+  profile.textContent = `Profile: ${obj.url}`;
+  address.textContent = obj.url;
+  followers.textContent = `Followers: ${obj.followers}`;
+  following.textContent = `Folllowing: ${obj.following}`;
+  bio.textContent = `Bio: ${obj.bio}`;
+
+   return card;
+ }
+ 
+const cardPoint = document.querySelector('.cards');
