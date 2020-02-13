faa2344f94694473b85e117d17e0ae8355fc2608
emma <sonicjane2020@gmail.com>
2020-02-13 12:45:07 -0800

    Finished step 1, works well, commented it out as to not get owned by the call limit

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,18 @@
            https://api.github.com/users/<your name>
 */
 
+// API GET REQUEST //
+
+/* 
+~~~~~~~~ Commented out so I don't get limited ~~~~~~~~
+
+axios.get("https://api.github.com/users/sonicemma").then(response => {
+  console.log(response.data);
+});
+
+~~~~~~~~ COMMENT OUT ENDS HERE ~~~~~~~~ 
+*/
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
