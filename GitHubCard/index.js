3f5cb5d1644d7e74c58118cda9bc04c334c46337
Teague Hannam <teaguehannam@gmail.com>
2020-02-13 17:03:19 -0700

    Finished MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,103 +1,4 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
-
-
-const htmlCards = document.querySelector(".cards");
-
-//Get own profile
-axios.get("https://api.github.com/users/teaguehannam")
-.then(response => {
-    console.log(response);
-    //Append card
-    htmlCards.appendChild(followerCard(response));
-})
-.catch(error => {
-    console.log("Error: ", error);
-});
-
-
-function followerCard(obj){
-
-  //main object
-  const card = document.createElement('div');
-  card.classList.add("card");
-
-  //img
-  const photo = document.createElement("img");
-  photo.setAttribute("src", obj.data.avatar_url);
-
-  //card-info div
-  const cardInfo = document.createElement("div");
-  cardInfo.classList.add("card-info");
-  card.appendChild(cardInfo);
-
-  const name = document.createElement("h3");
-  name.classList.add("name");
-
-  const username = document.createElement("p");
-  username.classList.add("username");
-
-  const location = document.createElement("p");
-  location.textContent = `Location: ${obj.data.location}`;
-  card.appendChild(location);
-
-  const profile = document.createElement("p");
-
-
-  const link = document.createElement("a");
-
-
-  const followers = document.createElement("p");
-
-
-  const following = document.createElement("p");
-
-
-  const bio = document.createElement("p");
-
-  //car-info div
-
-
-
-  //add to card info conainter 
-
-  //add to container
-
-
-
-
-
-
-  return card;
-}
-
-
-//Get Follow Profiles
-const followersArray = [
-  "XueYingLin",
-  "ScottSmith23",
-  "bergeronmatt",
-  "mmussel",
-  "landoDev",
-  "AlexandroM1234"
-];
-
-followersArray.forEach(follower => {
-  axios.get(`https://api.github.com/users/${follower}`)
-  .then(response => {
-      console.log(response);
-      //Append card
-      htmlCards.appendChild(followerCard(response));
-  })
-  .catch(error => {
-      console.log("Error: ", error);
-  });
-})
-
-
-/*
+/* HTML structure
 <div class="card">
   <img src={image url of user} />
   <div class="card-info">
@@ -113,3 +14,95 @@ followersArray.forEach(follower => {
   </div>
 </div>
 */
+
+const followersArray = [
+  "XueYingLin",
+  "ScottSmith23",
+  "bergeronmatt",
+  "mmussel",
+  "landoDev",
+  "AlexandroM1234"
+];
+
+
+//Location where cards will be added
+const htmlCards = document.querySelector(".cards");
+
+function followerCard(obj){
+  //main object
+  const card = document.createElement('div');
+  card.classList.add("card");
+
+  //img
+  const photo = document.createElement("img");
+  photo.setAttribute("src", obj.data.avatar_url);
+  card.appendChild(photo);
+
+  //card-info div
+  const cardInfo = document.createElement("div");
+  cardInfo.classList.add("card-info");
+  card.appendChild(cardInfo);
+
+  const name = document.createElement("h3");
+  name.textContent = obj.data.name;
+  name.classList.add("name");
+  cardInfo.appendChild(name);
+
+  const username = document.createElement("p");
+  username.textContent = obj.data.login;
+  username.classList.add("username");
+  cardInfo.appendChild(username);
+
+  const location = document.createElement("p");
+  location.textContent = `Location: ${obj.data.location}`;
+  card.appendChild(location);
+
+  const profile = document.createElement("p");
+  profile.textContent = "Profile: ";
+  cardInfo.appendChild(profile);
+
+  const link = document.createElement("a");
+  link.textContent = obj.data.html_url;
+  link.setAttribute("href", obj.data.html_url);
+  profile.appendChild(link);
+
+  const followers = document.createElement("p");
+  followers.textContent = `Followers: ${obj.data.followers}`;
+  cardInfo.appendChild(followers);
+
+  const following = document.createElement("p");
+  following.textContent = `Following: ${obj.data.following}`;
+  cardInfo.appendChild(following);
+
+  const bio = document.createElement("p");
+  bio.textContent = `Bio: ${obj.data.bio}`;
+  cardInfo.appendChild(bio);
+
+  //console.log(card);
+  return card;
+}
+
+
+//Create card of own profile
+axios.get("https://api.github.com/users/teaguehannam")
+.then(response => {
+    //Append card
+    htmlCards.appendChild(followerCard(response));
+})
+.catch(error => {
+    console.log("Error: ", error);
+});
+
+//Create cards from followers in array
+followersArray.forEach(follower => {
+  axios.get(`https://api.github.com/users/${follower}`)
+  .then(response => {
+      //Append card
+      htmlCards.appendChild(followerCard(response));
+  })
+  .catch(error => {
+      console.log("Error: ", error);
+  });
+})
+
+
