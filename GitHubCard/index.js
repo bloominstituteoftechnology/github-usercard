26096107c1ccba89a025296c42e959b51810a604
Ryan Erickson <rerickson21@yahoo.com>
2020-02-20 13:52:24 -0500

    MVP & Stretch done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,15 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+axios
+  .get("https://api.github.com/users/Ryan-Erickson")
+  .then(response => {
+  console.log(response);
+  cardPoint.append(userCard(response.data));
+})
+  .catch(error => {
+  console.log("the data was not returned", error);
+});
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -23,8 +31,6 @@
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
-
-const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -53,3 +59,100 @@ const followersArray = [];
   luishrd
   bigknell
 */
+function gitCards(data) {
+  function checkIfNull(str) {
+    if (str) return str;
+    return '';
+  }
+  const newCard = document.createElement('div'),
+    newImg = document.createElement('img'),
+    newCardInfo = document.createElement('div'),
+    name = document.createElement('h3'),
+    username = document.createElement('p'),
+    location = document.createElement('p'),
+    profile = document.createElement('p'),
+    profileUrl = document.createElement('a'),
+    followers = document.createElement('p'),
+    following = document.createElement('p'),
+    bio = document.createElement('p');
+
+  //setting content
+  newImg.src = data.avatar_url;
+  name.textContent = data.name;
+  username.textContent = data.login;
+  location.textContent = `Location: ${checkIfNull(data.location)}`;
+  profile.textContent = `Profile: ${checkIfNull(data.name)}`;
+  profileUrl.textContent = `Link: ${data.html_url}`;
+  followers.textContent = `Followers: ${checkIfNull(data.followers)}`;
+  following.textContent = `Following: ${checkIfNull(data.following)}`;
+  bio.textContent = `Bio: ${checkIfNull(data.bio)}`;
+
+  //Create Structure and append to the DOM
+
+  newCard.appendChild(newImg);
+  newCard.appendChild(newCardInfo);
+  newCardInfo.appendChild(name);
+  newCardInfo.appendChild(username);
+  newCardInfo.appendChild(location);
+  newCardInfo.appendChild(profile);
+  newCardInfo.appendChild(followers);
+  newCardInfo.appendChild(following);
+  newCardInfo.appendChild(bio);
+  newCardInfo.appendChild(profileUrl);
+
+  //applying styles and classes
+
+  newCard.classList.add('card');
+  newCardInfo.classList.add('card-info');
+  name.classList.add('name');
+  username.classList.add('username');
+
+  // event handlers
+
+  return newCard;
+}
+// setup the Array
+
+const entryPoint = document.querySelector('.cards');
+
+// this is my gitCard
+
+const cards = document.querySelector('.cards');
+
+axios.get('https://api.github.com/users/Ryan-Erickson').then(response => {
+  console.log(response.data);
+  cards.appendChild(gitCards(response.data));
+});
+
+// this is follower git Card they gave us changed with my own followers
+
+const followersArray = [
+  'MTaylor-tech',
+  'lela23',
+  'xpinero',
+  'SebastianGarces',
+];
+
+followersArray.forEach(follower => {
+  axios.get(`https://api.github.com/users/${follower}`)
+    .then(response => {
+      console.log(response);
+      cards.append(gitCards(response.data));
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+
+//My Follower's Cards
+// axios.get(`https://api.github.com/users/Ryan-Erickson/followers`)
+//   .then(response => {
+//     response.data.forEach(element => {
+//       axios.get(element.login);
+//       const newGitUserCard = new gitCards(element);
+//       cards.appendChild(newGitUserCard);
+//       console.log(element);
+//     });
+//   })
+
