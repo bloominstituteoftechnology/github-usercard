00012fa57cd596a85ed4df7487b940d7f8fea957
Victoria  Topham <topham.victoria@gmail.com>
2020-03-10 15:18:38 -0700

    got the API working, setting up the component function

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,14 @@
            https://api.github.com/users/<your name>
 */
 
+axios.get("https://api.github.com/users/vtopham")
+  .then(response => {
+    console.log(response);
+  });
+
+
+
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -10,9 +18,13 @@
    Skip to Step 3.
 */
 
+
+
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
+
+
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
@@ -26,6 +38,38 @@
 
 const followersArray = [];
 
+function createCard(inputObj) {
+
+  //create div
+  let card = document.createElement("div");
+  card.classList.add("card");
+      //create img
+      let userImg = document.createElement("img");
+      userImg.src = inputObj.avatar_url;
+      card.appendChild(userImg);
+      //create card info div
+      let cardInfo = document.createElement("div");
+      cardInfo.classList.add("card-info");
+      card.appendChild(cardInfo);
+          //create h3
+          let name = document.createElement("h3");
+          name.classList.add("name");
+          name.textContent = inputObj.name;
+          //create p
+          let username = document.createElement("p");
+          
+          //create p
+
+          //create a
+
+          //create p
+
+              
+
+          
+
+
+}
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
@@ -53,3 +97,41 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+
+
+
+// MY DATA 
+
+
+// login: "vtopham"
+// id: 49505499
+// node_id: "MDQ6VXNlcjQ5NTA1NDk5"
+// avatar_url: "https://avatars0.githubusercontent.com/u/49505499?v=4"
+// gravatar_id: ""
+// url: "https://api.github.com/users/vtopham"
+// html_url: "https://github.com/vtopham"
+// followers_url: "https://api.github.com/users/vtopham/followers"
+// following_url: "https://api.github.com/users/vtopham/following{/other_user}"
+// gists_url: "https://api.github.com/users/vtopham/gists{/gist_id}"
+// starred_url: "https://api.github.com/users/vtopham/starred{/owner}{/repo}"
+// subscriptions_url: "https://api.github.com/users/vtopham/subscriptions"
+// organizations_url: "https://api.github.com/users/vtopham/orgs"
+// repos_url: "https://api.github.com/users/vtopham/repos"
+// events_url: "https://api.github.com/users/vtopham/events{/privacy}"
+// received_events_url: "https://api.github.com/users/vtopham/received_events"
+// type: "User"
+// site_admin: false
+// name: null
+// company: null
+// blog: ""
+// location: null
+// email: null
+// hireable: null
+// bio: null
+// public_repos: 23
+// public_gists: 0
+// followers: 1
+// following: 1
+// created_at: "2019-04-11T02:49:03Z"
+// updated_at: "2020-03-10T21:37:09Z"
