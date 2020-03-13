5451d068d57618e10519463cab87505164cb48f4
Enclosed Letter <C.LuisMartinez.A@gmail.com>
2020-03-12 17:42:23 -0700

    done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,11 +2,11 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-axios
-  .get("https://api.github.com/users/EnclosedLetter")
-  .then(response => {
-    console.log(response);
-    });
+// axios
+//   .get("https://api.github.com/users/EnclosedLetter")
+//   .then(response => {
+//     console.log(response);
+//     });
 
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
@@ -30,7 +30,8 @@ axios
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ["enclosedletter", "tetondan", "dustinmyers", "justsml", "luishrd", "bigknell" ];
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -95,24 +96,36 @@ urlGit.href = apiData.data.url;
 // add content
 name.textContent = apiData.data.name;
 userName.textContent = apiData.data.login;
-
 const noLocation = () => {
   if(apiData.data.location === null)
   return location.textContent = 'Location: uknown location';
   return "Location " + location.textContent;}
   noLocation();
-profile.textContent = "profile: " + apiData.data.html_url;
-
+profile.textContent = "Profile: " + apiData.data.html_url;
+followers.textContent = "Followers: " + apiData.data.followers;
+following.textContent = "Following: " + apiData.data.following;
+// bio.textContent = "Bio: " + apiData.data.bio;
+const noBio = () => {
+  if(apiData.data.bio === null)
+  return bio.textContent = 'Bio: no-bio';
+  return "Bio: " + bio.textContent;}
+  noBio();
 
 return card;
 }
 
-const cardDiv = document.querySelector(".cards");
+const cardDiv = document.querySelector(".cards"); //Hooking up the div
 
 // cardDiv.append(cardCreator());
 
-axios 
-  .get("https://api.github.com/users/EnclosedLetter")
-  .then(response => {
-    cardDiv.append(cardCreator(response));
-  })
+// axios 
+//   .get("https://api.github.com/users/EnclosedLetter")
+//   .then(response => {
+//     cardDiv.append(cardCreator(response));
+//   })
+
+followersArray.forEach(item=>{
+  axios 
+  .get(`https://api.github.com/users/${item}`)
+  .then(response=> cardDiv.append(cardCreator(response) )
+  )})
