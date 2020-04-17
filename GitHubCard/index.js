02a4ce78db4108f16d2cf1f2d959d91179f85065
TonySo88 <60905365+TonySo88@users.noreply.github.com>
2020-04-16 20:56:30 -0500

    changes made

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,18 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+const cards = document.querySelector(".cards");
+
+axios.get("https://api.github.com/users/tonyso88")
+.then(response => {
+  // response.data.forEach((object) => {
+    const newUserCard = cardCreator(response.data)
+    cards.appendChild(newUserCard)
+  // })
+})
+.catch( error => {
+  console.log(error);
+})
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -24,7 +36,7 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ["tetondan", "dustinmyers", "justsml", "luishrd", "bigknell"];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -53,3 +65,53 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+function cardCreator(object) {
+  const card = document.createElement('div')
+  const image = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const name = document.createElement('h3')
+  const username = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p')
+  const githubPage = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
+
+  card.classList.add('card')
+  image.src = object.avatar_url
+  cardInfo.classList.add('card-info')
+  name.classList.add('name')
+  name.textContent = object.name
+  username.textContent = object.login
+  location.textContent = `Location: ${object.location}`
+  profile.textContent = 'Profile: '
+  githubPage.href = 'https://github.com/TonySo88'
+  followers.textContent = `Followers: ${object.followers}`
+  following.textContent = `Following: ${object.following}`
+  bio.textContent = `Bio: ${object.bio}`
+
+  profile.appendChild(githubPage)
+  cardInfo.appendChild(name)
+  cardInfo.appendChild(username)
+  cardInfo.appendChild(location)
+  cardInfo.appendChild(profile)
+  cardInfo.appendChild(followers)
+  cardInfo.appendChild(following)
+  cardInfo.appendChild(bio)
+  card.appendChild(image)
+  card.appendChild(cardInfo)
+
+  return card
+}
+
+followersArray.forEach( item => {
+axios.get(`https://api.github.com/users/${item}`)
+.then(response => {
+    const newUserCard = cardCreator(response.data);
+    cards.appendChild(newUserCard)
+  })
+.catch( error => {
+  console.log(error) })
+})
