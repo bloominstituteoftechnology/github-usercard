fbec26e051674ad094e2ef7e9f241e4be7c51e82
Ryan <mersmannryan@gmail.com>
2020-04-16 21:21:18 -0600

    MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,16 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+let cards = document.querySelector('.cards');
+
+axios.get('https://api.github.com/users/RyanMersmann')
+  .then(response => {
+  const newCard = githubCards(response);
+  cards.appendChild(newCard);
+  });
+
+  console.log(axios.get('https://api.github.com/users/RyanMersmann'));
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -24,7 +34,24 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  `https://api.github.com/users/tetondan`,
+  `https://api.github.com/users/dustinmyers`,
+  `https://api.github.com/users/justsml`,
+  `https://api.github.com/users/luishrd`,
+  `https://api.github.com/users/bigknell`,
+  `https://api.github.com/users/facebook`,
+  'https://api.github.com/users/addyosmani'
+];
+
+followersArray.forEach(link =>{
+  axios.get(link)
+  .then(response => {
+   //console.log(response);
+   const newCard = githubCards(response);
+   cards.appendChild(newCard);
+   });
+ });
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +72,50 @@ const followersArray = [];
 </div>
 
 */
+
+function githubCards (obj) {
+
+  //create the elements
+  const userCard = document.createElement('div'),
+        userImage = document.createElement('img'),
+        cardInfo = document.createElement('div'),
+        name = document.createElement('h3'),
+        username = document.createElement('p'),
+        userLocation = document.createElement('p'),
+        userProfile = document.createElement('p'),
+        profileLink = document.createElement('a'),
+        userFollowers = document.createElement('p'),
+        userFollowing = document.createElement('p'),
+        userBio = document.createElement('p');
+
+        
+  userCard.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  username.classList.add('username');
+
+  userImage.src = obj.data['avatar_url'];
+  name.textContent = obj.data.name;
+  username.textContent = obj.data.login;
+  location.textContent = obj.data.location;
+  profileLink.href = obj.data['html_url'];
+  userFollowers.textContent = obj.data.followers;
+  userFollowing.textContent = obj.data.following;
+  userBio.textContent = obj.data.bio;
+
+  userCard.appendChild(userImage);
+  userCard.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(userLocation);
+  cardInfo.appendChild(userProfile);
+  cardInfo.appendChild(userFollowers);
+  cardInfo.appendChild(userFollowing);
+  cardInfo.appendChild(userBio);
+  userProfile.appendChild(profileLink);
+
+  return userCard; 
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
--- README.md
+++ README.md
@@ -35,7 +35,7 @@ In this project we are going to be accessing the GitHub API and building a socia
 
 ### Preprocessor Setup
 
-  * None. We have gone ahead and taken care of this step for you. Focus on the Javascript 😁.
+  * None. We have gone ahead and taken care of this step for you. Focus on the Javascript.
 
 ### Part 1: Requesting Data from the GitHub API
 * Include the script element linking the `axios` library in your HTML. If you do not remember the code you can find it here: https://github.com/axios/axios
