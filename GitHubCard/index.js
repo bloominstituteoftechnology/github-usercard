df38605e4f91c73e6cc50e22a9d3b8a77d43d608
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-04-16 21:34:08 -0400

    finished 3

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,14 @@
            https://api.github.com/users/<your name>
 */
 
+
+
+
+  axios.get('https://api.github.com/users/jcharp')
+    .then(function (response) {
+      console.log(response);
+    })
+    
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -45,6 +53,45 @@ const followersArray = [];
 </div>
 
 */
+
+function people(){
+  const card = document.createElement('div');
+  const image =document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const userName = document.createElement('p');
+  const locaction = document.createElement('p');
+  const profile = document.createElement('p');
+  const anchor = document.createElement('a');
+  const followers = document.createElement('a');
+  const following = document.createElement('a');
+  const bio = document.createElement('p');
+
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  userName.classList.add('username');
+
+
+  card.appendChild(image);
+  card.appendChild('cardInfo');
+  cardInfo.appendChild('name');
+  cardInfo.appendChild('userName');
+  cardInfo.appendChild('location');
+  cardInfo.appendChild('profile');
+  profile.appendChild('anchor');
+  cardInfo.appendChild('followers');
+  cardInfo.appendChild('following');
+  cardInfo.appendChild('bio');
+
+
+
+//return people();
+
+
+}
+
+console.log(people);
 
 /* List of LS Instructors Github username's: 
   tetondan
