2998d98aa439a7666cc84a225605438a9a098ec4
arn-foto <38259824+arn-foto@users.noreply.github.com>
2020-05-08 01:03:21 -0700

    In progress

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,26 +4,24 @@
     https://api.github.com/users/<your name>
 */
 
-
-axios
-  .get('https://api.github.com/users/arn-foto')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
+// axios
+//   .get('https://api.github.com/users/arn-foto')
+//   .then(function (response) {
+//     // handle success
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .finally(function () {
+//     // always executed
+//   });
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
     data in order to use it to build your component function
-
     Skip to STEP 3.
 */
 
@@ -38,40 +36,31 @@ axios
     manually find some other users' github handles, or use the list found at the
     bottom of the page. Get at least 5 different Github usernames and add them as
     Individual strings to the friendsArray below.
-
     Using that array, iterate over it, requesting data for each user, creating a new card for each
     user, and adding that card to the DOM.
 */
 const userCards = document.querySelector('.cards');
 
-const followersArray = [
-  'jvaldezdipre',
-  'tetondan',
-  'dustinmyers',
-  'justsml',
-  'luishrd',
-  'bigknell',
-];
+const followersArray = [ 'jvaldezdipre', 'tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell' ];
 
 const getUser = () => {
-  followersArray.forEach(item => {
-    axios
-      .get(`https://api.github.com/users/${item}`)
-      .then(res => {
-        console.log(res.data);
-        userCards.appendChild(createUserCard(res.data));
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
+	followersArray.forEach((item) => {
+		axios
+			.get(`https://api.github.com/users/${item}`)
+			.then((res) => {
+				console.log(res.data);
+				userCards.appendChild(createUserCard(res.data));
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	});
 };
 
 getUser();
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
-
     <div class="card">
       <img src={image url of user} />
       <div class="card-info">
@@ -97,50 +86,50 @@ getUser();
     bigknell
 */
 
-const createUserCard = object => {
-  //create each element
-  const userCard = document.createElement('div');
-  const userImage = document.createElement('img');
-  const cardInfo = document.createElement('div');
-  const name = document.createElement('h3');
-  const userName = document.createElement('p');
-  const location = document.createElement('p');
-  const profile = document.createElement('p');
-  const link = document.createElement('a');
-  const followers = document.createElement('p');
-  const following = document.createElement('p');
-  const bio = document.createElement('p');
+const createUserCard = (object) => {
+	//create each element
+	const userCard = document.createElement('div');
+	const userImage = document.createElement('img');
+	const cardInfo = document.createElement('div');
+	const name = document.createElement('h3');
+	const userName = document.createElement('p');
+	const location = document.createElement('p');
+	const profile = document.createElement('p');
+	const link = document.createElement('a');
+	const followers = document.createElement('p');
+	const following = document.createElement('p');
+	const bio = document.createElement('p');
 
-  //Add class
-  userCard.classList.add('card');
-  cardInfo.classList.add('card-info');
-  name.classList.add('name');
-  userName.classList.add('username');
+	//Add class
+	userCard.classList.add('card');
+	cardInfo.classList.add('card-info');
+	name.classList.add('name');
+	userName.classList.add('username');
 
-  //Add the meanings
-  userImage.src = object.avatar_url;
-  name.textContent = object.name;
-  userName.textContent = object.login;
-  location.textContent = `Location: ${object.location}`;
-  profile.textContent = 'Profile:';
-  link.textContent = object.html_url;
-  link.href = object.html_url;
-  followers.textContent = `Followers: ${object.followers}`;
-  following.textContent = `Following: ${object.following}`;
-  bio.textContent = `Bio: ${object.bio}`;
+	//Add the meanings
+	userImage.src = object.avatar_url;
+	name.textContent = object.name;
+	userName.textContent = object.login;
+	location.textContent = `Location: ${object.location}`;
+	profile.textContent = 'Profile:';
+	link.textContent = object.html_url;
+	link.href = object.html_url;
+	followers.textContent = `Followers: ${object.followers}`;
+	following.textContent = `Following: ${object.following}`;
+	bio.textContent = `Bio: ${object.bio}`;
 
-  //Add each child to its parent
-  cardInfo.appendChild(name);
-  cardInfo.appendChild(userName);
-  cardInfo.appendChild(location);
-  cardInfo.appendChild(profile);
-  cardInfo.appendChild(followers);
-  cardInfo.appendChild(following);
-  cardInfo.appendChild(bio);
-  profile.appendChild(link);
+	//Add each child to its parent
+	cardInfo.appendChild(name);
+	cardInfo.appendChild(userName);
+	cardInfo.appendChild(location);
+	cardInfo.appendChild(profile);
+	cardInfo.appendChild(followers);
+	cardInfo.appendChild(following);
+	cardInfo.appendChild(bio);
+	profile.appendChild(link);
 
-  userCard.appendChild(userImage);
-  userCard.appendChild(cardInfo);
+	userCard.appendChild(userImage);
+	userCard.appendChild(cardInfo);
 
-  return userCard;
+	return userCard;
 };
