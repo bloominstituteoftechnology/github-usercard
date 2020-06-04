cf0d88feaa8b9a552a161c90a0d2329cc86d6dd6
Mark Pekun <markpekun@marks-mbp.lan>
2020-06-04 15:31:37 -0400

    step 1 + 2 complete

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,6 +4,15 @@
     https://api.github.com/users/<your name>
 */
 
+axios
+  .get("https://api.github.com/users/slightflow")
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log;
+  }, []);
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
