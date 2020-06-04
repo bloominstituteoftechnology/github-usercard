c0e540b760e56344c8192a60e3578daa4344d75a
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-06-04 14:18:19 -0600

    wip

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,43 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+// const data = axios.get('https://api.github.com/users/BlueEagle')
+// console.log(data)
+
+const data = {
+  "login": "BlueEagle",
+  "id": 6562992,
+  "node_id": "MDQ6VXNlcjY1NjI5OTI=",
+  "avatar_url": "https://avatars0.githubusercontent.com/u/6562992?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/BlueEagle",
+  "html_url": "https://github.com/BlueEagle",
+  "followers_url": "https://api.github.com/users/BlueEagle/followers",
+  "following_url": "https://api.github.com/users/BlueEagle/following{/other_user}",
+  "gists_url": "https://api.github.com/users/BlueEagle/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/BlueEagle/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/BlueEagle/subscriptions",
+  "organizations_url": "https://api.github.com/users/BlueEagle/orgs",
+  "repos_url": "https://api.github.com/users/BlueEagle/repos",
+  "events_url": "https://api.github.com/users/BlueEagle/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/BlueEagle/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "Collin Ballou",
+  "company": null,
+  "blog": "Balloucollin.dev",
+  "location": "Utah",
+  "email": null,
+  "hireable": true,
+  "bio": "Writing many things.\r\n\r\nStudent at Lambda School.\r\n\r\nPython is my side hustle.",
+  "twitter_username": null,
+  "public_repos": 37,
+  "public_gists": 2,
+  "followers": 2,
+  "following": 15,
+  "created_at": "2014-02-01T18:28:57Z",
+  "updated_at": "2020-06-04T19:04:19Z"
+}
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -49,6 +86,51 @@ const followersArray = [];
       </div>
     </div>
 */
+function createComponent(componentData) {
+  const { avatar_url, name, login, location, html_url, followers, following, bio } = componentData
+
+  const card = document.createElement('div')
+  const userImage = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const nameEl = document.createElement('h3')
+  const username = document.createElement('p')
+  const locationEl = document.createElement('p')
+  const profile = document.createElement('p')
+  const address = document.createElement('a')
+  const followersEl = document.createElement('p')
+  const followingEl = document.createElement('p')
+  const bioEl = document.createElement('p')
+
+  card.classList.add('card')
+  cardInfo.classList.add('card-info')
+  nameEl.classList.add('name')
+  username.classList.add('username')
+
+  userImage.src = avatar_url
+  nameEl.textContent = name
+  username.textContent = login
+  locationEl.textContent = `Location: ${location}`
+  profile.textContent = 'Profile: '
+  address.href = html_url
+  address.textContent = html_url
+  followersEl.textContent = `Followers: ${followers}`
+  followingEl.textContent = `Following: ${following}`
+  bioEl.textContent = `Bio: ${bio}`
+
+  card.appendChild(userImage)
+  card.appendChild(cardInfo)
+  cardInfo.appendChild(nameEl)
+  cardInfo.appendChild(username)
+  cardInfo.appendChild(locationEl)
+  cardInfo.appendChild(profile)
+  profile.appendChild(address)
+  cardInfo.appendChild(followersEl)
+  cardInfo.appendChild(followingEl)
+  cardInfo.appendChild(bioEl)
+
+  return card
+}
+// const component = createComponent(data)
 
 /*
   List of LS Instructors Github username's:
