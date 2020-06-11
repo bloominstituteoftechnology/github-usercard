cd5b7863f7411510b58bd4561a07b5d8073d371f
Melanie Mendoza <melanie.mendoza@gmail.com>
2020-06-11 01:12:41 -0400

    mvp

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,14 +1,19 @@
 /* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
+           (replacing the placeholder with your Github name):
            https://api.github.com/users/<your name>
 */
 
-axios.get("https://api.github.com/users/melanie-mendoza")
-  .then(resolve => {
-    console.log(resolve.data)
-    const newCard = createCards(resolve.data)
-    document.querySelector(".cards").appendChild(newCard)
-  })
+axios
+  .get('https://api.github.com/users/melanie-mendoza')
+  .then( (response) => {
+    //console.log(response);
+    const newCard = cardCreator(response);
+    entryPoint.appendChild(newCard);
+  });
+  // .catch( (err) => {
+  //   console.log('the data was not returned', err);
+  // });
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -21,6 +26,8 @@ axios.get("https://api.github.com/users/melanie-mendoza")
            create a new component and add it to the DOM as a child of .cards
 */
 
+//entryPoint.appendChild(cardCreator);
+
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
           , manually find some other users' github handles, or use the list found 
@@ -31,7 +38,25 @@ axios.get("https://api.github.com/users/melanie-mendoza")
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  'https://api.github.com/users/markanator',
+  'https://api.github.com/users/Aaron-Lamb',
+  'https://api.github.com/users/ClydeFrog04',
+  'https://api.github.com/users/linzyk86',
+  'https://api.github.com/users/robo-chick',
+  'https://api.github.com/users/JessBonanno'
+];
+
+followersArray.forEach(currValue => {
+  axios
+    .get(currValue)
+    .then(response => {
+      const newCard = cardCreator(response);
+      entryPoint.appendChild(newCard);
+    })
+})
+
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -60,3 +85,60 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+function cardCreator(obj) {
+
+// Variables
+  const card = document.createElement('div');
+  const image = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const userName = document.createElement('p');
+  const userLocation = document.createElement('p');
+  const userProfile = document.createElement('p');
+  const userUrl = document.createElement('a');
+  const followersCount = document.createElement('p');
+  const followingCount = document.createElement('p');
+  const userBio = document.createElement('p');
+
+// Add ClassList
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  userName.classList.add('username');
+
+
+// Structure Children
+  card.appendChild(image);
+  card.appendChild(cardInfo);
+
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(userName);
+  cardInfo.appendChild(userLocation);
+  cardInfo.appendChild(userProfile);
+
+  userProfile.appendChild(userUrl);
+
+  cardInfo.appendChild(followersCount);
+  cardInfo.appendChild(followingCount);
+  cardInfo.appendChild(userBio);
+
+// Content
+  userUrl.href = obj.data.html_url;
+  image.src = obj.data.avatar_url;
+  name.textContent = `${obj.data.name}`;
+  userName.textContent = `${obj.data.login}`;
+  userLocation.textContent = `Location: ${obj.data.location}`;
+
+  userUrl.textContent = obj.html_url;
+  userProfile.textContent = `Profile: ${userUrl}`;
+
+  followersCount.textContent = `Follower: ${obj.data.followers}`;
+  followingCount.textContent = `Following: ${obj.data.following}`;
+  userBio.textContent = `Bio: ${obj.data.bio}`;
+
+  return card
+
+}
+
+const entryPoint = document.querySelector('.cards');
