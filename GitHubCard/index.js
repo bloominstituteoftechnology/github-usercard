6c4b73efed8af9d83615665322d58737f25928dc
samcode206 <sameem-hadi@lambdastudents.com>
2020-07-09 08:51:10 -0700

    final

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,60 +1,94 @@
-/*
-  STEP 1: using axios, send a GET request to the following URL
-    (replacing the placeholder with your Github name):
-    https://api.github.com/users/<your name>
-*/
 
-/*
-  STEP 2: Inspect and study the data coming back, this is YOUR
-    github info! You will need to understand the structure of this
-    data in order to use it to build your component function
+const cardsAttach = document.querySelector('.cards');
 
-    Skip to STEP 3.
-*/
+  function createUserCard(obj){
 
-/*
-  STEP 4: Pass the data received from Github into your function,
-    and append the returned markup to the DOM as a child of .cards
-*/
+    // Create DOM elements
+  
+    const card = document.createElement('div');
+    const userImg = document.createElement('img');
+    const cardInfo = document.createElement('div');
+    const name = document.createElement('h3');
+    const userName = document.createElement('p');
+    const location = document.createElement('p');
+    const profile = document.createElement('p');
+    const profileAddress = document.createElement('a');
+    const followers = document.createElement('p');
+    const following = document.createElement('p');
+    const bio = document.createElement('p');
+  
+  
+    // Set classes
+  
+    card.className = ('card');
+    cardInfo.className = ('card-info');
+    name.className = ('name');
+    userName.className = ('username');
+  
+  
+    // Set contents / text
+  
+    userImg.setAttribute('src', obj.avatar_url);
+    name.textContent = obj.name;
+    userName.textContent = obj.login;
+    location.textContent = obj.location;
+    profile.textContent = 'Profile: '
+    profileAddress.textContent = obj.html_url;
+    //profileAddress.textContent = `${obj.html_url}`;
+    profileAddress.setAttribute('href', obj.html_url);
+    followers.textContent = `Followers: ${obj.followers}`;
+    following.textContent = `Following: ${obj.following}`;
+    bio.textContent = `Bio: ${obj.bio}`;
+  
+  
+    // Create Structure
+  
+    card.appendChild(userImg);
+    card.appendChild(cardInfo);
+    cardInfo.appendChild(name);
+    cardInfo.appendChild(userName);
+    cardInfo.appendChild(location);
+    cardInfo.appendChild(profile);
+    profile.appendChild(profileAddress);
+    cardInfo.appendChild(followers);
+    cardInfo.appendChild(following);
+    cardInfo.appendChild(bio);
+  
+  
+    return card;
+  
+  
+  }
 
-/*
-  STEP 5: Now that you have your own card getting added to the DOM, either
-    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
-    manually find some other users' github handles, or use the list found at the
-    bottom of the page. Get at least 5 different Github usernames and add them as
-    Individual strings to the friendsArray below.
+  // rendering my card 
+  axios.get('https://api.github.com/users/samcode206')
+  .then(response => {
 
-    Using that array, iterate over it, requesting data for each user, creating a new card for each
-    user, and adding that card to the DOM.
-*/
+    const gitData = response.data;
 
-const followersArray = [];
+    cardsAttach.appendChild(createUserCard(gitData));
 
-/*
-  STEP 3: Create a function that accepts a single object as its only argument.
-    Using DOM methods and properties, create and return the following markup:
+  })
 
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
-*/
+  .catch(error => {
+    console.log('Here is the problem', error);
+  })
 
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
-*/
+// rendering the instructor cards
+
+const instructors = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
+instructors.forEach(inst => {
+  axios.get(`https://api.github.com/users/${inst}`)
+    .then(response => {
+
+      const gitData = response.data;
+
+      cardsAttach.appendChild(createUserCard(gitData));
+    })
+
+    .catch(error => {
+      console.log('Here is the problem', error)
+    })
+})
+
