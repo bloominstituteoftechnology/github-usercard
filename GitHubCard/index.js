33eb58fa292f7b1b25df3fe57034122153c30697
Harry Henry Gebel <hhgebel@gmail.com>
2020-07-09 04:36:55 -0400

    Step 1 completed

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -13,6 +13,9 @@ import axios from 'axios';
     https://api.github.com/users/<your name>
 */
 
+axios.get('https://api.github.com/users/HarryHenryGebel')
+  .then(response => makeCard(response.data));
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
