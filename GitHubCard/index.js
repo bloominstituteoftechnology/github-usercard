06c508af6e062112ed07734e2dd82e65a0f17d66
samantha <toxicwow21@gmail.com>
2020-07-10 08:50:51 -0400

    finished MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,20 +1,27 @@
+import axios from 'axios';
+
+
+const card = document.querySelector('.cards');
+const myUserData = 'https://api.github.com/users/svoncannon';
+
+
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-const cards = document.querySelector(".cards");
+axios.get('https://api.github.com/users/svoncannon').then(response => {
+    console.log(response);
+}).catch(error => {
+    console.log('there has been an error' + error);
+})
 
-axios.get("https://api.github.com/users/svoncannon")
-.then(response => {
-  // response.data.forEach((object) => {
-    const newUserCard = cardCreator(response.data)
-    cards.appendChild(newUserCard)
-  // })
-})
-.catch( error => {
-  console.log(error);
-})
+axios.get(myUserData).then(response => {
+  card.prepend(cardMaker(response))
+}).catch(error => {
+  console.log('gotta catch \'em all!' + error)
+});
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -38,8 +45,20 @@ axios.get("https://api.github.com/users/svoncannon")
     Using that array, iterate over it, requesting data for each user, creating a new card for each
     user, and adding that card to the DOM.
 */
+const followersArray = ['tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell'];
 
-const followersArray = [];
+followersArray.forEach(item => {
+  axios.get('https://api.github.com/users/' + item)
+  .then(response => {
+    card.appendChild(cardMaker(response));
+  }).catch(error => {
+    console.log('gotta catch \'em all' + error);
+  })
+});
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -60,6 +79,86 @@ const followersArray = [];
       </div>
     </div>
 */
+function cardMaker(user){
+  const card = document.createElement('div'); 
+  const userImg = document.createElement('img'); 
+  const cardInfo = document.createElement('div'); 
+  const userName = document.createElement('h3'); 
+  const userHandle = document.createElement('p')
+  const userLocale = document.createElement('p'); 
+  const userProfile = document.createElement('p');
+  // make sure to append the link below to the profile
+  const profileLink = document.createElement('a'); 
+  // make sure you APPEND THIS.
+  const userFollowers = document.createElement('p'); 
+  const userFollows = document.createElement('p'); 
+  const userBio = document.createElement('p'); 
+  const button = document.createElement('button'); 
+  
+  const contactCard = document.createElement('div'); 
+  const userContact = document.createElement('p'); 
+  const userRepos = document.createElement('p'); 
+  const reposLink = document.createElement('a'); 
+  const twitterHandle = document.createElement('p'); 
+
+  // now we can add class names to the appropriate elements 
+card.classList.add('card'); 
+cardInfo.classList.add('card-info'); 
+userName.classList.add('name'); 
+userHandle.classList.add('username');
+button.classList.add('button');
+contactCard.classList.add('closed')
+userContact.classList.add('contact')
+// with the above done, we have to append the elements we've made to the parents 
+// append img to card
+card.appendChild(userImg); 
+// append card-info div to card div 
+card.appendChild(cardInfo); 
+// append the profile link to the paragraph 
+// NOTE: Needed to add the text content and append before the others - best practice: do the creating, adding classes/content and appending on one element at a time. 
+userProfile.textContent = 'Profile: '
+userProfile.appendChild(profileLink);
+// append all the items to the card info 
+cardInfo.appendChild(userName); 
+cardInfo.appendChild(userHandle);
+cardInfo.appendChild(userLocale);
+cardInfo.appendChild(userProfile);
+cardInfo.appendChild(userFollowers);
+cardInfo.appendChild(userFollows);
+cardInfo.appendChild(userBio);
+cardInfo.appendChild(button);
+cardInfo.appendChild(contactCard); 
+contactCard.appendChild(userContact); 
+contactCard.appendChild(userRepos); 
+userRepos.textContent = 'User\'s repos: ';
+userRepos.appendChild(reposLink); 
+twitterHandle.textContent = 'Twitter: ' + user.data.twitter_username; 
+contactCard.appendChild(twitterHandle);
+// testing, testing, is this thing on?
+// console.log(card)
+// adding content to the elements 
+userImg.src = user.data.avatar_url; 
+userName.textContent = user.data.name; 
+userHandle.textContent = user.data.login; 
+userLocale.textContent = user.data.location; 
+ // this is hardcoded to house the link
+profileLink.textContent = user.data.html_url;
+profileLink.setAttribute('href', user.data.html_url); 
+userFollowers.textContent = 'Followers: ' + user.data.followers; 
+userFollows.textContent = 'Following: ' + user.data.following; 
+userBio.textContent = 'Bio: ' + user.data.bio;
+button.textContent = 'More Info'; 
+userContact.textContent = 'Email: ' + user.data.email; 
+reposLink.textContent = 'Repos Link'; 
+reposLink.setAttribute('href', user.data.repos_url);
+
+button.addEventListener('click', () => {
+  contactCard.classList.toggle('closed')
+});
+
+// THOU MUST RETURN 
+return card
+}
 
 /*
   List of LS Instructors Github username's:
