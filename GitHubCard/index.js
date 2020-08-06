01c82245ecb717a457064d3b5731c7189d0b10a8
devbeau <48367657+devbeau@users.noreply.github.com>
2020-08-06 15:26:13 -0400

    mvp done with comments, stretch one done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,33 +1,27 @@
 import axios from 'axios';
 
-/*
-  STEP 1: using axios, send a GET request to the following URL
-    (replacing the placeholder with your Github name):
-    https://api.github.com/users/<your name>
-*/
-const myProfile = 'https://api.github.com/users/devbeau';
-function getCard(profile){
+const myProfile = 'https://api.github.com/users/devbeau'; 
+function getCard(profile){ // function gets a card based on api url
   axios.get(profile)
       .then(response => {
         console.log(response);
         let userDataObj = response.data;
-        let card = createCard(userDataObj);
-        appendToPage(card);
-        debugger;
+        let card = createCard(userDataObj); //creates card from dataObj
+        appendToPage(card); //attaches card to the cards container
       })
       .catch(error => {
         console.log("something went wrong", error)
         debugger
       });}
 
-function getFollowerCards (profile){
-  const followersArray = [];
-  axios.get(`${profile}/followers`)
+function getFollowerCards (profile){ // gets the url of followers from the api
+  const followersArray = [];         // call on a profile and then requests 
+  axios.get(`${profile}/followers`)  // their profile from the api.
       .then(response => {
-        response.data.forEach((item, index) =>{
+        response.data.forEach((item, index) =>{ // urls into array
           followersArray[index] = item.url
         })
-        followersArray.forEach(item =>{
+        followersArray.forEach(item =>{ // array into profile urls for getCard
           getCard(item);
         })
       })
@@ -36,35 +30,39 @@ function getFollowerCards (profile){
         debugger;
       })
 }
-                              
+
+// create an element with element, classname, and textcontent
 function createElement(htmlElement, className = 0, textContent = 0){
   let element = document.createElement(htmlElement);
+  // ternary conditions for default values
   className !== 0 ? element.classList.add(className) : element;
   textContent !== 0 ? element.textContent = textContent : element.textContent = null;
   return element;
 }
-
+// create an image with src attribute
 function createImg(imgSrc){
   let image = createElement('img');
   image.setAttribute('src', imgSrc);
   return image;
 }
-
+// create an anchor with url and text attributes
 function createAnchor(anchorUrl, anchorText){
   let anchor = createElement('a', 0, anchorText);
   anchor.setAttribute('href', anchorUrl);
   return anchor;
 }
-
+// append completed card to page
 function appendToPage(card){
-  let container = document.querySelector('.cards');
+  let container = document.querySelector('.cards'); //container for cards
   return container.appendChild(card);
 }
-
+// instantiate elements and create the structure and content of the component:
 function createCard(userDataObj){
 
+  //instantiate the elements:
   let cardDiv = createElement('div','card');
   let cardImg = createImg(userDataObj.avatar_url);
+  let cardInfo = createElement('div','card-info');
   let cardHeader = createElement('h3', 'name', userDataObj.name);
   let cardUsername = createElement('p', 'username', userDataObj.login);
   let cardLocation = createElement('p', 0, `Location: ${userDataObj.location}`);
@@ -74,103 +72,24 @@ function createCard(userDataObj){
   let cardFollowing = createElement('p', 0, `Following: ${userDataObj.following}`);
   let cardBio = createElement('p', 0, userDataObj.bio);
 
+  //appending the structure:
   cardProfile.appendChild(profileAnchor);
-  for (let el of  [cardImg, cardHeader, cardUsername, cardLocation,
+  console.log("cardProfile", cardProfile)
+  for (let el of  [cardHeader, cardUsername, cardLocation,
                   cardProfile, cardFollowers, cardFollowing, cardBio]){
-        cardDiv.appendChild(el);
+        console.log("appending loop", el);
+        cardInfo.appendChild(el);
       }
-  return cardDiv;
-}
-                
-getCard(myProfile);                    
-getFollowerCards(myProfile);
+  cardDiv.appendChild(cardImg);
+  cardDiv.appendChild(cardInfo);
 
-
-/*
-  STEP 2: Inspect and study the data coming back, this is YOUR
-    github info! You will need to understand the structure of this
-    data in order to use it to build your component function
-
-    Skip to STEP 3.
-*/
-
-/*
-  STEP 4: Pass the data received from Github into your function,
-    and append the returned markup to the DOM as a child of .cards
-*/
-
-/*
-  STEP 5: Now that you have your own card getting added to the DOM, either
-    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
-    manually find some other users' github handles, or use the list found at the
-    bottom of the page. Get at least 5 different Github usernames and add them as
-    Individual strings to the friendsArray below.
-
-    Using that array, iterate over it, requesting data for each user, creating a new card for each
-    user, and adding that card to the DOM.
-*/
-
-/*
-  STEP 3: Create a function that accepts a single object as its only argument.
-    Using DOM methods and properties, create and return the following markup:
-
-  
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
-*/
-function createElement(htmlElement, className = 0, textContent = 0){
-    let element = document.createElement(htmlElement);
-    className !== 0 ? element.classList.add(className) : element;
-    textContent !== 0 ? element.textContent = textContent : element.textContent = null;
-    return element;
-  }
-function createImg(imgSrc){
-  let image = createElement('img');
-  image.setAttribute('src', imgSrc);
-  return image;
-}
-function createAnchor(anchorUrl, anchorText){
-  let anchor = createElement('a', 0, anchorText);
-  anchor.setAttribute('href', anchorUrl);
-  return anchor;
-}
-function appendToPage(card){
-  let container = document.querySelector('.cards');
-  return container.appendChild(card);
+  console.log("card right before return", cardDiv);
+  return cardDiv; // returns card component
 }
 
-function createCard(userDataObj){
+getCard(myProfile);                    //execute
+getFollowerCards(myProfile);           //execute
 
-    let cardDiv = createElement('div','card');
-    let cardImg = createImg(userDataObj.avatar_url);
-    let cardHeader = createElement('h3', 'name', userDataObj.name);
-    let cardUsername = createElement('p', 'username', userDataObj.login);
-    let cardLocation = createElement('p', 0, `Location: ${userDataObj.location}`);
-    let cardProfile = createElement('p', 0, 'Profile: ');
-    let profileAnchor = createAnchor(userDataObj.html_url, userDataObj.html_url);
-    let cardFollowers = createElement('p', 0, `Followers: ${userDataObj.followers}`);
-    let cardFollowing = createElement('p', 0, `Following: ${userDataObj.following}`);
-    let cardBio = createElement('p', 0, userDataObj.bio);
-
-    cardProfile.appendChild(profileAnchor);
-    for (let el of  [cardImg, cardHeader, cardUsername, cardLocation,
-                    cardProfile, cardFollowers, cardFollowing, cardBio]){
-          cardDiv.appendChild(el);
-        }
-    return cardDiv;
-}
 /*
   List of LS Instructors Github username's:
     tetondan
