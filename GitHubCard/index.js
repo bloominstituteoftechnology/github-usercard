e0309e7a49acd9254bdb7e2734cacab3eab98ee7
Cameron Garner <camerongarner9@gmail.com>
2020-10-01 15:34:48 -0400

    finished the project

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,8 +2,16 @@
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
-*/
+*/ 
+axios.get("https://api.github.com/users/CameronGarner")
+.then(function(futuredata){
+  cards.appendChild(createDiv(futuredata))
+})
+.catch(function(err){
+console.log(err)
+})
 
+let cards = document.querySelector(".cards")
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -17,8 +25,9 @@
     and append the returned markup to the DOM as a child of .cards
 */
 
-/*
-  STEP 5: Now that you have your own card getting added to the DOM, either
+
+
+  /*STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
     manually find some other users' github handles, or use the list found at the
     bottom of the page. Get at least 5 different Github usernames and add them as
@@ -28,7 +37,21 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [ 
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell"
+];
+
+followersArray.forEach(function(item){
+  axios.get("https://api.github.com/users/" + item)
+  .then(function(thisdata){
+    cards.appendChild(createDiv(thisdata))
+  })
+})
+
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -49,6 +72,50 @@ const followersArray = [];
       </div>
     </div>
 */
+
+function createDiv(object){
+  let div = document.createElement("div")
+  let img = document.createElement("img")
+  let div2 = document.createElement("div")
+  let h3 = document.createElement("h3")
+  let p = document.createElement("p")
+  let p2 = document.createElement("p")
+  let p3 = document.createElement("p")
+  let anchor = document.createElement("a")
+  let p4 = document.createElement("p")
+  let p5 = document.createElement("p")
+  let p6 = document.createElement("p")
+  div.classList.add("card")
+  div2.classList.add("card-info")
+  h3.classList.add("name")
+  p.classList.add("username")
+  div.appendChild(img)
+  div.appendChild(div2)
+  div2.appendChild(h3)
+  div2.appendChild(p)
+  div2.appendChild(p2)
+  div2.appendChild(p3)
+  div2.appendChild(p4)
+  div2.appendChild(p5)
+  div2.appendChild(p6)
+  img.src = object.data.avatar_url
+  h3.textContent = object.data.name
+  p.textContent = object.data.login
+  p2.textContent = `Location: ${object.data.location}`
+  let text = document.createTextNode(object.data.html_url)
+  anchor.href = object.data.html_url
+  anchor.appendChild(text)
+  p3.textContent = "Profile:"
+  p4.textContent = `Followers: ${object.data.followers}`
+  p5.textContent = `Following: ${object.data.following}`
+  p6.textContent = `Bio: ${object.data.bio}`
+  p3.appendChild(anchor)
+  return div
+}
+
+
+
+
 
 /*
   List of LS Instructors Github username's:
