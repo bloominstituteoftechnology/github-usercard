e782cd2153f43b2b8ed9d4f78e281b4e69ac59df
Caroline <carolinefallonlucas@gmail.com>
2020-10-01 21:29:26 -0400

    parts 1,2,3 - no stretch

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,60 +1,109 @@
+
+
+import axios from 'axios'
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
 
-/*
-  STEP 2: Inspect and study the data coming back, this is YOUR
-    github info! You will need to understand the structure of this
-    data in order to use it to build your component function
-
-    Skip to STEP 3.
-*/
 
 /*
-  STEP 4: Pass the data received from Github into your function,
-    and append the returned markup to the DOM as a child of .cards
+  STEP 2: Inspect and study the data coming back, this is YOUR github info! 
+  You will need to understand the structure of this user, and adding that card to the DOM.
 */
 
-/*
-  STEP 5: Now that you have your own card getting added to the DOM, either
-    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
-    manually find some other users' github handles, or use the list found at the
-    bottom of the page. Get at least 5 different Github usernames and add them as
-    Individual strings to the friendsArray below.
 
-    Using that array, iterate over it, requesting data for each user, creating a new card for each
-    user, and adding that card to the DOM.
-*/
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'bigknell', 'luishrd'];
 
-const followersArray = [];
+followersArray.forEach(item =>
+{
+  axios.get(`https://api.github.com/users/${item}`)
+    .then(res =>
+    {
+      gitAppend.appendChild(gitHubMaker(res.data))
+    })
+    .catch(drama => console.log(`wrongo: ${drama}`))
+})
 
-/*
-  STEP 3: Create a function that accepts a single object as its only argument.
-    Using DOM methods and properties, create and return the following markup:
 
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
-*/
+const gitAppend = document.querySelector('.container')
 
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
-*/
+
+
+axios.get('https://api.github.com/users/carolinefallonlucas')
+  .then(res =>
+  {
+    console.log(res)
+    return gitHubMaker(res.data)
+
+  })
+  .catch(drama => console.log(drama))
+
+
+
+function gitHubMaker(object)
+{
+
+  //setting elements
+  const gitCard = document.createElement('div')
+  const gitImg = document.createElement('img')
+  const gitCardInfo = document.createElement('div')
+  const gitName = document.createElement('h3')
+  const gitUserName = document.createElement('p')
+  const gitLocation = document.createElement('p')
+  const gitProfile = document.createElement('p')
+  const gitProfileA = document.createElement('a')
+  const gitFollowers = document.createElement('p')
+  const gitFollowing = document.createElement('p')
+  const gitBio = document.createElement('p')
+
+  //setting class names and attributes
+
+
+
+  gitCard.classList.add('card')
+  gitCardInfo.classList.add('card-info')
+  gitName.classList.add('name')
+  gitName.textContent = object['login']
+  gitUserName.classList.add('username')
+  gitUserName.textContent = object['login']
+  gitImg.src = object.avatar_url
+
+
+  // gitImg.src = objectURL
+
+
+
+  // gitImg.src = avatar_url
+  gitProfileA.href = object.html_url
+  gitLocation.textContent = object.location
+  gitFollowers.textContent = object.followers
+  gitFollowing.textContent = object.following
+  gitBio.textContent = object.login
+  gitProfile.textContent = `Profile: ${gitProfileA}`
+  //setting hierarchy
+  gitAppend.append(gitCard)
+  gitCard.appendChild(gitImg)
+  gitCard.appendChild(gitCardInfo)
+  gitCardInfo.appendChild(gitName)
+  gitCardInfo.appendChild(gitUserName)
+  gitCardInfo.appendChild(gitLocation)
+  gitCardInfo.appendChild(gitProfile)
+  gitProfile.appendChild(gitProfileA)
+  gitCardInfo.appendChild(gitFollowers)
+  gitCardInfo.appendChild(gitFollowing)
+  gitCardInfo.appendChild(gitBio)
+
+  return gitCard
+
+
+}
+
+
+
+
+
+
+
