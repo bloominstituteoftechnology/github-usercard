c55fb59255fc9748c099ac7548c947300db5ff1e
ruizaj13 <ruizaj13@gmail.com>
2020-10-29 13:07:46 -0700

    axios implemented and functional

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,12 @@
+import axios from 'axios';
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+axios
+  .get('https://api.github.com/users/ruizaj13')
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
