a99a497162bded35381feed5dc3882a5110a1d58
Nico Herrera <nherrera0831@gmail,com>
2020-11-05 22:07:57 -0800

    completed task 5

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,22 @@
+const axios = require("axios");
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+const getPractice = axios
+  .get("https://api.github.com/users/nico-herrera")
+  .then((res) => {
+    console.log(res);
+    const info = returnHTML(res.data);
+    document.querySelector("body").appendChild(info);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
+console.log(getPractice);
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -28,8 +41,27 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell",
+];
 
+followersArray.forEach((item) => {
+  axios
+    .get(`https://api.github.com/users/${item}`)
+    .then((res) => {
+      console.log(res.data);
+      const info = returnHTML(res.data);
+      document.querySelector("body").appendChild(info);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+console.log;
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -49,6 +81,60 @@ const followersArray = [];
       </div>
     </div>
 */
+
+const data = getPractice.data;
+console.log(data);
+
+function returnHTML(data) {
+  const mainDiv = document.createElement("mainDiv"); // main div
+  mainDiv.classList.add("card");
+
+  const img = document.createElement("img"); // image
+  img.src = data.avatar_url;
+
+  const div = document.createElement("div"); // inner div
+  div.classList.add("card-info");
+
+  const name = document.createElement("h3"); // h3 element
+  name.textContent = data.name;
+  name.classList.add("name");
+
+  const username = document.createElement("p");
+  username.classList.add("username");
+  username.textContent = data.login;
+
+  const location = document.createElement("p");
+  location.textContent = `Location: ${data.location}`;
+
+  const profile = document.createElement("p");
+  const a = document.createElement("a");
+  a.href = data.url;
+  profile.textContent = a;
+
+  const followers = document.createElement("p");
+  followers.textContent = `Followers: ${data.followers}`;
+
+  const following = document.createElement("p");
+  following.textContent = `Following: ${data.following}`;
+
+  const bio = document.createElement("p");
+  bio.textContent = `Bio: ${data.bio}`;
+
+  mainDiv.appendChild(img);
+  mainDiv.appendChild(div);
+  div.appendChild(name);
+  div.appendChild(username);
+  div.appendChild(location);
+  div.appendChild(profile);
+  profile.appendChild(a);
+  div.appendChild(followers);
+  div.appendChild(following);
+  div.appendChild(bio);
+
+  return mainDiv;
+}
+
+// console.log(returnHTML(data));
 
 /*
   List of LS Instructors Github username's:
