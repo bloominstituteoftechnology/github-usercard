1fa300a37de36fe32527098a0464f4259a102b0f
Chris <chris@databoxdigital.com>
2020-12-02 09:22:01 -0600

    finished mvp

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,10 @@
+import axios from 'axios';
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -28,33 +29,41 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
 
-/*
-  STEP 3: Create a function that accepts a single object as its only argument.
-    Using DOM methods and properties, create and return the following markup:
+function getCardData(username) {
+  axios.get(`https://api.github.com/users/${username}`)
+    .then(response => {
+      document.querySelector('.cards').insertAdjacentHTML('beforeend', cardCreator(response.data));
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
 
-    <div class="card">
-      <img src={image url of user} />
+getCardData('christophersutton');
+
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+followersArray.forEach(e => getCardData(e));
+
+
+//  STEP 3: Create a function that accepts a single object as its only argument.
+//    Using DOM methods and properties, create and return the following markup:
+
+function cardCreator(data) {
+  const card =
+    `<div class='card'>
+      <img src=${data.avatar_url} />
       <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
+        <h3 class="name">${data.name}</h3>
+        <p class="username">${data.login}</p>
+        <p>Location: ${data.location}</p>
         <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
+          <a href=${data.html_url}>${data.html_url}</a>
         </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
+        <p>Followers: ${data.followers}</p>
+        <p>Following: ${data.following}</p>
+        <p>Bio: ${data.bio}</p>
       </div>
-    </div>
-*/
-
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
-*/
+    </div>`
+  return card;
+}
