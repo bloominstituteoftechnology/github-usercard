425db3cbcdfd6da87e3bdc519c488775f1aa30e1
Diego Roman <diegormnv209@gmail.com>
2020-12-03 12:53:45 -0800

    Added cards of followers as well as the instructors given

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -42,8 +42,14 @@ axios.get('https://api.github.com/users/Diegormnv')
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell', 'Aszalea-Calderon', 'jaypdl', 'katieolson84', 'c-melchor', 'ahmedseragcodes'];
 
+followersArray.forEach((follower) =>{
+  axios.get(`https://api.github.com/users/${follower}`)
+  .then((res) =>{
+    profiles.appendChild(userCard(res.data));
+  })
+})
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
