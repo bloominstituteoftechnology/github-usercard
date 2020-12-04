8dbd00b4ad0216a8db6116de23bde13d75e3f62d
Michelle Chen <mchen0062@gmail.com>
2020-12-03 20:05:25 -0800

    created cards for followers

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,19 +3,21 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-import axios from "axios"
+import axios from "axios";
 
 axios
 .get(`https://api.github.com/users/michelleccodes`)
-.then((res) => {
-  
-})
+.then(res => {
+  const newCard = makeCard(res.data)
+  cards.prepend(newCard)
+  })
 .catch((err)=>{
   console.log("you have an error)':")
 })
 
-function makeCard (object) {
+const cards = document.querySelector(".cards"); 
 
+function makeCard (obj) {
   const card = document.createElement("div");
   const userImage = document.createElement("img");
   const cardInfo = document.createElement("div");
@@ -27,6 +29,8 @@ function makeCard (object) {
   const followers = document.createElement("p");
   const following = document.createElement("p");
   const bio = document.createElement("p");
+
+  console.log(profile)
   
   card.appendChild(userImage); 
   card.appendChild(cardInfo);
@@ -38,7 +42,18 @@ function makeCard (object) {
   cardInfo.appendChild(following);
   cardInfo.appendChild(bio);
   profile.appendChild(link);
-  
+
+  userImage.src = obj.avatar_url;
+  name.textContent = obj.name;
+  username.textContent = obj.login;
+  location.textContent = `Location: ${obj.location}`;
+  profile.textContent = 'Profile:';
+  link.href = obj.html_url;
+  link.textContent = "GitHub";
+  followers.textContent = `Followers: ${obj.followers}`;
+  following.textContent = `Following: ${obj.following}`;
+  bio.textContent = `Bio: ${obj.bio}`;
+
   card.classList.add("card");
   cardInfo.classList.add("card-info"); 
   name.classList.add("name");
@@ -46,9 +61,6 @@ function makeCard (object) {
   
   return card; 
   }
-  
-  const cards = document.querySelector(".cards"); 
-  
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -60,19 +72,6 @@ function makeCard (object) {
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
-
-/*
-  STEP 5: Now that you have your own card getting added to the DOM, either
-    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
-    manually find some other users' github handles, or use the list found at the
-    bottom of the page. Get at least 5 different Github usernames and add them as
-    Individual strings to the friendsArray below.
-
-    Using that array, iterate over it, requesting data for each user, creating a new card for each
-    user, and adding that card to the DOM.
-*/
-
-const followersArray = [];
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -93,6 +92,31 @@ const followersArray = [];
       </div>
     </div>
 */
+
+
+/*
+  STEP 5: Now that you have your own card getting added to the DOM, either
+    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
+    manually find some other users' github handles, or use the list found at the
+    bottom of the page. Get at least 5 different Github usernames and add them as
+    Individual strings to the friendsArray below.
+
+    Using that array, iterate over it, requesting data for each user, creating a new card for each
+    user, and adding that card to the DOM.
+*/
+const followersArray = ['michellewong793', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
+followersArray.forEach(item => {
+  axios 
+    .get(`https://api.github.com/users/${item}`)
+    .then(res => {
+      const newCard = makeCard(res.data)
+      cards.appendChild(newCard)
+    })
+    .catch(err =>{
+      console.log("error):")
+    })
+})
 
 /*
   List of LS Instructors Github username's:
