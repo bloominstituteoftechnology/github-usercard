236747133b499d58f97d8390c8beab5695b72954
Molly Brown <mollybrown0516@gmail.com>
2020-12-10 20:55:42 -0500

    Completed creating DOM elements

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,11 +1,13 @@
+import axios from 'axios';
 /*
-  STEP 1: using axios, send a GET request to the following URL
+ [x] STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+const mollyGitHub = axios.get(`https://api.github.com/users/mollybee`);
+console.log('it works', mollyGitHub); //it works!
 /*
-  STEP 2: Inspect and study the data coming back, this is YOUR
+  [x]STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
     data in order to use it to build your component function
 
@@ -29,17 +31,46 @@
 */
 
 const followersArray = [];
+function gitHubCardMaker (someObject){
+  const divMainCard = document.createElement('div'); 
+  const imageElement = document.createElement('img');
+  const divCardInfo = document.createElement('div');
+  const usersNameElement = document.createElement('h3');
+  const firstParagraphElementUsersUserName = document.createElement('p');
+  const secondParagraphElementLocation = document.createElement('p');
+  const thirdParagraphElementProfile = document.createElement('p');
+  const fourthParagraphElementFollowers = document.createElement('p');
+  const fifthParagraphElementFollowing = document.createElement('p');
+  const sixthParagraphElementBio = document.createElement('p');
+  const urlAddressElement = document.createElement('a');
+  
+  divMainCard.classList.add('card');
+  imageElement.src = someObject.avatar_url;
+  divCardInfo.classList.add('card-info');
+  usersNameElement.classList.add('name');
+  firstParagraphElementUsersUserName.classList.add('username');
+  secondParagraphElementLocation.textContent = `Location: ${someObject.location}`;
+  thirdParagraphElementProfile.textContent = 'Profile: ';
+  urlAddressElement.src = someObject.html.url;
+  fourthParagraphElementFollowers.src = someObject.followers;
+  fifthParagraphElementFollowing.src = someObject.following;
+  sixthParagraphElementBio.src = `Bio: ${someObject.bio}`;
 
+
+  
+
+  
+}
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
 
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
+   x <div class="card">
+   x   <img src={image url of user} />
+   x   <div class="card-info">
+   x     <h3 class="name">{users name}</h3>
+   x     <p class="username">{users user name}</p>
+   x     <p>Location: {users location}</p>
         <p>Profile:
           <a href={address to users github page}>{address to users github page}</a>
         </p>
