3a50b3e5c80ea8f3466d75200b43c88e1fd15fa0
miketillman95 <matillman95@gmail.com>
2020-12-10 23:29:23 -0800

    hmwk

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,18 @@
+import axios from 'axios';
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+axios.get("https://api.github.com/users/miketillman95")
+.then(response => {
+console.log(response)
+})
+.catch( err => {
+console.log(err)
+})
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -16,6 +26,16 @@
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
+ const grabData = (event) => {
+  axios.get("https://api.github.com/users/miketillman95")
+  .then(response => {
+  console.log(response)
+  })
+  .catch( err => {
+  console.log(err)
+  })
+    event.append(".cards")
+} 
 
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
