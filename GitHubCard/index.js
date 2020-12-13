c1ff1bc14863028ce96e0fe1134aed8dda0705b0
miketillman95 <matillman95@gmail.com>
2020-12-13 15:36:07 -0800

    hmwk

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -6,14 +6,47 @@ import axios from 'axios';
     https://api.github.com/users/<your name>
 */
 
-axios.get("https://api.github.com/users/miketillman95")
-.then(response => {
-console.log(response)
-})
-.catch( err => {
-console.log(err)
-})
+// create funtction that gets user info from API
+function getData(username) {
+  
+//  .get to retrieve api, uses .then to tell the function what to do with the info it recieves
+  axios.get(`https://api.github.com/users/${username}`)
+  .then(response => { 
+    // create a variable with an object and store data
+    let user = {
+      avatar_url: response.data.avatar_url,
+      name: response.data.name ,
+      login: response.data.login,
+      location:response.data.location ,
+      html_url: response.data.html_url,
+      followers: response.data.followers,
+      following: response.data.following,
+      bio:response.data.bio , 
+      
+    
+    }
+    domCreate(user)
+    let entry = document.querySelector(".cards")
+    entry.appendChild(domCreate(user))
+    console.log(user, 'zac')
+  })
 
+  // github api going through the users
+  // .then(( ) => {
+  //   axios.get(`https://api.github.com/users/${username}/followers`)
+  //   .then(res => {
+  //     console.log(res.data)
+  //   })
+  //   .catch(err => {
+  //     console.log(err, 'broken')
+  //   })
+  // })
+  .catch( err => {
+  console.log(err)
+  })
+}
+
+getData('miketillman95')
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -26,16 +59,7 @@ console.log(err)
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
- const grabData = (event) => {
-  axios.get("https://api.github.com/users/miketillman95")
-  .then(response => {
-  console.log(response)
-  })
-  .catch( err => {
-  console.log(err)
-  })
-    event.append(".cards")
-} 
+
 
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
@@ -56,6 +80,17 @@ const followersArray = [
 "shayanzare",
 ];
 
+let cards = document.querySelector(".cards")
+
+followersArray.forEach((followers)=> {
+  axios.get(`https://api.github.com/users/${followers}`)
+  .then(info => {
+  cards.appendChild(domCreate(info.data))
+  })
+  .catch(err => {
+    console.log(err)
+  })
+})
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -84,35 +119,51 @@ const followersArray = [
     luishrd
     bigknell
 */
-function domCreate (dom){
+function domCreate (data){
 const card = document.createElement("div")
-  card.classList.add(".card")
+card.classList.add("card")
 
-    const pic = document.createElement('img')
-      pic.src = "https://api.github.com/users/miketillman95"
+const pic = document.createElement('img')
+pic.src = data.avatar_url
+card.appendChild(pic)
 
-        const header = document.createElement('h3')
-        header.classList.add('.name')
-        header.textContent = grabData.username
 
-          const content1 = document.createElement("p")
-          content1.classList.add('.username')
-          content1.textContent = 
 
-           const content2 = document.createElement("p")
-              content2.textContent.add(`Location: ${grabData.location}`)
+ const header = document.createElement('h3')
+ header.classList.add('name')
+ header.textContent = data.name
+ card.appendChild(header)
 
-                const content3 = document.createElement("p")
-                  const anchor = document.createElement('a')
-                    content3.addel
+ const content1 = document.createElement("p")
+ content1.classList.add('username')
+ content1.textContent = data.login
+ card.appendChild(content1)
 
-                      const content3 = document.createElement("p")
-                        content3.textContent.add()
+ const content2 = document.createElement("p")
+ content2.textContent = `Location: ${data.location}`
+ card.appendChild(content2)
+ 
+ const content3 = document.createElement("p")
+content3.textContent = "Profile:"
+card.appendChild(content3)
+
+ const anchor = document.createElement('a')
+ anchor.textContent = data.html_url
+ content3.appendChild(anchor)
+ 
+
+ const content4 = document.createElement("p")
+ content4.textContent = `Followers: ${data.followers}`
+ card.appendChild(content4)
                   
-                            const content4 = document.createElement("p")
-                               content4.textContent.add() 
+ const content5 = document.createElement("p")
+ content5.textContent = `Following: ${data.following}`
 
-                               const content4 = document.createElement("p")
-                               content4.textContent.add(`Bio: ${}`) 
+ const content6 = document.createElement("p")
+content6.textContent = `Bio: ${data.bio}` 
+card.appendChild(content6)
+return card
     }
-    }
+
+
+    
