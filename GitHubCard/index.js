3e113f3c2689ddeb4db7ff06563a2d7e1a4f0d10
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-12-16 14:06:29 -0600

    Update index.js
    Imported Axios
    
    returning data object with user info like, login, id, avatar_url, and more.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,13 @@
+import axios from 'axios';
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/CleverOscar').then( response => 
+  console.log(response)
+).catch(err => console.log(err))
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
