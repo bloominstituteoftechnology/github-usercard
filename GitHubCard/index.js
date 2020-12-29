f535c2853b851945daae6fd0a5057788c6ac6c5c
Vanessa Dixon <vanessazdixon@gmail.com>
2020-12-29 02:42:32 -0500

    completed mvp

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,12 +1,7 @@
-/*
-  STEP 1: using axios, send a GET request to the following URL
-    (replacing the placeholder with your Github name):
-    https://api.github.com/users/<your name>
-*/
+
 const cardDiv = document.querySelector('div.cards');
 const card = axios.get(' https://api.github.com/users/vanessadixon1')
 .then(res => {
-  console.log(res.data)
   const response = res.data;
   cardDiv.append(githubCard(response))
 })
@@ -14,31 +9,7 @@ const card = axios.get(' https://api.github.com/users/vanessadixon1')
   console.log(`error is:`, e)
 })
 
-/*
-  STEP 2: Inspect and study the data coming back, this is YOUR
-    github info! You will need to understand the structure of this
-    data in order to use it to build your component function
-
-    Skip to STEP 3.
-*/
-
-/*
-  STEP 4: Pass the data received from Github into your function,
-    and append the returned markup to the DOM as a child of .cards
-*/
-
-/*
-  STEP 5: Now that you have your own card getting added to the DOM, either
-    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
-    manually find some other users' github handles, or use the list found at the
-    bottom of the page. Get at least 5 different Github usernames and add them as
-    Individual strings to the friendsArray below.
-
-    Using that array, iterate over it, requesting data for each user, creating a new card for each
-    user, and adding that card to the DOM.
-*/
-
-const followersArray = [];
+const followersArray = ["Wais-A","chelsabeth","DannyManzietti", "mary-clayton","afialydia"];
 
 function githubCard(obj) {
   const div = document.createElement('div');
@@ -51,10 +22,12 @@ function githubCard(obj) {
   div2.classList.add('card-info');
 
   const h3 = document.createElement('h3');
-  h3.classList.add('hame');
+  h3.classList.add('name');
+  h3.textContent = obj.name;
 
   const p1 = document.createElement('p');
   p1.classList.add('username');
+  p1.textContent = obj.login;
 
   const p2 = document.createElement('p');
   p2.textContent = `Location: ${obj.location}`;
@@ -81,32 +54,12 @@ function githubCard(obj) {
   return div;
 }
 
-githubCard(card);
-/*
-  STEP 3: Create a function that accepts a single object as its only argument.
-    Using DOM methods and properties, create and return the following markup:
 
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
-*/
+followersArray.forEach(item => {
+  axios.get(`https://api.github.com/users/${item}`)
+  .then(response => {
+    cardDiv.append(githubCard(response.data))
+  })
+})
 
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
-*/
+
