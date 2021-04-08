0b49d58dc6ff8251b3f6571c4230bad2ec658fc1
cynthiaannberg <cynthia.ann.berg@me.com>
2021-04-08 17:45:51 -0600

    usercard

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,7 +1,20 @@
+axios
+  .get("https://api.github.com/users/cynthiaannberg")
+  .then((res) => {
+    constInfo.forEach( (item) => {
+      const finishedProduct = cardMaker({ imgUrl: user.avatar_url, name: userInfo.name, username: userInfo.login, location: userInfo.location, url: userInfo.html_url, followers: user.Info.followers, following: userInfo.following, bio: userInfo.bio})
+      document.querySelector('.cards').appendChild(finishedProduct);
+      console.log(finishedProduct);
+    })
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
-    https://api.github.com/users/<your name>
+    https://api.github.com/users/cynthiaannberg
 */
 
 /*
@@ -28,7 +41,37 @@
     user, and adding that card to the DOM.
 */
 
+// const followersArray = [];
+// followersArray.forEach((profileURL) => {
+//   const url='https://api.github.com/users/cynthiaannberg/followers'
+// })
+// axios
+
 const followersArray = [];
+
+axios 
+
+  .get("https://api.github.com/users/cynthiaannberg/followers")
+  .then((res) => {
+    res.data.forEach( user => {
+      followersArray.push(user.login);
+    })
+    
+    //iterate over followersArray & create&append a new card.
+    followersArray.forEach( cynthiaannberg => {
+      appendUserCard(cynthiaannberg);
+    })
+  
+  })
+  .catch((error) => {
+    // handle the drama (ie errors)
+    console.log(error);
+  });
+//   .finally(() => {
+// ​
+//     console.log('done');
+
+
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -50,11 +93,54 @@ const followersArray = [];
     </div>
 */
 
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
-*/
+const cards = document.querySelector('.cards');
+
+function cardMaker(obj){
+const cardDiv = document.querySelector('div');
+const userImage = document.querySelector('img');
+const cardInfo = document.querySelector('div');
+const name = document.querySelector('h3');
+const userName = document.querySelector('p');
+const location = documnet.querySelector('p');
+const profile = document.querySelector('p');
+const followers = document.querySelector('p');
+const following = document.querySelector('p');  
+const bio = document.querySelector('p');
+
+newCard.appendChild(profilePic);
+newCard.appendChild(cardInfo);
+cardInfo.appendChild(nameText);
+cardInfo.appendChild(usernameText);
+cardInfo.appendChild(locationText);
+cardInfo.appendChild(profileText);
+profileText.appendChild(githubPageURL);
+cardInfo.appendChild(followersText);
+cardInfo.appendChild(followingText);
+cardInfo.appendChild(bioText);
+
+
+cardDiv.classList.add('card');
+userImage.src = `${image-url-of-user}`; 
+cardInfo.classList.add('name');
+name.classList.add('name');
+name.textContent = name;
+userName.classList.add['username'];
+userName.textContent = `${users-user-name}`;
+location.textContent =  `location: ${users-location}`;
+profile.textContent = `Profile:
+<a href={address to users github page}>${address-to-users-github-page}`
+followers.textContent = `Followers: ${users-followers-count}`
+following.textContent = `Following: ${users-following-count}`;
+bio.textContent = `bio: ${users-bio}`;
+
+
+return cards;
+
+
+}
+  // List of LS Instructors Github username's:
+  //   tetondan
+  //   dustinmyers
+  //   justsml
+  //   luishrd
+  //   bigknell
