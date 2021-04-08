9166a876a7aa73f451888144fcf2327ee0b4499f
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-04-08 12:33:34 -0700

    initial commit

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,32 +1,32 @@
-/*
-  STEP 1: using axios, send a GET request to the following URL
-    (replacing the placeholder with your Github name):
-    https://api.github.com/users/<your name>
-*/
+import axios from "axios"
+  // STEP 1: using axios, send a GET request to the following URL
+  //   (replacing the placeholder with your Github name):
+  //   https://api.github.com/users/<your name>
 
-/*
-  STEP 2: Inspect and study the data coming back, this is YOUR
-    github info! You will need to understand the structure of this
-    data in order to use it to build your component function
 
-    Skip to STEP 3.
-*/
 
-/*
-  STEP 4: Pass the data received from Github into your function,
-    and append the returned markup to the DOM as a child of .cards
-*/
+  // STEP 2: Inspect and study the data coming back, this is YOUR
+  //   github info! You will need to understand the structure of this
+  //   data in order to use it to build your component function
 
-/*
-  STEP 5: Now that you have your own card getting added to the DOM, either
-    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
-    manually find some other users' github handles, or use the list found at the
-    bottom of the page. Get at least 5 different Github usernames and add them as
-    Individual strings to the friendsArray below.
+  //   Skip to STEP 3.
 
-    Using that array, iterate over it, requesting data for each user, creating a new card for each
-    user, and adding that card to the DOM.
-*/
+
+
+  // STEP 4: Pass the data received from Github into your function,
+  //   and append the returned markup to the DOM as a child of .cards
+
+
+
+  // STEP 5: Now that you have your own card getting added to the DOM, either
+  //   follow this link in your browser https://api.github.com/users/<Your github name>/followers,
+  //   manually find some other users' github handles, or use the list found at the
+  //   bottom of the page. Get at least 5 different Github usernames and add them as
+  //   Individual strings to the friendsArray below.
+
+  //   Using that array, iterate over it, requesting data for each user, creating a new card for each
+  //   user, and adding that card to the DOM.
+
 
 const followersArray = [];
 
