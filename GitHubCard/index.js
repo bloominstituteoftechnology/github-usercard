09e8f8bc2a16bd4c9ca0577a26e3be5c2f573573
Rocko Gilbert <rocko2122@gmail.com>
2021-04-15 23:53:42 -0500

    Still not working properly

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,12 @@
+import axios from 'axios';
+
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/<RockoGilbert>')
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -28,8 +31,12 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
-
+const followersArray = ["tetondan", "dustinmyers", "justsml", "luishrd", "bigknell"];
+  followersArray.forEach( item => {
+    axios.get('htttps://api.github.com/users/'+item)
+      .then(response => dispatchEvent.appendChild(guestCard(response)))
+      .catch( err =>console.log(Error))
+  })
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -49,7 +56,74 @@ const followersArray = [];
       </div>
     </div>
 */
+ const card = document.querySelector('.card')
 
+ function guestCard(info){
+   const {data} = info;
+    console.log(data)
+ }
+   //Create Element
+   let userCard = document.createElement('div')
+   let image = document.createElement('img')
+   let cardInfo = document.createElement('div')
+   let className = document.createElement(h3)
+   let userName = createElement('p')
+   let location = createElement('p')
+   let profile = document.createElement('p')
+   let aTag = document.createElement('a')
+   let followers = document.createElement('p')
+   let following = document.createElement('p')
+   let bio = document.createElement('p') 
+
+  // ClassList
+    userCard.classList.add('card')
+    cardInfo.classList.add('card-info')
+    className.classList.add('name')
+    userName.classList.add('userName')
+    
+  // Append
+    card.appendChild(userCard)
+    userCard.appendChild(image)
+    image.appendChild(cardInfo)
+    cardInfo.appendChild(className)
+    cardInfo.appendChild(userName)
+    cardInfo.appendChild(location)
+    cardInfo.appendChild(profile)
+    profile.appendChild(aTag)
+    cardInfo.appendChild(followers)
+    cardInfo.appendChild(following)
+    cardInfo.appendChild(bio)
+
+
+  //Text
+    image.src = data.avatar_url;
+    aTag.setAttribute('target', '_blank')
+    className.textContent = 'data.name'
+    userName.textContent = 'data.login'
+    location.textContent = 'data.location'
+    aTag.textContent = data.avatar_url;
+    aTag.setAttribute('href', data.url);
+    followers.textContent = 'Followers: ' + data.followers
+    following.textContent = 'Following: ' + data.following
+    bio.textContent = data.bio;
+  
+
+  //Get data
+    axios.get('https://api.github.com/users/RockoGilbert')
+      .then(response => {
+        console.log('response');
+      })
+
+      .catch(err => {
+        console.log('error from rejected promise', err)
+      })
+      
+      
+
+ 
+
+ return card
+ 
 /*
   List of LS Instructors Github username's:
     tetondan
