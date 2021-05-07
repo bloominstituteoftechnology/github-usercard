90c6887fe3e2441fdce5e10b9b7763d598feeb5c
John Laubscher <jlmemphiscommanders@gmail.com>
2021-05-07 11:46:30 -0500

    debugged, it works

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -7,16 +7,15 @@ import axios from "axios";
 
 const result = axios.get("https://api.github.com/users/john-laubscher");
 
-
 axios
-.get("https://api.github.com/users/john-laubscher")
-  console.log("1. here is the result", result)
-.then((futureData) =>{
-    console.log("2. here is future data", futureData)
+  .get("https://api.github.com/users/john-laubscher")
+  .then((futureData) => {
+    console.log("2. here is future data", futureData);
   })
-.catch((error) =>{
-    console.log(error)
+  .catch((error) => {
+    console.log(error);
   });
+console.log("1. here is the result", result);
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -42,13 +41,6 @@ axios
 */
 //#5 adding data to followers array---how do I request data for a user???---function will create the card and add it after I call it for the new array. Will I need to add new user data to the result array or call the new array?
 
-const newArray = axios.get("https://api.github.com/users/john-laubscher");
-followersArray.push("deG3nt3lm4n", "bgoonz", "esin", "Magicianred")
-
-cardCreator(newArray);
-
-const followersArray = [];
-
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -69,9 +61,9 @@ const followersArray = [];
     </div>
 */
 //creating element to append to
-const cards = document.querySelector(".cards")
-const creator = function cardCreator(cardObj) {
-
+const cards = document.querySelector(".cards");
+function cardCreator(cardObj) {
+  console.log("cardObj in card", cardObj);
   //creating html
   const cardContainer = document.createElement("div");
   const img = document.createElement("img");
@@ -84,15 +76,15 @@ const creator = function cardCreator(cardObj) {
   const followers = document.createElement("p");
   const following = document.createElement("p");
   const userBio = document.createElement("p");
-  
+
   //creating scaffolding
-cardContainer.appendChild(img);
-cardContainer.appendChild(cardInfo);
+  cardContainer.appendChild(img);
+  cardContainer.appendChild(cardInfo);
   cardInfo.appendChild(realName);
   cardInfo.appendChild(userName);
   cardInfo.appendChild(location);
   cardInfo.appendChild(profileContainer);
-    cardInfo.appendChild(gitLink);
+  cardInfo.appendChild(gitLink);
   cardInfo.appendChild(followers);
   cardInfo.appendChild(following);
   cardInfo.appendChild(userBio);
@@ -104,32 +96,27 @@ cardContainer.appendChild(cardInfo);
   userName.classList.add("username");
 
   //passing in values from the array
-  img.textContent = cardObj.avatar_url
-  realName.textContent = cardObj.name
-  userName.textContent = cardObj.login
-  location.textContent = cardObj.location
-  gitLink.textContent = cardObj.html_url
-  followers.textContent = cardObj.followers
-  following.textContent = cardObj.following
-  userBio.textContent = cardObj.bio
+  img.src = cardObj.data.avatar_url;
+  realName.textContent = cardObj.data.name;
+  userName.textContent = cardObj.data.login;
+  location.textContent = cardObj.data.location;
+  gitLink.textContent = cardObj.data.html_url;
+  followers.textContent = cardObj.data.followers;
+  following.textContent = cardObj.data.following;
+  userBio.textContent = cardObj.data.bio;
+  // returning new card
+  // const newCardObj = result.map((resultItem) => {       //what is this error over const?--what is a parsing error?
+  //   console.log(newCardObj)
+  cards.appendChild(cardContainer);
+  return cardContainer;
 
-  console.log((img)
-//returning new card
-  const newCardObj = result.map((resultItem) => {       //what is this error over const?--what is a parsing error?
-    console.log(newCardObj)
-    return cardCreator(resultItem);
-
-
-});
-//iterating and appending to html
-newCardObj.forEach((resultElement) => {
-  cards.appendChild(resultElement);
-})
-
-
+  // });
+  // iterating and appending to html
+  // newCardObj.forEach((resultElement) => {
+  //   cards.appendChild(resultElement);
+  // })
 }
-cardCreator(result);
-
+// cardCreator(result);
 
 /*
   List of LS Instructors Github username's:
@@ -139,3 +126,43 @@ cardCreator(result);
     luishrd
     bigknell
 */
+const followersArray = [];
+followersArray.push(
+  "deG3nt3lm4n",
+  "bgoonz",
+  "esin",
+  "Magicianred",
+  "john-laubscher"
+);
+
+const cardsArray = [];
+
+followersArray.forEach((githubName) => {
+  var card;
+
+  axios //if we're doing something with the network data, we need to wait until it arrives
+    .get(`https://api.github.com/users/${githubName}`)
+    .then((userData) => {
+      card = cardCreator(userData);
+      console.log("userData", userData);
+      cardsArray.push(card);
+      console.log(cardsArray);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+/* async function axiosCreator(githubName) {
+  const userData = axios                            //if we're doing something with the network data, we need to wait until it arrives
+    .get(`https://api.github.com/users/${githubName}`)
+    .then((futureData) => {
+      console.log("2. here is future data", futureData);
+      return futureData;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  return userData;
+} */
