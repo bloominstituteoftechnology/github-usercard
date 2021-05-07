d519111601f95a4bafec294e789ba1c8c58bdd77
Taormina7575 <vinnie.taormina@gmail.com>
2021-05-06 22:18:52 -0400

    final

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -6,11 +6,14 @@ import axios from 'axios';
 */
 axios.get('https://api.github.com/users/Taormina7575')
       .then((myData) => {
-        console.log(myData.data)
+        console.log(myData.data.html_url)
+        let card = makeCard(myData.data);
+        document.querySelector('.cards').appendChild(card)
       })
       .catch((err) => {
         console.log(err)
       })
+
 
 
   
@@ -38,8 +41,24 @@ axios.get('https://api.github.com/users/Taormina7575')
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  'tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell',
+];
 
+followersArray.forEach((userTitle) => {
+  axios.get(`https://api.github.com/users/${userTitle}`)
+        .then((userData) => {
+          let newCard = makeCard(userData.data);
+          document.querySelector('.cards').appendChild(newCard);
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+})
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -61,7 +80,48 @@ const followersArray = [];
 */
 function makeCard({avatar_url, name, login, location, html_url, followers, following, bio}) {
   let wrapDiv = document.createElement('div')
+  let img = document.createElement('img')
+  let infoDiv = document.createElement('div')
+  let personName = document.createElement('h3')
+  let userName = document.createElement('p')
+  let local = document.createElement('p')
+  let profileWrap = document.createElement('p')
+  let profileLink = document.createElement('a')
+  let followersCount = document.createElement('p')
+  let followingCount = document.createElement('p')
+  let bioWrap = document.createElement('p')
+
+  wrapDiv.appendChild(img)
+  wrapDiv.appendChild(infoDiv)
+  infoDiv.appendChild(personName)
+  infoDiv.appendChild(userName)
+  infoDiv.appendChild(local)
+  infoDiv.appendChild(profileWrap)
+  profileWrap.appendChild(profileLink)
+  infoDiv.appendChild(followersCount)
+  infoDiv.appendChild(followingCount)
+  infoDiv.appendChild(bioWrap)
   
+
+  wrapDiv.classList.add('card')
+  infoDiv.classList.add('card-info')
+  personName.classList.add('name')
+  userName.classList.add('username')
+  
+  console.log(html_url)
+
+  img.src = avatar_url;
+  personName.textContent = name;
+  userName.textContent = login;
+  local.textContent = `Location: ${location}`;
+  profileWrap.textContent = `Profile: `;
+  profileLink.textContent = `https://github.com/${login}`;
+  profileLink.src = html_url;
+  followersCount.textContent = `Followers: ${followers}`
+  followingCount.textContent = `Following: ${following}`;
+  bioWrap.textContent = bio;
+
+  return wrapDiv;
 }
 /*
   List of LS Instructors Github username's:
