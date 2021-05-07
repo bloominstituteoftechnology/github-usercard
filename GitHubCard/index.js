590522897cb47b3acb902da0ddd24540523f90b1
John Laubscher <jlmemphiscommanders@gmail.com>
2021-05-07 00:42:46 -0500

    finished most. What is 5 even asking me to do? I'm supposed to request data, and then somehow that new data is imported into my friendsArray so that I can use it to create new cards?

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -6,7 +6,7 @@
 import axios from "axios";
 
 const result = axios.get("https://api.github.com/users/john-laubscher");
-console.log(result)
+
 
 axios
 .get("https://api.github.com/users/john-laubscher")
@@ -40,6 +40,11 @@ axios
     Using that array, iterate over it, requesting data for each user, creating a new card for each
     user, and adding that card to the DOM.
 */
+//#5 adding data to followers array---how do I request data for a user???---function will create the card and add it after I call it for the new array. Will I need to add new user data to the result array or call the new array?
+
+const newArray = axios.get("https://api.github.com/users/john-laubscher");
+followersArray.push("deG3nt3lm4n", "bgoonz", "esin", "Magicianred")
+
 
 const followersArray = [];
 
@@ -62,6 +67,68 @@ const followersArray = [];
       </div>
     </div>
 */
+//creating element to append to
+const cards = document.querySelector(".cards")
+const creator = function cardCreator(cardObj) {
+
+  //creating html
+  const cardContainer = document.createElement("div");
+  const img = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const realName = document.createElement("h3");
+  const userName = document.createElement("p");
+  const location = document.createElement("p");
+  const profileContainer = document.createElement("p");
+  const gitLink = document.createElement("a");
+  const followers = document.createElement("p");
+  const following = document.createElement("p");
+  const userBio = document.createElement("p");
+  
+  //creating scaffolding
+cardContainer.appendChild(img);
+cardContainer.appendChild(cardInfo);
+  cardInfo.appendChild(realName);
+  cardInfo.appendChild(userName);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profileContainer);
+    cardInfo.appendChild(gitLink);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(userBio);
+
+  // adding classes
+  cardContainer.classList.add("card");
+  cardInfo.classList.add("card-info");
+  realName.classList.add("name");
+  userName.classList.add("username");
+
+  //passing in values from the array
+  img.textContent = cardObj.avatar_url
+  realName.textContent = cardObj.name
+  userName.textContent = cardObj.login
+  location.textContent = cardObj.location
+  gitLink.textContent = cardObj.html_url
+  followers.textContent = cardObj.followers
+  following.textContent = cardObj.following
+  userBio.textContent = cardObj.bio
+
+  console.log((img)
+//returning new card
+  const newCardObj = result.map((resultItem) => {       //what is this error over const?--what is a parsing error?
+    console.log(newCardObj)
+    return cardCreator(resultItem);
+
+
+});
+//iterating and appending to html
+newCardObj.forEach((resultElement) => {
+  cards.appendChild(resultElement);
+})
+
+
+}
+cardCreator(result);
+
 
 /*
   List of LS Instructors Github username's:
