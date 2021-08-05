ad0b9790444ef307fe47e0cf9f1272cd4cd146ac
Arison13 <arisonarias@gmail.com>
2021-08-05 16:52:20 -0400

    finished thru step 3 and html

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,12 +3,18 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+const myName = axios.get('https://api.github.com/users/Arison13')
+  .then(response => {
+    console.log(myName);
+  })
+  .catch (err =>{
+    console.error(err);
+  })
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
     data in order to use it to build your component function
-
+  
     Skip to STEP 3 (line 34).
 */
 
@@ -34,7 +40,47 @@ const followersArray = [];
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
 
-    <div class="card">
+    
+*/
+const cards = document.querySelector('.cards');
+
+function makeCard(object){
+  //creating elements
+  const containerDiv = document.createElement('div')
+  const image = document.createElement('img')
+  const secondDiv = document.createElement('div')
+  const h3 = document.createElement('h3')
+  const p1 = document.createElement('p')
+  const p2 = document.createElement('p')
+  const p3 = document.createElement('p')
+  const a = document.createElement('a')
+  const p4 = document.createElement('p')
+  const p5 = document.createElement('p')
+  const p6 = document.createElement('p')
+  //kids and hierarchy
+  containerDiv.appendChild(image)
+  containerDiv.appendChild(secondDiv)
+  secondDiv.appendChild(h3)
+  secondDiv.appendChild(p)
+  secondDiv.appendChild(p)
+  secondDiv.appendChild(p)
+  secondDiv.appendChild(a)
+  secondDiv.appendChild(p)
+  secondDiv.appendChild(p)
+  secondDiv.appendChild(p)
+//adding classes
+  containerDiv.classList.add('card')
+  secondDiv.classList.add('card-info')
+  h3.classList.add('name')
+  p1.classList.add('username')
+//content
+p2.textContent = 'Location: '
+p4.textContent = 'Followers: '
+p5.textContent = 'Following: '
+p6.textContent = 'Bio: '
+}
+
+{/* <div class="card">
       <img src={image url of user} />
       <div class="card-info">
         <h3 class="name">{users name}</h3>
@@ -47,9 +93,7 @@ const followersArray = [];
         <p>Following: {users following count}</p>
         <p>Bio: {users bio}</p>
       </div>
-    </div>
-*/
-
+    </div> */}
 /*
   List of LS Instructors Github username's:
     tetondan
