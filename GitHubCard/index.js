6d9f982cebab58cae607b95766300bb02b33c016
Arison13 <arisonarias@gmail.com>
2021-08-05 17:30:02 -0400

    arison-arias

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,12 +4,13 @@
     https://api.github.com/users/<your name>
 */
 const myName = axios.get('https://api.github.com/users/Arison13')
-  .then(response => {
-    console.log(myName);
-  })
-  .catch (err =>{
-    console.error(err);
-  })
+  console.log(myName);
+// .then(response => {
+  //   console.log(myName);
+  // })
+  // .catch (err =>{
+  //   console.error(err);
+  // })
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -33,17 +34,32 @@ const myName = axios.get('https://api.github.com/users/Arison13')
     Using that array, iterate over it, requesting data for each user, creating a new card for each
     user, and adding that card to the DOM.
 */
-
-const followersArray = [];
+const cards = document.querySelector('.cards');
+const followersArray = ["tetondan", "dustinmyers", "justsml", "luishrd", "bigknell"];
+followersArray.forEach(person => {
+  const newPerson = axios.get(`https://api.github.com/users/${person}`)
+  makeCard(newPerson)
+  cards.appendChild(makeCard(newPerson));
+})
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
-
-    
+{/* <div class="card">
+      <img src={image url of user} />
+      <div class="card-info">
+        <h3 class="name">{users name}</h3>
+        <p class="username">{users user name}</p>
+        <p>Location: {users location}</p>
+        <p>Profile:
+          <a href={address to users github page}>{address to users github page}</a>
+        </p>
+        <p>Followers: {users followers count}</p>
+        <p>Following: {users following count}</p>
+        <p>Bio: {users bio}</p>
+      </div>
+    </div> }
 */
-const cards = document.querySelector('.cards');
-
 function makeCard(object){
   //creating elements
   const containerDiv = document.createElement('div')
@@ -61,39 +77,34 @@ function makeCard(object){
   containerDiv.appendChild(image)
   containerDiv.appendChild(secondDiv)
   secondDiv.appendChild(h3)
-  secondDiv.appendChild(p)
-  secondDiv.appendChild(p)
-  secondDiv.appendChild(p)
+  secondDiv.appendChild(p1)
+  secondDiv.appendChild(p2)
+  secondDiv.appendChild(p3)
   secondDiv.appendChild(a)
-  secondDiv.appendChild(p)
-  secondDiv.appendChild(p)
-  secondDiv.appendChild(p)
+  secondDiv.appendChild(p4)
+  secondDiv.appendChild(p5)
+  secondDiv.appendChild(p6)
+  p3.appendChild(a)
 //adding classes
   containerDiv.classList.add('card')
   secondDiv.classList.add('card-info')
   h3.classList.add('name')
   p1.classList.add('username')
-//content
-p2.textContent = 'Location: '
-p4.textContent = 'Followers: '
-p5.textContent = 'Following: '
-p6.textContent = 'Bio: '
+object
+  .then(response => {
+    image.src = response.data.avatar_url
+    h3.textContent = response.data.name
+    p1.textContent = response.data.login
+    p2.textContent = (`Location: ${response.data.location}`)
+    a.textContent = `Profile: ${response.data.html_url}`
+    p4.textContent = `Followers: ${response.data.followers}`
+    p5.textContent = `Following: ${response.data.following}`
+    p6.textContent = `Bio: ${response.data.bio}`
+  })
+  return containerDiv
 }
 
-{/* <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div> */}
+
 /*
   List of LS Instructors Github username's:
     tetondan
