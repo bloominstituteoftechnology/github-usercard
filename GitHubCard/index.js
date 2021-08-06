e81f7252d97a6a75fcbf19c05611257b4d31ace1
vgaranin <vasilii_garanin@laptevfamily.com>
2021-08-06 00:56:16 -0500

    function cardCreator made

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -5,17 +5,26 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-axios.get("https://api.github.com/users/vasilii-garanin")
-    .then(
-        response =>
-        {
-            console.log("response");
-        })
-    .catch(error =>
-    {
-        console.log("error");
-    });
+const cards = document.querySelector(".cards");
+cardsCreator(["vasilii-garanin"]);
+// cardsCreator(["anastasia-lapteva", "BrityHemming", "austenallred", "tetondan", "dustinmyers", "justsml"]);
 
+function cardsCreator(usersArray)
+{
+    usersArray.forEach(username =>
+    {
+        axios.get(`https://api.github.com/users/${username}`)
+            .then(response =>
+            {
+                const card = cardCreator(response.data);
+                cards.appendChild(card);
+            })
+            .catch(error =>
+            {
+                console.error("error");
+            });
+    });
+}
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -40,8 +49,8 @@ axios.get("https://api.github.com/users/vasilii-garanin")
     user, and adding that card to the DOM.
 */
 
-const followersArray = ["anastasia-lapteva"];
-const FollowingArray = ["BrityHemming", "austenallred", "tetondan", "dustinmyers", "justsml"];
+// const followersArray = ["anastasia-lapteva", "BrityHemming", "austenallred", "tetondan", "dustinmyers", "justsml"];
+// cardCreator(followersArray);
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -61,45 +70,51 @@ const FollowingArray = ["BrityHemming", "austenallred", "tetondan", "dustinmyers
       </div>
     </div>
 */
-const card = document.querySelector("div");
-const cardImg = document.querySelector("img");
-const cardInfo = document.querySelector('div');
-const cardInfoName = document.querySelector("h3");
-const cardInfoUserName = document.querySelector("p");
-const cardInfoLocation = document.querySelector("p");
-const cardInfoProfile = document.querySelector("p");
-const cardInfoProfileUrl = document.querySelector("a");
-const cardInfoFollowers = document.querySelector("p");
-const cardInfoFollowing = document.querySelector("p");
-const cardInfoBio = document.querySelector("p");
+function cardCreator(data)
+{
+    const card = document.querySelector("div");
+    const cardImg = document.querySelector("img");
+    const cardInfo = document.querySelector('div');
+    const cardInfoName = document.querySelector("h3");
+    const cardInfoUserName = document.querySelector("p");
+    const cardInfoLocation = document.querySelector("p");
+    const cardInfoProfile = document.querySelector("p");
+    const cardInfoProfileUrl = document.querySelector("a");
+    const cardInfoFollowers = document.querySelector("p");
+    const cardInfoFollowing = document.querySelector("p");
+    const cardInfoBio = document.querySelector("p");
 
-card.classList.add("card");
-cardImg.src = data["avatar_url"];
-cardInfo.classList.add("card-info");
-cardInfoName.classList.add("name");
-cardInfoUserName.classList.add("username");
-cardInfoName.textContent = data["name"];
-cardInfoLocation.textContent = data["location"];
-cardInfoProfileUrl.href = `Location:${data["location"]}`;
-cardInfoProfile.textContent = "Profile: ";
-cardInfoProfileUrl.href = data["html_url"];
-cardInfoProfileUrl.textContent = data["html_url"];
-cardInfoFollowers.textContent = `Followers: ${data["followers"]}`;
-cardInfoFollowing.textContent = `Following: ${data["following"]}`;
-cardInfoBio.textContent = data["bio"];
+    card.classList.add("card");
+    cardInfo.classList.add("card-info");
+    cardInfoName.classList.add("name");
+    cardInfoUserName.classList.add("username");
 
 
-card.appendChild(cardImg);
-card.appendChild(cardInfo);
-cardInfo.appendChild(cardInfoName);
-cardInfo.appendChild(cardInfoUserName);
-cardInfo.appendChild(cardInfoLocation);
-cardInfo.appendChild(cardInfoProfile);
-cardInfoProfile.appendChild(cardInfoProfileUrl);
-cardInfo.appendChild(cardInfoFollowers);
-cardInfo.appendChild(cardInfoFollowing);
-cardInfo.appendChild(cardInfoBio);
+    cardImg.src = data["avatar_url"];
+    cardInfoName.textContent = data["name"];
+    cardInfoUserName.textContent = data["login"];
+    cardInfoLocation.textContent = `Location:${data["location"]}`;
+    cardInfoProfile.textContent = "Profile: ";
+    cardInfoProfileUrl.href = data["html_url"];
+    cardInfoProfileUrl.textContent = data["html_url"];
+    cardInfoFollowers.textContent = `Followers: ${data["followers"]}`;
+    cardInfoFollowing.textContent = `Following: ${data["following"]}`;
+    cardInfoBio.textContent = data["bio"];
 
+
+    card.appendChild(cardImg);
+    card.appendChild(cardInfo);
+    cardInfo.appendChild(cardInfoName);
+    cardInfo.appendChild(cardInfoUserName);
+    cardInfo.appendChild(cardInfoLocation);
+    cardInfo.appendChild(cardInfoProfile);
+    cardInfoProfile.appendChild(cardInfoProfileUrl);
+    cardInfo.appendChild(cardInfoFollowers);
+    cardInfo.appendChild(cardInfoFollowing);
+    cardInfo.appendChild(cardInfoBio);
+
+    return card;
+}
 
 
 
