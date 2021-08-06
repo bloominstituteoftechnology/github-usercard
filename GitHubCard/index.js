8563ff5743de2577aef3f0874aa8ffcf5fe670ba
vgaranin <vasilii_garanin@laptevfamily.com>
2021-08-05 23:48:17 -0500

    All classes assigned, all HTML elements created js, all elements nested

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,20 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+axios.get("https://api.github.com/users/vasilii-garanin")
+    .then(
+        response =>
+        {
+            console.log("response");
+        })
+    .catch(error =>
+    {
+        console.log("error");
+    });
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -28,8 +40,8 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
-
+const followersArray = ["anastasia-lapteva"];
+const FollowingArray = ["BrityHemming", "austenallred", "tetondan", "dustinmyers", "justsml"];
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -49,6 +61,55 @@ const followersArray = [];
       </div>
     </div>
 */
+const card = document.querySelector("div");
+const cardImg = document.querySelector("img");
+const cardInfo = document.querySelector('div');
+const cardInfoName = document.querySelector("h3");
+const cardInfoUserName = document.querySelector("p");
+const cardInfoLocation = document.querySelector("p");
+const cardInfoProfile = document.querySelector("p");
+const cardInfoProfileUrl = document.querySelector("a");
+const cardInfoFollowers = document.querySelector("p");
+const cardInfoFollowing = document.querySelector("p");
+const cardInfoBio = document.querySelector("p");
+
+card.classList.add("card");
+cardImg.src = data["avatar_url"];
+cardInfo.classList.add("card-info");
+cardInfoName.classList.add("name");
+cardInfoUserName.classList.add("username");
+cardInfoName.textContent = data["name"];
+cardInfoLocation.textContent = data["location"];
+cardInfoProfileUrl.href = `Location:${data["location"]}`;
+cardInfoProfile.textContent = "Profile: ";
+cardInfoProfileUrl.href = data["html_url"];
+cardInfoProfileUrl.textContent = data["html_url"];
+cardInfoFollowers.textContent = `Followers: ${data["followers"]}`;
+cardInfoFollowing.textContent = `Following: ${data["following"]}`;
+cardInfoBio.textContent = data["bio"];
+
+
+card.appendChild(cardImg);
+card.appendChild(cardInfo);
+cardInfo.appendChild(cardInfoName);
+cardInfo.appendChild(cardInfoUserName);
+cardInfo.appendChild(cardInfoLocation);
+cardInfo.appendChild(cardInfoProfile);
+cardInfoProfile.appendChild(cardInfoProfileUrl);
+cardInfo.appendChild(cardInfoFollowers);
+cardInfo.appendChild(cardInfoFollowing);
+cardInfo.appendChild(cardInfoBio);
+
+
+
+
+
+
+
+
+
+
+
 
 /*
   List of LS Instructors Github username's:
