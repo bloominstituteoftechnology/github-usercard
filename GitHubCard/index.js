8f262d7f699da03f029a0b3ad75fbc1a89f436a7
SheebaHashmi <sheebahashmi0@gmail.com>
2021-10-28 13:49:41 -0400

    Complete step 1

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,15 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+import axios from "axios";
+
+axios.get('https://api.github.com/users/SheebaHashmi')
+.then(res => {
+  console.log(res.data)
+})
+.catch(err => {
+  console.error(err)
+})
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
