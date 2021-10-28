ce9e6e54ecf4113e4260cebe2ed48bdd51333309
Nithya Narayanan <n.nithya@pm.me>
2021-10-28 11:26:27 -0700

    Completed step 1 .

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,12 @@
+import axios from "axios";
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/narayanan-nithya').then(res =>{
+  console.log(res);
+})
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
