425fd405a249fbc8fb3dcfb72402ad2226720e40
Ryan Howard <ryanghoward@protonmail.com>
2021-10-28 11:52:01 -0700

    Axios imported and step 1 done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,15 @@
+import axios from 'axios';
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+axios.get('https://api.github.com/users/ryanghoward')
+  .then(res => {
+    console.log(res);
+  })
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
