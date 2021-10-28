857eedad1ce31ee5068ab9f2a9df08b9860eef9d
Bobby Gomez <bobbygomez@Bobbys-MacBook-Pro.local>
2021-10-28 15:11:06 -0500

    Homework with Single Object Complete

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,11 +1,19 @@
-import axios from 'axios';
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+const cardsElem = document.querySelector('.cards');
+import axios from 'axios';
+  axios.get('https://api.github.com/users/itsmerobert12')
+  .then(resp => {
+        cardsElem.appendChild(makeCards(resp.data))
+        console.log(resp)
+    }) .catch(err => {
+        console.error(err)
+})
 
-axios.get('https://api.github.com/users/itsmerobert12');
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github cardInfo! You will need to understand the structure of this
@@ -29,7 +37,15 @@ axios.get('https://api.github.com/users/itsmerobert12');
     Using that array, iterate over it, requesting data for each user, creating a new card for each
     user, and adding that card to the DOM.
 */
-
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell']
+followersArray.forEach(element => {
+  axios.get(`https://api.github.com/users/${element}`)
+  .then(resp => {
+    cardsElem.appendChild(makeCards(resp.data))
+}) .catch(err => {
+    console.error(err)
+  })
+})
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -50,81 +66,6 @@ axios.get('https://api.github.com/users/itsmerobert12');
     </div>
 */
 
-function cardMaker({avatar_url, name, login, location, html_url, followers, following, bio}) {
-
-
-  const card = document.createElement('div')
-  const avatar = document.createElement('img')
-  const div2 = document.createElement('div')
-  const h3 = document.createElement('h3')
-  const userName = document.createElement('p')
-  const userLocation = document.createElement('p')
-  const userProfile = document.createElement('p')
-  const userLink = document.createElement('a')
-  const userFollowers = document.createElement('p')
-  const userFollowing = document.createElement('p')
-  const userBio = document.createElement('p')
-
-  avatar.src = avatar_url
-  h3.textContent = name
-  userName.textContent = login
-  userLocation.textContent = `Location: ${location}`
-  userProfile.textContent = `Profile: ${html_url}`
-  userFollowers.textContent = `Followers: ${followers}`
-  userFollowing.textContent = `Following: ${following}`
-  userBio.textContent = `Bio: ${bio}`
-
-
-  card.classList.add('card')
-  div2.classList.add('card-info')
-  h3.classList.add('name')
-  userName.classList.add('username')
-
-  // card.append('.cards')
-  card.appendChild(avatar)
-  card.appendChild(div2)
-  div2.appendChild(h3)
-  div2.appendChild(userName)
-  div2.appendChild(userLocation)
-  div2.appendChild(userProfile)
-  userProfile.appendChild(userLink)
-  div2.appendChild(userFollowers)
-  div2.appendChild(userFollowing)
-  div2.appendChild(userBio)
-
-  return card;
-}
-
-
-function getInfo() {
-  axios.get(`https://api.github.com/users/itsmerobert12`)
-  .then(res => {
-    console.log(res.data)
-    const cardsElement = document.querySelector('.cards');
-    const newCard = cardMaker(res.data)
-    cardsElement.appendChild(newCard)
-  })
-  .catch(err => {
-    console.error(err);
-  })
-  .finally(() => {
-    console.log('get request')
-  })
-}
-getInfo()
-
-const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell',];
-followersArray.forEach(elem => {
-  axios.get(`https://api.github.com/users/${elem}`)
-  .then(resp =>{
-    const info = resp.data
-    const cardsElement = document.querySelector('.cards');
-    cardsElement.appendChild(cardMaker(info))
-  })
-  .catch(err => {
-    console.error(err)
-  })
-})
 
 /*
   List of LS Instructors Github username's:
@@ -136,3 +77,50 @@ followersArray.forEach(elem => {
 */
 
 
+function makeCards(object) {
+
+  const githubCard = document.createElement('div')
+  const profileImg = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const usersName = document.createElement('h3')
+  const usersUserName = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p')
+  const profileLink = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
+
+  githubCard.classList.add('card')
+  cardInfo.classList.add('card-info')
+  usersName.classList.add('name')
+  usersUserName.classList.add('username')
+
+  profileImg.src = object.avatar_url
+  usersName.textContent = object.name
+  usersUserName.textContent = object.username
+  location.textContent = object.users_location
+  profile.textContent = object.address
+  profileLink.textContent = object.html_url
+  followers.textContent = object.followers
+  following.textContent = object.following
+  bio.textContent = object.bio
+
+  profileImg.setAttribute('src', object.avatar_url)
+
+
+  githubCard.appendChild(profileImg);
+  githubCard.appendChild(cardInfo);
+  cardInfo.appendChild(usersName);
+  cardInfo.appendChild(usersUserName);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+  profile.appendChild(profileLink);
+
+  console.log(githubCard)
+
+  return githubCard;
+}
