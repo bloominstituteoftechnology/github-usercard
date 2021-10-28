cabd62112b6734cbf9b8e6339bc8a921af587d7f
Tuan147 <90153951+Tuan147@users.noreply.github.com>
2021-10-28 16:18:41 -0400

    finished tasks

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,17 @@
+const { default: axios } = require("axios");
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+import axios from 'axios'
+axios.get('https://api.github.com/users/Tuan147')
+.then(res => { 
+  const cardData = usercardMaker(res.data)
+})
+.catch(err => console.error(err))
+
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -16,6 +25,54 @@
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
+const myCard = document.querySelector('.cards');
+
+function usercardMaker ({ avatar_url, name, login, location, html_url, followers, following, bio}){
+
+
+  const card = document.createElement('div');
+  const avt = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const realName = document.createElement('h3');
+  const userName = document.createElement('p');
+  const userLocation = document.createElement('p');
+  const userProfile = document.createElement('p');
+  const userGitLink = document.createElement('a');
+  const userFollowers = document.createElement('p');
+  const userFollowing = document.createElement('p');
+  const userBio = document.createElement('p');
+  
+  card.appendChild(avt);
+  card.appendChild(cardInfo);
+  card.appendChild(realName);
+  card.appendChild(userName);
+  card.appendChild(userLocation);
+  card.appendChild(userProfile);
+  card.appendChild(userGitLink);
+  card.appendChild(userFollowers);
+  card.appendChild(userFollowing);
+  card.appendChild(userBio);
+
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  realName.classList.add('name');
+  userName.classList.add('username');
+  avt.src = avatar_url;
+  realName.textContent = `${name}`;
+  userName.textContent = `${login}`;
+  userLocation.textContent = `Location: ${location}`;
+  userProfile.textContent = `Profile: `;
+  userGitLink.setAttribute('href', `${html_url}`);
+  userProfile.appendChild(userGitLink);
+  userFollowers.textContent.add(`Followers: ${followers}`);
+  userFollowing.textContent.add(`Following: ${following}`);
+  userBio.textContent.add(`Bio: ${bio}`);
+
+  return card;
+  
+  }
+
+
 
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
