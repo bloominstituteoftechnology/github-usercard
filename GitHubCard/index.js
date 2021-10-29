a37901a68c8e6868fa63b71a2a27b96fa8cdd69a
SafiaOmar <90978408+SafiaOmar@users.noreply.github.com>
2021-10-28 20:01:23 -0400

    done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -29,7 +29,7 @@ console.error(err)
 
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
-    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
+    follow this link in your browser https://api.github.com/users/SafiaOmar/followers ,
     manually find some other users' github handles, or use the list found at the
     bottom of the page. Get at least 5 different Github usernames and add them as
     Individual strings to the friendsArray below.
