40321185011fc017c79efd0695f956f5bd4f4971
Nyamekye Annor <nyamekyeannor@Nyamekyes-MacBook-Pro.local>
2021-10-28 20:19:31 -0500

    DONE

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -62,6 +62,55 @@ const followersArray = [];
     bigknell
 */
 
+// not sure if this will work
+// function cardMaker (obj){
+//   const card = document.createElement('div')
+//   const img = document.createElement('img');
+//   const cardInfo = document.createElement('div');
+//   const name = document.createElement('h3')
+//   const username = document.createElement('p')
+//   const location = document.createElement('p')
+//   const profile = document.createElement('p')
+//   const link = document.createElement('a')
+//   const followers = document.createElement('p')
+//   const following = document.createElement('p')
+//   const bio = document.createElement('p')
+
+//   card.classList.add('card');
+//   cardInfo.classList.add('card-info');
+//   name.classList.add('name');
+//   username.classList.add('username');
+
+//   img.setAttribute('src,obj.avatar_url');
+//   name.textContent = obj.name;
+//   username.textContent = obj.login;
+//   location.textContent = obj.location;
+//   profile.textContent = 'Profile  ';
+//   link.textContent = obj.url;
+//   link.href = obj.url;
+//   followers.textContent = `Followers: ${obj.followers}`;
+//   following.textContent = `Followers: ${obj.followers}`;
+
+//   card.appendChild(img);
+//   card.appendChild(cardInfo);
+//   cardInfo.appendChild(name);
+//   cardInfo.appendChild(username);
+//   cardInfo.appendChild(location);
+//   cardInfo.appendChild(profile);
+//   profile.appendChild(link);
+//   cardInfo.appendChild(followers);
+//   cardInfo.appendChild(following);
+//   cardInfo.appendChild(bio);
+//   return card;
+
+// }
+
+
+
+
+
+// go back to this is all fails 
+
 const cards = document.querySelector('.cards');
 
 function cardMaker (gitHubTag){
@@ -81,14 +130,33 @@ function cardMaker (gitHubTag){
   const following = document.createElement('p');
   const bio = document.createElement('p');
 
-  img.src = gitHubTag.avatar_url;
-   name.textContent =gitHubTag.name;
-  username.textContent = 'Username:' + gitHubTag.login;
-  location.textContent = 'Location:' + gitHubTag.location;
-  profileAddress.textContent = 'URL: ' + gitHubTag.url;
-  followers.textContent = 'Followers:' + gitHubTag.followers;
-  following.textContent = 'following:' + gitHubTag.following;
-  bio.textContent = 'Bio:' + gitHubTag.bio;
+  axios.get (`https://api.github.com/users/${gitHubTag}`)
+  .then(res => {
+    const cards = document.querySelector('.cards')
+    const cardReturn = cardMaker(res.data)
+
+    img.src = res.data.avatar_url;
+   name.textContent =res.data.name;
+  username.textContent = 'Username:' + res.data.login;
+  location.textContent = 'Location:' + res.data.location;
+  profileAddress.textContent = 'URL: ' + res.data.url;
+  followers.textContent = 'Followers:' + res.data.followers;
+  following.textContent = 'following:' + res.data.following;
+  bio.textContent = 'Bio:' + res.data.bio;
+    
+  })
+  .catch(err => {
+    console.error(err)
+  });
+
+  // img.src = gitHubTag.avatar_url;
+  //  name.textContent =gitHubTag.name;
+  // username.textContent = 'Username:' + gitHubTag.login;
+  // location.textContent = 'Location:' + gitHubTag.location;
+  // profileAddress.textContent = 'URL: ' + gitHubTag.url;
+  // followers.textContent = 'Followers:' + gitHubTag.followers;
+  // following.textContent = 'following:' + gitHubTag.following;
+  // bio.textContent = 'Bio:' + gitHubTag.bio;
 
 
 
@@ -106,21 +174,23 @@ function cardMaker (gitHubTag){
   const endGame = cards.appendChild(container);
 
   return endGame;
+
 }
 function getUser (username){
   const cards =document.querySelector('.cards');
 
-  axios.get (`htpps://api.github.com/users/${username}`)
-  .then(res => {
-    const cards = document.querySelector('.cards')
-    const cardReturn = cardMaker(res.data)
-    cards.appendChild(cardReturn)
+  // axios.get (`https://api.github.com/users/${username}`)
+  // .then(res => {
+  //   const cards = document.querySelector('.cards')
+  //   const cardReturn = cardMaker(res.data)
+  //   cards.appendChild(cardReturn)
     
-  })
-  .catch(err => {
-    console.error(err)
-  });
+  // })
+  // .catch(err => {
+  //   console.error(err)
+  // });
 }
+cardMaker
 
 
 cardMaker('NyamekyeAnnor');
@@ -130,82 +200,3 @@ followersarray.push('dustinmyers')
 followersarray.push('justsml')
 followersarray.push('luishrd')
 followersarray.push('bigknell')
-
-
-
-
-
-
-
-
-
-// go back to this is all fails 
-
-// const cards = document.querySelector('.cards');
-
-// function cardMaker (gitHubTag){
-//   const container = document.createElement('div');
-//   container.classList.add('card');
-//   const img = document.createElement('img');
-//   const cardInfo = document.createElement('div');
-//   cardInfo.classList.add('card-info');
-//   const name = document.createElement('h3');
-//   name.classList.add('name');
-//   const username = document.createElement('p');
-//   username.classList.add('username');
-//   const location = document.createElement('p');
-//   const profile = document.createElement('p');
-//   const profileAddress = document.createElement('a');
-//   const followers = document.createElement('p');
-//   const following = document.createElement('p');
-//   const bio = document.createElement('p');
-
-//   img.src = gitHubTag.avatar_url;
-//    name.textContent =gitHubTag.name;
-//   username.textContent = 'Username:' + gitHubTag.login;
-//   location.textContent = 'Location:' + gitHubTag.location;
-//   profileAddress.textContent = 'URL: ' + gitHubTag.url;
-//   followers.textContent = 'Followers:' + gitHubTag.followers;
-//   following.textContent = 'following:' + gitHubTag.following;
-//   bio.textContent = 'Bio:' + gitHubTag.bio;
-
-
-
-//   container.appendChild(img);
-//   container.appendChild(cardInfo);
-//   cardInfo.appendChild(name);
-//   cardInfo.appendChild(username);
-//   cardInfo.appendChild(location);
-//   cardInfo.appendChild(profile);
-//   cardInfo.appendChild(profileAddress);
-//   cardInfo.appendChild(followers);
-//   cardInfo.appendChild(following);
-//   cardInfo.appendChild(bio);
-
-//   const endGame = cards.appendChild(container);
-
-//   return endGame;
-// }
-// function getUser (username){
-//   const cards =document.querySelector('.cards');
-
-//   axios.get (`htpps://api.github.com/users/${username}`)
-//   .then(res => {
-//     const cards = document.querySelector('.cards')
-//     const cardReturn = cardMaker(res.data)
-//     cards.appendChild(cardReturn)
-    
-//   })
-//   .catch(err => {
-//     console.error(err)
-//   });
-// }
-
-
-// cardMaker('NyamekyeAnnor');
-// const followersarray = [];``
-// followersarray.push('tetondan')
-// followersarray.push('dustinmyers')
-// followersarray.push('justsml')
-// followersarray.push('luishrd')
-// followersarray.push('bigknell')
