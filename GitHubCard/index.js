075fee15b1a3c37a129a5d6192fa9f9634dd35b6
Krystle Mitchell <mitchell.krystle2@gmail.com>
2021-10-31 20:37:17 -0700

    created half of the component in task 3

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -6,6 +6,17 @@ import axios from 'axios';
     https://api.github.com/users/<your name>
 */
 
+
+axios.get('https:api.github.com/users/krystleM26')
+.then(res => {
+  console.log(res.data);
+})
+.catch(err => {
+  console.error(err);
+})
+
+
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -36,6 +47,8 @@ const followersArray = [];
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
 
+    
+
     <div class="card">
       <img src={image url of user} />
       <div class="card-info">
@@ -51,6 +64,17 @@ const followersArray = [];
       </div>
     </div>
 */
+
+function cardFace (object) {
+  let cardElement = document.createElement('div');
+    cardElement.setAttribute('class', 'card');
+    console.log(cardElement);
+
+  let cardImg = document.createElement('img');
+cardImg = object["avatar_url"];
+
+  
+}
 
 /*
   List of LS Instructors Github username's:
