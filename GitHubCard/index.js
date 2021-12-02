ddab5ae5058065cb2609f30e954365689d3c9289
Carlton Lauckern <reed@Carltons-MacBook-Air.local>
2021-12-02 12:35:35 -0800

    added-elements

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,12 @@
+import axios from 'axios';
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+ const url = `https://api.github.com/users/Rlauckern`
+
+
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -49,6 +53,20 @@ const followersArray = [];
       </div>
     </div>
 */
+
+//create elements 
+const cardElem = document.createElement('div');
+const img = document.createElement('img');
+const cardInfo = document.createElement('div');
+const header = document.createElement('h3');
+const username = document.createElement('p');
+const location = document.createElement('p');
+const profile = document.createElement('p');
+const github = document.createElement('p');
+const followers = document.createElement('p');
+const following = document.createElement('p');
+const bio = document.createElement('p');
+
 
 /*
   List of LS Instructors Github username's:
