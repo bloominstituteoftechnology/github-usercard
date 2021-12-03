e26a86d8fbb41474ad47455aab8f3f6ad405791e
AnneKGoodfriend <AnneKGoodfriend@gmail.com>
2021-12-02 20:05:42 -0500

    my card works

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,20 @@
+import axios from 'axios'
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+axios.get(`https://api.github.com/users/anneKgoodfriend`)
+  .then(response =>{
+    // console.log(response)
+    // console.log(response.data)
+    const cardInfo = cardMaker(response.data);
+    document.querySelector('div.cards').appendChild(cardInfo);
+  }).catch(error =>{
+    console.error(error);
+  })
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -49,6 +61,57 @@ const followersArray = [];
       </div>
     </div>
 */
+
+function cardMaker (object){
+  //create elements
+  const card = document.createElement('div');
+  const image = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const userName = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const profileLink = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+
+  //organize elements
+  card.appendChild(image);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(userName);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  profile.appendChild(profileLink);   
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+  //add classes
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  userName.classList.add('username');
+
+  //add content
+  image.src = object.avatar_url;
+  name.textContent = object.name;
+  userName.textContent = object.login;
+  location.textContent = object.location;
+  profileLink.href = object.html_url;
+  followers.textContent = object.followers;
+  following.textContent = object.following;
+  bio.textContent = object.bio;
+  
+  console.log(card);
+  return card;
+}
+
+//add to dom
+
+
+
 
 /*
   List of LS Instructors Github username's:
