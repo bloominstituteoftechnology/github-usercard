020c961534fac70463501808b4d7f1db56133c32
williamvelichko <williamvelichko2003@gmail.com>
2021-12-02 16:02:06 -0800

    i did most of it except one little thing

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
@@ -11,7 +12,16 @@
 
     Skip to STEP 3 (line 34).
 */
-
+// axios
+//   .get("https://api.github.com/users/williamVelichko")
+//   .then((resp) => {
+//     const user = userInterface(resp.data);
+//     document.querySelector(".cards").prepend(user);
+//     console.log(resp);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
 /*
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
@@ -28,13 +38,76 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell",
+  "williamvelichko",
+];
+
+function userInterface(obj) {
+  console.log(obj.name);
+  const mainContent = document.createElement("div");
+  const imageType = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const title = document.createElement("h3");
+  const usersName = document.createElement("p");
+  const usersLocation = document.createElement("p");
+  const usersProfile = document.createElement("p");
+  const anchorTag = document.createElement("a");
+  const followerCount = document.createElement("p");
+  const followingCount = document.createElement("p");
+  const usersBio = document.createElement("p");
+
+  mainContent.classList.add("card");
+  imageType.src = obj.avatar_url;
+  cardInfo.classList.add("card-info");
+  title.classList.add("name");
+  title.textContent = obj.name;
+  usersName.classList.add("username");
+  usersName.textContent = obj.login;
+  usersLocation.textContent = `Location: ${obj.location}`;
+  usersProfile.textContent = `profile: ${(anchorTag.href = obj.html_url)}`;
+  anchorTag.href = obj.html_url;
+  followerCount.textContent = `Followers: ${obj.followers}`;
+  followingCount.textContent = `Following: ${obj.following}`;
+  usersBio.textContent = `Bio: ${obj.bio}`;
+  //usersProfile.textContent = obj.html_url;
+
+  mainContent.appendChild(imageType);
+  mainContent.appendChild(cardInfo);
+  cardInfo.appendChild(title);
+  cardInfo.appendChild(usersName);
+  cardInfo.appendChild(usersLocation);
+  cardInfo.appendChild(usersProfile);
+  cardInfo.appendChild(followerCount);
+  cardInfo.appendChild(followingCount);
+  cardInfo.appendChild(usersBio);
+
+  usersProfile.appendChild(anchorTag);
+
+  return mainContent;
+}
+
+for (let i = 0; i < followersArray.length; i++) {
+  axios
+    .get(`https://api.github.com/users/${followersArray[i]}`)
+    .then((resp) => {
+      const user = userInterface(resp.data);
+      document.querySelector(".cards").appendChild(user);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
 
-    <div class="card">
+    <div class="card">                      
       <img src={image url of user} />
       <div class="card-info">
         <h3 class="name">{users name}</h3>
