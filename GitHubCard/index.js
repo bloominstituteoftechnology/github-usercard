f99e592b5325ff8099e3fd55301fa68d8f662850
David Conley <dconley1989@gmail.com>
2021-12-02 17:08:03 -0700

    finished the step 5 and finished the homework

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -6,19 +6,29 @@ import axios from "axios";
     https://api.github.com/users/<your name>
 */
 const divCards = document.querySelector(".cards");
-axios
-  .get("https://api.github.com/users/dconley1212")
-  .then((resp) => {
-    console.log(resp.data);
-    const githubCards = githubCard(resp.data);
-    console.log(githubCards);
-    divCards.appendChild(githubCards);
-  })
-  .catch((error) => {
-    console.log(error);
-  })
-  .finally(console.log("Woo"));
 
+const followersArray = [
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell",
+];
+
+followersArray.forEach((user) => {
+  axios
+    .get(`https://api.github.com/users/${user}`)
+    .then((resp) => {
+      console.log(resp.data);
+      const githubCards = githubCard(resp.data);
+      console.log(githubCards);
+      divCards.appendChild(githubCards);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .finally(console.log("Woo"));
+});
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -42,8 +52,6 @@ axios
     Using that array, iterate over it, requesting data for each user, creating a new card for each
     user, and adding that card to the DOM.
 */
-
-const followersArray = [];
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -107,12 +115,3 @@ function githubCard(obj) {
 
   return card;
 }
-
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
-*/
