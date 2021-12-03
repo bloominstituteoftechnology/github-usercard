59808dcbab6fe1916bc1e48073c1db61514d7594
Visnu <vgcaisse@gmail.com>
2021-12-02 22:50:31 -0500

    still working

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,19 @@
+import axios from "axios";
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/vgcaisse')
+  .then(resp => {
+    document.querySelector('.card').appendChild(cardMaker(resp.data))
+  })
+  .catch(err=> console.error(err));
 
+  cardCont.classList.add('card');
+  info.classList.add('card-info');
+  name.classList.add('name');
+  login.classList.add('username')
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -49,6 +59,45 @@ const followersArray = [];
       </div>
     </div>
 */
+
+function cardMaker(obj) {
+  const cardCont = document.createElement('div');
+  const img = document.createElement('img');
+  const info = document.createElement('div');
+  const name = document.createElement('h3');
+  const login = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const link = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+
+  img.src = gitInfo.avatar_url;
+  img.alt = 'git user profile picture';
+  name.textContent = gitInfo.name;
+  login.textContent = gitInfo.login;
+  location.textContent = gitInfo.location;
+  profile.textContent = 'Profile';
+  link.textContent = 'Link to Profile';
+  link.href = gitInfo.html_url;
+  followers.textContent = `Followers: ${gitInfo.followers}`;
+  following.textContent = `Followers: ${gitInfo.following}`;
+  bio.textContent = gitInfo.bio;
+
+  cardCont.appendChild(img);
+  cardCont.appendChild(info);
+  cardCont.appendChild(name);
+  cardCont.appendChild(login);
+  cardCont.appendChild(location);
+  cardCont.appendChild(profile);
+  cardCont.appendChild(link);
+  cardCont.appendChild(followers);
+  cardCont.appendChild(following);
+  cardCont.appendChild(bio);
+
+  return card;
+}
 
 /*
   List of LS Instructors Github username's:
