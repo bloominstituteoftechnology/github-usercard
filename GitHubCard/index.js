fbe02eecbb84fb69c43311d8f499a6e02e7a5b17
lomelo_x <aalomelo@gmail.com>
2021-12-02 18:06:01 -0600

    completed MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,65 +1,34 @@
 import axios from 'axios';
-/*
-  STEP 1: using axios, send a GET request to the following URL
-    (replacing the placeholder with your Github name):
-    https://api.github.com/users/lomelo-x
-*/
-axios.get('https://api.github.com/users/lomelo-x')
+
+const followersArray = [ 'tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
+const userCard = document.querySelector('.cards');
+
+function getFollowers (followers){
+  followers.forEach(User =>{
+    axios.get(`https://api.github.com/users/${User}`)
+    .then(response => {
+    document.querySelector('.cards').append(userCardMaker(response.data))}
+    )
+  })
+}
+
+function getUser (gitHubUserName){
+  axios.get(`https://api.github.com/users/${gitHubUserName}`)
 .then(response => {
+  userCard.appendChild(userCardMaker(response.data))
   console.log(response.data);
-}).catch(error => {
+})
+.catch(error => {
   console.error(error);
-  const errorMsg = document.createElement('p');
-  errorMsg.textContent = "AHHHHHHH I AM BROKEN";
-  entryPoint.appendChild(errorMsg);
-}).finally(() => console.log("WOOOOOHOOOO I WORK!"))
-/*
+})
+.finally(() => console.log("WOOOOOHOOOO I WORK!"))
+}
 
-  STEP 2: Inspect and study the data coming back, this is YOUR
-    github info! You will need to understand the structure of this
-    data in order to use it to build your component function
+getUser('lomelo-x')
+getFollowers(followersArray)
 
-    Skip to STEP 3 (line 34).
-*/
-
-/*
-  STEP 4: Pass the data received from Github into your function,
-    and append the returned markup to the DOM as a child of .cards
-*/
-
-/*
-  STEP 5: Now that you have your own card getting added to the DOM, either
-    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
-    manually find some other users' github handles, or use the list found at the
-    bottom of the page. Get at least 5 different Github usernames and add them as
-    Individual strings to the friendsArray below.
-
-    Using that array, iterate over it, requesting data for each user, creating a new card for each
-    user, and adding that card to the DOM.
-*/
-
-const followersArray = [];
-
-/*
-  STEP 3: Create a function that accepts a single object as its only argument.
-    Using DOM methods and properties, create and return the following markup:
-
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
-*/
-function userCardMaker(userName) {
+function userCardMaker(gitUser) {
   const card = document.createElement('div');
   const cardImage = document.createElement('img');
   const cardInfo = document.createElement('div')
@@ -67,41 +36,38 @@ function userCardMaker(userName) {
   const cardUserName = document.createElement('p');
   const cardLocation = document.createElement('p');
   const cardProfile = document.createElement('p');
+  const cardProfileLink = document.createElement('a')
   const cardFollowers = document.createElement('p');
   const cardFollowing = document.createElement('p')
   const cardBio = document.createElement('p')
 
   card.classList.add('card');
-  cardImage.src = avatar_url;
-  cardImage.classList.add('user-image')
-  cardInfo.classList.add('card-info')
-  cardName.classList.add('name')
-  cardUserName.classList.add('username')
-  cardLocation.textContent = 'Location:'
-  cardProfile.textContent = 'Profile:'
-  cardFollowers.textContent = 'Followers:'
-  cardFollowing.textContent = 'Following:'
-  cardBio.textContent = 'Bio:'
+  cardImage.src = gitUser.avatar_url;
+  cardImage.classList.add('user-image');
+  cardInfo.classList.add('card-info');
+  cardName.classList.add('name');
+  cardName.textContent = gitUser.name;
+  cardUserName.classList.add('username');
+  cardUserName.textContent = gitUser.login;
+  cardLocation.textContent = `Location: ${gitUser.location}`;
+  cardProfile.textContent = 'Profile: ';
+  cardProfileLink.textContent = gitUser.html_url;
+  cardProfileLink.href = gitUser.html_url;
+  cardFollowers.textContent = `Followers: ${gitUser.followers}`;
+  cardFollowing.textContent = `Following: ${gitUser.following}`;
+  cardBio.textContent = `Bio: ${gitUser.bio}`;
 
-  card.appendChild(cardImage)
-  card.appendChild(cardInfo)
-  card.appendChild(cardName)
-  card.appendChild(cardUserName)
-  card.appendChild(cardLocation)
-  card.appendChild(cardProfile)
-  card.appendChild(cardFollowers)
-  card.appendChild(cardFollowing)
-  card.appendChild(cardBio)
+  
+  card.appendChild(cardImage);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(cardName);
+  cardInfo.appendChild(cardUserName);
+  cardInfo.appendChild(cardLocation)
+  cardInfo.appendChild(cardProfile);
+  cardProfile.appendChild(cardProfileLink);
+  cardInfo.appendChild(cardFollowers);
+  cardInfo.appendChild(cardFollowing);
+  cardInfo.appendChild(cardBio)
 
   return card;
-};
-
-console.log(userCardMaker)
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
-*/
+}
--- README.md
+++ README.md
@@ -42,18 +42,18 @@ In this project we are going to be accessing the GitHub API and building a socia
 
 #### Option 1 (adding Axios via script element)
 
-* [ ] Include the script element linking the `axios` library in your HTML.
-* [ ] If you do not remember the code you can find it [here](https://github.com/axios/axios).
+* [X] Include the script element linking the `axios` library in your HTML.
+* [X] If you do not remember the code you can find it [here](https://github.com/axios/axios).
 
 #### Option 2 (installing Axios with npm)
 
-* [ ] Navigate to the root of the project with your command line.
-* [ ] Run `npm install axios` to download the dependency (it will be added to the `package.json` file).
-* [ ] At the top of the `GitHubCard/index.js` file, type `import axios from 'axios';`
+* [X] Navigate to the root of the project with your command line.
+* [X] Run `npm install axios` to download the dependency (it will be added to the `package.json` file).
+* [X] At the top of the `GitHubCard/index.js` file, type `import axios from 'axios';`
 
 ### Part 1: Requesting Data from the GitHub API
 
-* [ ] Follow the instructions found in the `GitHubCard/index.js` file to request data from the GitHub API.
+* [X] Follow the instructions found in the `GitHubCard/index.js` file to request data from the GitHub API.
 
 ### Part 2: Create the component function
 
