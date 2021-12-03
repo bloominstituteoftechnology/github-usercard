868ea4967d1865d5dc830c04025eff725ab1732f
Ken Smith <kennethologist@gmail.com>
2021-12-02 23:32:23 -0500

    Initial check in with task 1-3 completed

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,18 @@
+import axios from 'axios'
+
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/kennethologist')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error){
+    console.log(error);
+  })
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -30,6 +40,28 @@
 
 const followersArray = [];
 
+function cardMaker(card) {
+  const cardDiv = document.createElement("div");
+  cardDiv.className = "card";
+
+  const userImg = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  cardInfo.className = "card-info";
+  const nameh3 = document.createElement("h3");
+  nameh3.className = "name";
+
+  const usernamep = document.createElement("p");
+  usernamep.className = "username";
+  const locationp = document.createElement("p");
+
+  const profilep = document.createElement("p");
+  const linkgithubpage = document.createElement("a");
+
+  const followersp = document.createElement("p");
+  const followingp = document.createElement("p");
+  const biop = document.createElement("p");
+
+}
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
