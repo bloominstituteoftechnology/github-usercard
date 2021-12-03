dcb7f5559107cd5c906187b23f4de6193c15b5e4
AnneKGoodfriend <AnneKGoodfriend@gmail.com>
2021-12-02 20:54:14 -0500

    test

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -6,16 +6,6 @@ import axios from 'axios'
     https://api.github.com/users/<your name>
 */
 
-axios.get(`https://api.github.com/users/anneKgoodfriend`)
-  .then(response =>{
-    // console.log(response)
-    // console.log(response.data)
-    const cardInfo = cardMaker(response.data);
-    document.querySelector('div.cards').appendChild(cardInfo);
-  }).catch(error =>{
-    console.error(error);
-  })
-
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -40,8 +30,6 @@ axios.get(`https://api.github.com/users/anneKgoodfriend`)
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
-
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -61,6 +49,19 @@ const followersArray = [];
       </div>
     </div>
 */
+const followersArray = [ 'tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
+followersArray.forEach(elem => {
+  axios.get(`https://api.github.com/users/${elem}`)
+    .then(response =>{
+      // console.log(response)
+      // console.log(response.data)
+      const cardInfo = cardMaker(response.data);
+      document.querySelector('div.cards').appendChild(cardInfo);
+    }).catch(error =>{
+      console.error(error);
+    })
+})
 
 function cardMaker (object){
   //create elements
