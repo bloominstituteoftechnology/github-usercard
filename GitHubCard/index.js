d4d9ad1e52efcddcfb3d6a1ea52d2aaf9e329f1e
marcel <marcel_rodrgez@yahoo.com>
2021-12-02 23:07:18 -0500

    Finished homework added my card and my followers cards

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,29 @@
+import axios from 'axios';
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/marcel-rodriguez')
+.then(resp => {
+  const gitObj = {
+    avatar_url: resp.data.avatar_url,
+    name: resp.data.name,
+    login: resp.data.login,
+    location: resp.data.location,
+    html_url: resp.data.html_url,
+    followers: resp.data.followers,
+    following: resp.data.following,
+    bio: resp.data.bio
+  }
+  console.log(gitObj)
+  gitCardMaker(gitObj)
+})
+.catch(error => {
+  console.error(error)
+})
+
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -28,7 +49,35 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['benjamin-forest', 'JessWillCode', 'Berenika14', 'friend4', 'friend5'];
+
+function getFriendsInfo(array){
+  axios.get(`https://api.github.com/users/marcel-rodriguez/followers`)
+  .then(resp => {
+    resp.data.forEach(follower => {
+      axios.get(`https://api.github.com/users/${follower.login}`)
+      .then(resp => {
+        const followerObj = {
+          isFollower: true,
+          avatar_url: resp.data.avatar_url,
+          name: resp.data.name,
+          login: resp.data.login,
+          location: resp.data.location,
+          html_url: resp.data.html_url,
+          followers: resp.data.followers,
+          following: resp.data.following,
+          bio: resp.data.bio
+        }
+        gitCardMaker(followerObj)
+      })
+    })  
+  })
+  .catch(error => {
+    console.error(error);
+  })
+}
+
+getFriendsInfo(followersArray)
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -49,6 +98,73 @@ const followersArray = [];
       </div>
     </div>
 */
+
+function gitCardMaker(obj){
+  const gitCard = document.createElement('div')
+  gitCard.classList.add('card')
+
+  const userImage = document.createElement('img')
+  userImage.src = obj.avatar_url
+
+  const cardInfoDiv = document.createElement('div')
+  cardInfoDiv.classList.add('card-info')
+
+  const realName = document.createElement('h3')
+  realName.textContent = obj.name
+  realName.classList.add('name')
+
+  const userName = document.createElement('p')
+  userName.textContent = obj.login
+  userName.classList.add('username')
+
+  const userLocation = document.createElement('p')
+  userLocation.textContent = `Location: ${obj.location}`
+
+  const profileLinkHolder = document.createElement('p')
+  profileLinkHolder.textContent = `Profile: `
+
+  const profileLink = document.createElement('a')
+  profileLink.setAttribute('href', obj.html_url)
+  profileLink.textContent = obj.html_url
+
+  const followers = document.createElement('p')
+  followers.textContent = `Followers: ${obj.followers}`
+
+  const following = document.createElement('p')
+  following.textContent =  `Following: ${obj.following}`
+
+  const userBio = document.createElement('p')
+  userBio.textContent = `Bio: ${obj.bio}`
+
+  const isAFollower = document.createElement('p')
+  isAFollower.classList.add('username')
+  isAFollower.textContent = 'Marcel\'s Follower'
+
+  if(obj.bio === null){
+    userBio.textContent = `Bio: ${obj.login} does not have a bio!`
+  }
+  if(obj.location === null){
+    userLocation.textContent = `Location: ${obj.login} does not have a location set!`
+  }
+
+  gitCard.appendChild(userImage)
+  gitCard.appendChild(cardInfoDiv)
+  cardInfoDiv.appendChild(realName)
+  cardInfoDiv.appendChild(userName)
+  obj.isFollower ? cardInfoDiv.appendChild(isAFollower) : '';
+  cardInfoDiv.appendChild(userLocation)
+  cardInfoDiv.appendChild(profileLinkHolder)
+  profileLinkHolder.appendChild(profileLink)
+  cardInfoDiv.appendChild(followers)
+  cardInfoDiv.appendChild(following)
+  cardInfoDiv.appendChild(userBio)
+
+  document.querySelector('div.cards').appendChild(gitCard)
+
+  return gitCard
+}
+
+// console.log(gitCardMaker(gitObj))
 
 /*
   List of LS Instructors Github username's:
