d8803e95951483e8c97673a566ca3b4e3d5441a6
Gage Clancy <gclancy121@gmail.com>
2022-03-10 16:13:04 -0600

    got some baseline stuff working

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,14 @@
+import axios from "axios";
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/gclancy121')
+.then ((res) => {
+  createProfile(res.data);
+})
+
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -33,7 +39,42 @@ const followersArray = [];
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
+  */
 
+  function createProfile(obj) {
+    const card = document.createElement('div');
+    const image = document.createElement('img');
+    const cardInfo = document.createElement('div');
+    const name = document.createElement('h3');
+    const username = document.createElement('p');
+    const location = document.createElement('p');
+    const profile = document.createElement('p');
+    const profileURL =  document.createElement('a');
+    const followers = document.createElement('p');
+    const following = document.createElement('p');
+    const bio = document.createElement('p');
+    card.appendChild(image);
+    card.appendChild(cardInfo);
+    cardInfo.appendChild(name);
+    cardInfo.appendChild(username);
+    cardInfo.appendChild(location);
+    cardInfo.appendChild(profile);
+    profile.appendChild(profileURL);
+    cardInfo.appendChild(followers);
+    cardInfo.appendChild(following);
+    cardInfo.appendChild(bio);
+    card.classList.add('card');
+    cardInfo.classList.add('card-info');
+    name.classList.add('name');
+    username.classList.add('username');
+   
+  
+  return card;
+}
+
+createProfile();
+console.log(createProfile());
+    /*
     <div class="card">
       <img src={image url of user} />
       <div class="card-info">
