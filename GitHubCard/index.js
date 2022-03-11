e13ff0998968539b46db78e75b656dea9c3fba4d
Jericho-West <97463076+Jericho-West@users.noreply.github.com>
2022-03-10 23:58:43 -0500

    Steps 3 and 4

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -9,10 +9,8 @@ import axios from "axios"
 axios.get("https://api.github.com/users/Jericho-West")
 .then (x => {
   console.log(x.data)
-})
-.catch (err => {
-  console.log(err)
-})
+  let pancake = x.data
+
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -68,3 +66,58 @@ const followersArray = [];
     luishrd
     bigknell
 */
+
+function cardMaker (card0) {
+  let attach = document.querySelector(".cards")
+
+  let mainDiv = document.createElement("div")
+  let img0 = document.createElement("img")
+  let sideDiv = document.createElement("div")
+  let water = document.createElement("h3")
+  let p0 = document.createElement("p")
+  let p1 = document.createElement("p")
+  let p2 = document.createElement("p")
+  let p3 = document.createElement("p")
+  let p4 = document.createElement("p")
+  let p5 = document.createElement("p")
+  let aa = document.createElement("a")
+
+  mainDiv.className = "card"
+  sideDiv.className = "card-info"
+  water.className = "name"
+  p0.className = "username"
+  img0.src = card0.avatar_url
+  aa.href = card0.html_url
+  water.textContent = card0.name
+  p0.textContent = card0.login
+  p1.textContent = `Location: ${card0.location}`
+  p2.textContent = "Profile: "
+  p3.textContent = `Followers: ${card0.followers}`
+  p4.textContent = `Following: ${card0.following}`
+  p5.textContent = `Bio: ${card0.bio}`
+  aa.textContent = `${card0.html_url}`
+
+
+  attach.appendChild(mainDiv)
+  mainDiv.appendChild(img0)
+  mainDiv.appendChild(sideDiv)
+  sideDiv.appendChild(water)
+  sideDiv.appendChild(p0)
+  sideDiv.appendChild(p1)
+  sideDiv.appendChild(p2)
+  sideDiv.appendChild(p3)
+  sideDiv.appendChild(p4)
+  sideDiv.appendChild(p5)
+  p2.appendChild(aa)
+}
+
+
+
+
+
+cardMaker(pancake)
+
+})
+.catch (err => {
+  console.log(err)
+})
