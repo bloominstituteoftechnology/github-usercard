ee2572b75b63850f14921c903fa8a60adcc8b117
Jordon Pruitt <jordonpruitt@Jordons-MacBook-Pro.local>
2022-07-14 06:48:30 -0400

    done with proj, come back for stretch :D

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,7 +3,30 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+import axios from "axios";
 
+const followersArray = ['tetondan',
+'dustinmyers',
+'justsml',
+'luishrd',
+'bigknell'];
+
+followersArray.forEach(e =>{
+  addMoreCards(e)
+})
+    
+  function addMoreCards(name){
+    axios.get(`https://api.github.com/users/${name}`)
+    .then(response =>{
+        container.appendChild(gHubCard(response.data))
+    })
+    .catch(err => {
+      debugger
+    })
+  }
+
+  
+  
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -28,12 +51,18 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+// const followersArray = [
+//   'https://api.github.com/users/dustinmyers',
+//   'https://api.github.com/users/JordonP123',
+//   'https://api.github.com/users/justsml',
+//   'https://api.github.com/users/luishrd',
+//   'https://api.github.com/users/bigknell',
+// ]
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
-
+  
     <div class="card">
       <img src={image url of user} />
       <div class="card-info">
@@ -50,11 +79,66 @@ const followersArray = [];
     </div>
 */
 
+function gHubCard(obj){
+  //elements apart of gHub card;
+const containerDiv = document.createElement('div');
+const img = document.createElement('img');
+const secondContainerDiv = document.createElement('div');
+const h3 = document.createElement('h3');
+const pUser = document.createElement('p');
+const pLoc = document.createElement('p');
+const pProfile = document.createElement('p');
+const pFollowers = document.createElement('p');
+const pFollowing = document.createElement('p');
+const pBio = document.createElement('p');
+const anchor = document.createElement('a')
+
+//appending children
+containerDiv.appendChild(img);
+containerDiv.appendChild(secondContainerDiv);
+secondContainerDiv.appendChild(h3);
+secondContainerDiv.appendChild(pUser);
+secondContainerDiv.appendChild(pLoc);
+secondContainerDiv.appendChild(pProfile);
+secondContainerDiv.appendChild(pFollowers);
+secondContainerDiv.appendChild(pFollowing);
+secondContainerDiv.appendChild(pBio);
+pProfile.appendChild(anchor)
+
+
+//adding classes
+containerDiv.classList.add('card');
+secondContainerDiv.classList.add('card-info');
+h3.classList.add('name');
+pUser.classList.add('username');
+
+//adding textContent
+img.src = obj.avatar_url;
+h3.textContent = obj.name;
+pUser.textContent = obj.login;
+pLoc.textContent = 'Location';
+pLoc.textContent = obj.location;
+pProfile.textContent = 'Profile';
+anchor.href = obj.html_url;
+pFollowers.textContent = `Followers: ${obj.followers}`
+pFollowing.textContent = `Following: ${obj.following}`
+pBio.textContent = `Bio: ${obj.bio}`
+
+return containerDiv
+
+}
+//testing appends
+const container = document.querySelector('.cards');
+// console.log(container.appendChild(gHubCard()))
+
+
+
+
 /*
   List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
+    'tetondan',
+    'dustinmyers',
+    'justsml',
+    'luishrd',
+    'bigknell'
 */
