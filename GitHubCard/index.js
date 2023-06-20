9ba2e8ead2db823566956acee8d2a25e46e05171
Christian Anderson <tehninjawaffle@gmail.com>
2023-06-20 11:29:50 -0700

    Got data from github and have made the card layout

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,10 @@
+import axios from "axios";
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+const myData = axios.get("https://api.github.com/users/Malekkos")
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -11,6 +12,13 @@
 
     Skip to STEP 3 (line 34).
 */
+console.log(myData);
+// Things to be added on the card 
+// Real Name and username ---------------------------------------- object.data.login & object.data.name
+// Location ------------------------------------------------------ object.data.location
+// a Link to my profile eg. https://github.com/Malekkos ---------- object.data.html_url
+// followers as well as who im following (both are zero); -------- object.data.followers  &  object.data.following
+// bio ----------------------------------------------------------- object.data.bio
 
 /*
   STEP 4: Pass the data received from Github into your function,
@@ -49,7 +57,44 @@ const followersArray = [];
       </div>
     </div>
 */
-
+function userCardCreator (obj) {
+  const container = document.createElement("div");
+  const profilePicture = document.createElement("img");
+  const cardInfoContainer = document.createElement("div");
+  const realName = document.createElement("h3");
+  const userName = document.createElement("p");
+  const location = document.createElement("p");
+  const profile = document.createElement("p");
+  const profAddress = document.createElement("a");
+  const followers = document.createElement("p");
+  const following = document.createElement("p");
+  const bio = document.createElement("p");
+  container.classList = "card";
+  cardInfoContainer.classList = "card-info";
+  realName.classList = "name";
+  userName.classList = "username";
+  profilePicture.src = obj.name.avatar_url;
+  realName.textContent = obj.data.name;
+  userName.textContent = obj.data.login;
+  location.textContent = `Location: ${obj.data.location}`;
+  profile.textContent = "Profile:";
+  profAddress.href = obj.data.url;
+  profAddress.textContent = obj.data.url;
+  followers.textContent = `Followers: ${obj.data.followers}`;
+  following.textContent = `Following: ${obj.data.following}`;
+  bio.textContent = `Bio: ${obj.data.bio}`;
+  container.appendChild(profilePicture);
+  container.appendChild(cardInfoContainer);
+  cardInfoContainer.appendChild(realName)
+  cardInfoContainer.appendChild(userName)
+  cardInfoContainer.appendChild(location)
+  cardInfoContainer.appendChild(profile)
+  profile.appendChild(profAddress)
+  cardInfoContainer.appendChild(followers)
+  cardInfoContainer.appendChild(following)
+  cardInfoContainer.appendChild(bio)
+  return container;
+}
 /*
   List of LS Instructors Github username's:
     tetondan
