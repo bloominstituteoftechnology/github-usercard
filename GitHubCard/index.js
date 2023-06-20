dbf4783849cbb2bfc578e7cfadeef57d0d7e0d23
Christian Anderson <tehninjawaffle@gmail.com>
2023-06-20 12:01:32 -0700

    Got my own Thing on the site, just need to figure out how to get others on

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -5,6 +5,19 @@ import axios from "axios";
     https://api.github.com/users/<your name>
 */
 const myData = axios.get("https://api.github.com/users/Malekkos")
+.then(res => {
+  const card = userCardCreator(res);
+  let holder = document.querySelector(".cards");
+  // console.log(res);
+  holder.appendChild(card);
+  console.log(card);
+  // return res;
+  return card
+})
+.catch(err => {
+  return err
+})
+console.log(myData);
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -12,7 +25,7 @@ const myData = axios.get("https://api.github.com/users/Malekkos")
 
     Skip to STEP 3 (line 34).
 */
-console.log(myData);
+// console.log(myData);
 // Things to be added on the card 
 // Real Name and username ---------------------------------------- object.data.login & object.data.name
 // Location ------------------------------------------------------ object.data.location
@@ -24,7 +37,18 @@ console.log(myData);
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
-
+function axiosGrabber() {
+  console.log("funny haha");
+  axios.get("https://api.github.com/users/Malekkos")
+    .then(res => {
+      console.log(res.data.name);
+    })
+    .catch(err => {
+      console.log(`there is nothing to be found! ${err}`)
+    })
+}
+axiosGrabber();
+// userCardCreator(result)
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
@@ -36,8 +60,13 @@ console.log(myData);
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
-
+const followersArray = ["tetondan", "dustinmyers", "justsml", "luishrd", "bigknell"];
+console.log(followersArray);
+const allTheNames = followersArray.forEach(name => {
+  const newCard = `https://api.github.com/users/${name}`;
+  console.log(newCard);
+  return newCard
+})
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -73,13 +102,13 @@ function userCardCreator (obj) {
   cardInfoContainer.classList = "card-info";
   realName.classList = "name";
   userName.classList = "username";
-  profilePicture.src = obj.name.avatar_url;
+  profilePicture.src = obj.data.avatar_url;
   realName.textContent = obj.data.name;
   userName.textContent = obj.data.login;
   location.textContent = `Location: ${obj.data.location}`;
   profile.textContent = "Profile:";
-  profAddress.href = obj.data.url;
-  profAddress.textContent = obj.data.url;
+  profAddress.href = obj.data.html_url;
+  profAddress.textContent = obj.data.html_url;
   followers.textContent = `Followers: ${obj.data.followers}`;
   following.textContent = `Following: ${obj.data.following}`;
   bio.textContent = `Bio: ${obj.data.bio}`;
